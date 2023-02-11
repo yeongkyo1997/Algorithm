960c822aa304283ff3bc707f370f5a946a9ba73c
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-11 17:49:31 +0900

    --wip-- [skip ci]

---
+++ Algorithm/src/Main_1074.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main_1074 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        bw.write(sol(N, r, c) + "");
+        bw.close();
+    }
+
+    static int sol(int N, int r, int c) {
+        if (N == 0)
+            return 0;
+        return 2 * (r % 2) + (c % 2) + 4 * sol(N - 1, r / 2, c / 2);
+    }
+}
