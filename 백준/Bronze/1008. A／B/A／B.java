42e162ebf71a818d41087c4d4bbb52ebdb942306
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-15 10:15:55 +0900

    [Bronze V] Title: A/B, Time: 132 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        bw.write((Double.parseDouble(st.nextToken()) / Integer.parseInt(st.nextToken())) + "");
+        bw.flush();
+        bw.close();
+    }
+}
