bbe2d2d0429af5b2f24ff3833580199e3c50a571
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-16 09:16:39 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 148 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -7,26 +7,26 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    private static int result;
 
     public static void main(String[] args) throws IOException {
+        Map<String, Integer> map = new HashMap<>();
+
         int T = Integer.parseInt(br.readLine());
 
-        for (int t = 0; t < T; t++) {
+        while (T-- != 0) {
             int N = Integer.parseInt(br.readLine());
-            Map<String, Integer> map = new HashMap<>();
+            map.clear();
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 st.nextToken();
-                String key = st.nextToken();
-                map.put(key, map.getOrDefault(key, 0) + 1);
+                String str = st.nextToken();
+                map.put(str, map.getOrDefault(str, 0) + 1);
             }
-            result = 1;
+            int sum = 1;
             for (int value : map.values()) {
-                result *= (value + 1);
+                sum *= (value + 1);
             }
-            bw.write(result - 1 + "\n");
-        
+            bw.write(sum - 1 + "\n");
         }
         bw.close();
     }
