efb7e7ebf72e5948b0e08a628bb69d05a9724179
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-18 16:22:02 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 124 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        String str = br.readLine();
+        if (str.charAt(0) == str.charAt(1)) bw.write(1 + "");
+        else bw.write(0 + "");
+        bw.close();
+    }
+}
