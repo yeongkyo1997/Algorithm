4d9c2a9b4d51ba0c5ee161786a70708733ab58a4
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-18 14:20:07 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        BigInteger result = BigInteger.ONE;
+
+        for (int i = 1; i < N + 1; i++) {
+            result = result.multiply(new BigInteger(String.valueOf(i)));
+        }
+        bw.write(result + "");
+        bw.close();
+    }
+}
