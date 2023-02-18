8de66aeaf196709a258573aaedff4ede39cfd874
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-18 14:26:46 +0900

    [Bronze V] Title: Serca, Time: 280 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        while (N-- != 0)
+            bw.write(" @@@   @@@ \n" + "@   @ @   @\n" + "@    @    @\n" + "@         @\n" + " @       @ \n" + "  @     @  \n" + "   @   @   \n" + "    @ @    \n" + "     @     \n" + "");
+
+        bw.close();
+    }
+}
