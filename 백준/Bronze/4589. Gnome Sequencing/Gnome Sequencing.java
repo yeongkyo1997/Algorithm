3e215e9bdbc13f0c3770f7bf941406c4e10854e9
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-21 12:48:42 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        bw.write("Gnomes:" + "\n");
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a, b, c;
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+
+            if (a < b && b < c) bw.write("Ordered" + "\n");
+            else if (a > b && b > c) bw.write("Ordered" + "\n");
+            else bw.write("Unordered" + "\n");
+        }
+        bw.close();
+    }
+}
