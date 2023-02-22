774465d11fcf0b0da4d66c01d8e3e6f56cc33304
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-22 20:33:55 +0900

    [Gold IV] Title: 공유기 설치, Time: 308 ms, Memory: 29312 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// BOJ 2110 공유기 설치
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 1;
+        int right = arr[N - 1] - arr[0];
+        int result = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int start = arr[0];
+            int cnt = 1;
+
+            for (int i = 1; i < N; i++) {
+                if (arr[i] - start >= mid) {
+                    cnt++;
+                    start = arr[i];
+                }
+            }
+
+            if (cnt >= C) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        bw.write(result + "");
+        bw.close();
+    }
+}
