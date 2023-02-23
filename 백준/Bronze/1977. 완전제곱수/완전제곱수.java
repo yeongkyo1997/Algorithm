59b5603d7903c972302926b46656d2ba7d3ee190
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-23 10:12:14 +0900

    [Bronze II] Title: 완전제곱수, Time: 152 ms, Memory: 15992 KB -BaekjoonHub

---
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int min = 0;
+
+        for (int i = N; i <= M; i++) {
+            if (isSquare(i)) {
+                sum += i;
+                if (min == 0) min = i;
+            }
+        }
+        if (sum == 0) bw.write("-1");
+        else bw.write(sum + "\n" + min);
+        bw.close();
+    }
+
+    static boolean isSquare(int n) {
+        int sqrt = (int) Math.sqrt(n);
+        return sqrt * sqrt == n;
+    }
+}
