c4bd61c6ea36e099bd4ae2fc8648759fd849c85b
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-23 22:56:40 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -0,0 +1,13 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def main():
+    n = int(input())
+    for i in range(n):
+        print('*' * (i + 1))
+
+
+if __name__ == '__main__':
+    main()
