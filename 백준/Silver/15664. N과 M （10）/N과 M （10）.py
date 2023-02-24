159e43add97ec739f1daa13ecb7b9a05ef9157b0
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-24 10:12:30 +0900

    [Silver II] Title: N과 M (10), Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -1,22 +1,12 @@
+import itertools
 import sys
 
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
 
-n, m = map(int, input().split())
-arr = sorted(map(int, input().split()))
-result = []
+def main():
+    N, M = map(int, input().split())
+    for i in sorted(set(itertools.combinations(sorted(map(int, input().split())), M))):
+        print(*i)
 
-def dfs(depth, num):
-    if depth == m:
-        print(" ".join(map(str, result)))
-        return
-    mem = 0
-
-    for i in range(num, n):
-        if mem != arr[i]:
-            result.append(arr[i])
-            mem = arr[i]
-            dfs(depth + 1, i + 1)
-            result.pop()
-
-dfs(0, 0)
+if __name__ == '__main__':
+    main()
