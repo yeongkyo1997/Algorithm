59a27b2027eb9c98bc3f25fd1279e651353137c5
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-24 10:01:48 +0900

    [Silver III] Title: N과 M (8), Time: 144 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.py
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -1,20 +1,14 @@
+import itertools
 import sys
 
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
 
-n, m = map(int, input().split())
-arr = sorted(map(int, input().split()))
-result = []
+def main():
+    N, M = map(int, input().split())
+    arr = map(int, input().split())
+    
+    for i in itertools.combinations_with_replacement(sorted(arr), M):
+        print(*i)
 
-
-def dfs(num):
-    if len(result) == m:
-        print(" ".join(map(str, result)))
-        return
-    for i in range(num, n):
-        result.append(arr[i])
-        dfs(i)
-        result.pop()
-
-
-dfs(0)
+if __name__ == '__main__':
+    main()
