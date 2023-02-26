452cdf7a6f450983f767a69159f5aeb7b3808a15
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-26 12:26:01 +0900

    [Bronze V] Title: 검증수, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,5 +1,4 @@
-arr=list(map(int, input().split()))
-result = 0
-for n in arr:
-    result += n ** 2
-print(result % 10)
+arr = map(int, input().split())
+s = sum(map(lambda x: x ** 2, arr))
+
+print(str(s)[-1])
