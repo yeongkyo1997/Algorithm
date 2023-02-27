a7391d6401141ac3239db603793276e05695204b
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-02-27 15:46:02 +0900

    [Gold IV] Title: 서강그라운드, Time: 120 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,35 +1,31 @@
 import sys
 
+sys.setrecursionlimit(10000)
 input = lambda: sys.stdin.readline().rstrip()
 
-
-# BOJ 14938 서강그라운드
 def main():
-    n, m, r = map(int, input().split())
+    N, M, R = map(int, input().split())
     items = [0] + list(map(int, input().split()))
-    graph = [[sys.maxsize] * (n + 1) for _ in range(n + 1)]
-    for _ in range(r):
+    graph = [[float('inf')] * (N + 1) for _ in range(N + 1)]
+    for _ in range(R):
         a, b, l = map(int, input().split())
         graph[a][b] = l
         graph[b][a] = l
-
-    for i in range(1, n + 1):
+    for i in range(1, N + 1):
         graph[i][i] = 0
-
-    for k in range(1, n + 1):
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-
+    for k in range(1, N + 1):
+        for i in range(1, N + 1):
+            for j in range(1, N + 1):
+                if graph[i][j] > graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
     result = 0
-    for i in range(1, n + 1):
+    for i in range(1, N + 1):
         cnt = 0
-        for j in range(1, n + 1):
-            if graph[i][j] <= m:
+        for j in range(1, N + 1):
+            if graph[i][j] <= M:
                 cnt += items[j]
         result = max(result, cnt)
     print(result)
-
 
 if __name__ == '__main__':
     main()
