b8a6fd91583605dc91791d412adbb494913c47c8
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-01 09:58:03 +0900

    [Bronze V] Title: A-B, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.py
+++ 백준/Bronze/1001. A－B/A－B.py
@@ -1,12 +1,2 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
-
-def main():
-    a, b = map(int, input().split())
-    print(a - b)
-
-
-if __name__ == '__main__':
-    main()
+a, b = map(int, input().split())
+print(a - b)
