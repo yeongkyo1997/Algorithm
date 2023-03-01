9bac7b42aeb52e86669085e309f1dc6937574cab
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-01 15:02:15 +0900

    [Silver III] Title: N과 M (6), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -1,14 +1,17 @@
 import itertools
 import sys
 
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
+
 
 def main():
     N, M = map(int, input().split())
     arr = map(int, input().split())
-    
+
     for i in itertools.combinations(sorted(arr), M):
         print(*i)
+
 
 if __name__ == '__main__':
     main()
