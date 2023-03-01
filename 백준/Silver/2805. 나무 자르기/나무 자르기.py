4af8f98018173f237e3331e6b398044a34a965d6
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-01 12:38:44 +0900

    [Silver II] Title: 나무 자르기, Time: 2556 ms, Memory: 150704 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,26 +1,30 @@
 import sys
 
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
 
 def main():
     N, M = map(int, input().split())
-    arr = list(map(int, input().split()))
-    left = 0
-    right = max(arr)
 
-    while left < right:
-        cur = 0
+    arr = list(map(int, input().split()))
+
+    left = 1
+    right = 2000000000
+
+    while left <= right:
+        total = 0
         mid = (left + right) // 2
 
         for i in arr:
             if i - mid > 0:
-                cur += i - mid
+                total += i - mid
 
-        if cur < M:
-            right = mid
-        else:
+        if total >= M:
             left = mid + 1
+        else:
+            right = mid - 1
+
     print(left - 1)
 
 
