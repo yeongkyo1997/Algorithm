1c99ed4c50debb37ea48aa650d024fdd4c643a5f
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-01 09:55:41 +0900

    [Silver V] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -5,19 +5,17 @@ input = lambda: sys.stdin.readline().rstrip()
 
 def main():
     N = int(input())
-    arr = list(map(int, input().split()))
+    arr = map(int, input().split())
     cnt = 0
-
-    for i in range(N):
-        if isPrime(arr[i]):
+    for i in arr:
+        if isPrime(i):
             cnt += 1
     print(cnt)
 
 
 def isPrime(n):
-    if n <= 1:
+    if n == 1:
         return False
-
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
