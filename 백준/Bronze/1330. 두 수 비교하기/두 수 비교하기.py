6863aa4529e3a9c1a954df951359eb9628b64074
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-01 10:03:22 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -1,17 +1,8 @@
-import sys
+a, b = map(int, input().split())
 
-input = lambda: sys.stdin.readline().rstrip()
-
-
-def main():
-    a, b = map(int, input().split())
-    if a > b:
-        print('>')
-    elif a < b:
-        print('<')
-    else:
-        print('==')
-
-
-if __name__ == '__main__':
-    main()
+if a < b:
+    print('<')
+elif a > b:
+    print('>')
+else:
+    print('==')
