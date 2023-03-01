b280955f6ba2d5867fe37bf4c11b97e09a68ded9
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-01 09:58:51 +0900

    [Bronze V] Title: A/B, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.py
+++ 백준/Bronze/1008. A／B/A／B.py
@@ -1,12 +1,2 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
-
-def main():
-    a, b = map(int, input().split())
-    print(f"{a / b:.10f}")
-
-
-if __name__ == '__main__':
-    main()
+a, b = map(int, input().split())
+print(a / b)
