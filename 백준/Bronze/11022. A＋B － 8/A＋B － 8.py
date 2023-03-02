e127472f36fcd0a277b4af5269759d248c4009e4
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-02 12:38:34 +0900

    [Bronze V] Title: A+B - 8, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,12 +1,5 @@
-import sys
+T = int(input())
 
-input = lambda: sys.stdin.readline().rstrip()
-
-def main():
-    T = int(input())
-    for i in range(T):
-        a, b = map(int, input().split())
-        print(f'Case #{i + 1}: {a} + {b} = {a + b}')
-
-if __name__ == '__main__':
-    main()
+for i in range(T):
+    a, b = map(int, input().split())
+    print(f'Case #{i + 1}: {a} + {b} = {a + b}')
