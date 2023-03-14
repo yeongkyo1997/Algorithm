68e8acbf091516dfefcce89aeb631cad7271c758
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-14 14:54:30 +0900

    [Silver I] Title: 경로 찾기, Time: 248 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.setrecursionlimit(1000000)
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if arr[i][k] == 1 and arr[k][j] == 1:
+                arr[i][j] = 1
+
+for i in range(N):
+    for j in range(N):
+        print(arr[i][j], end=' ')
+    print()
