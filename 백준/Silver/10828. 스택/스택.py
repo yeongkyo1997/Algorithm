f6082ec2144d250f7ffbca91005951fe70088e68
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-24 03:14:37 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,27 +1,34 @@
 import sys
 
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
-stack = []
-for _ in range(int(input())):
-    arr = input().split()
 
-    if arr[0] == 'push':
-        stack.append(arr[1])
-    elif arr[0] == 'pop':
-        if stack:
-            print(stack.pop())
-        else:
-            print(-1)
-    elif arr[0] == 'size':
-        print(len(stack))
-    elif arr[0] == 'empty':
-        if stack:
-            print(0)
-        else:
-            print(1)
-    else:
-        if stack:
-            print(stack[-1])
-        else:
-            print(-1)
+def main():
+    n = int(input())
+    stack = []
+    for _ in range(n):
+        cmd = input().split()
+        if cmd[0] == 'push':
+            stack.append(cmd[1])
+        elif cmd[0] == 'pop':
+            if stack:
+                print(stack.pop())
+            else:
+                print(-1)
+        elif cmd[0] == 'size':
+            print(len(stack))
+        elif cmd[0] == 'empty':
+            if stack:
+                print(0)
+            else:
+                print(1)
+        elif cmd[0] == 'top':
+            if stack:
+                print(stack[-1])
+            else:
+                print(-1)
+
+
+if __name__ == '__main__':
+    main()
