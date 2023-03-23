ee4d19cabef4a6154902985ead7412c407835322
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-24 02:54:39 +0900

    [Silver V] Title: 영화감독 숌, Time: 644 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,21 +1,15 @@
 import sys
 
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
-
-def main():
-    N = int(input())
-    cnt = 0
-    num = 666
-
-    while True:
-        if '666' in str(num):
-            cnt += 1
-        if cnt == N:
-            print(num)
-            break
-        num += 1
-
-
-if __name__ == '__main__':
-    main()
+n = int(input())
+cnt = 0
+num = 666
+while True:
+    if '666' in str(num):
+        cnt += 1
+    if cnt == n:
+        print(num)
+        break
+    num += 1
