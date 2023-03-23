10893009f0c843f5fd8f47ff0f224adf7b0c8808
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-23 20:10:37 +0900

    [Gold V] Title: 토마토, Time: 2332 ms, Memory: 107424 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -4,44 +4,36 @@ import sys
 sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
-N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(M)]
-visited = [[0] * N for _ in range(M)]
-queue = collections.deque()
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+M, N = map(int, input().split())
+tomato = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
+dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
+q = collections.deque()
+for i in range(N):
+    for j in range(M):
+        if tomato[i][j] == 1:
+            q.append((i, j))
+            visited[i][j] = 1
 
+while q:
+    x, y = q.popleft()
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M and tomato[nx][ny] == 0 and visited[nx][ny] == 0:
+            tomato[nx][ny] = 1
+            visited[nx][ny] = visited[x][y] + 1
+            q.append((nx, ny))
 
-def check():
-    for i in range(M):
-        for j in range(N):
-            if arr[i][j] == 0:
-                return False
+flag = 0
+for i in range(N):
+    for j in range(M):
+        if tomato[i][j] == 0:
+            flag = 1
+            break
+    if flag:
+        break
 
-    return True
-
-
-def bfs():
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-
-            if 0 <= nx < M and 0 <= ny < N and visited[nx][ny] == 0 and arr[nx][ny] == 0:
-                visited[nx][ny] = visited[x][y] + 1
-                arr[nx][ny] = 1
-                queue.append((nx, ny))
-
-    if check():
-        return max(map(max, visited))
-    else:
-        return -1
-
-
-for i in range(M):
-    for j in range(N):
-        if arr[i][j] == 1:
-            queue.append((i, j))
-
-print(bfs())
+if flag:
+    print(-1)
+else:
+    print(max(map(max, visited)) - 1)
