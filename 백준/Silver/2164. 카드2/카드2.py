cdeca90be5f2e00d95b88248b609aa13d1a53c2b
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-24 03:06:03 +0900

    [Silver IV] Title: 카드2, Time: 228 ms, Memory: 55092 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,13 +1,12 @@
 import collections
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+input = lambda: sys.stdin.readline().rstrip()
 
 n = int(input())
-
-queue = collections.deque()
-
-for i in range(1, n + 1):
-    queue.append(i)
-
-while len(queue) > 1:
-    queue.popleft()
-    queue.append(queue.popleft())
-print(queue.pop())
+q = collections.deque([i for i in range(1, n + 1)])
+while len(q) > 1:
+    q.popleft()
+    q.append(q.popleft())
+print(q[0])
