4baa4de8fad122b4c5b4b2f362d12f91340d7e25
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-23 20:22:48 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 336 ms, Memory: 56524 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,38 +1,43 @@
 import sys
 
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
-print = lambda x: sys.stdout.write(str(x) + '\n')
 
 V, E = map(int, input().split())
 parent = [i for i in range(V + 1)]
-Edges = []
-for _ in range(E):
-    a, b, c = map(int, input().split())
-    Edges.append((a, b, c))
 
-Edges.sort(key=lambda x: x[2])
+edges = []
+
+for _ in range(E):
+    edges.append(list(map(int, input().split())))
+
+edges.sort(key=lambda _: _[2])
+
 
 def find(a):
-    if parent[a] == a:
+    if a == parent[a]:
         return a
-    
+
     parent[a] = find(parent[a])
     return parent[a]
 
+
 def union(a, b):
-    a, b = find(a), find(b)
-    
+    a = find(a)
+    b = find(b)
+
     if a == b:
         return False
-    
+
     parent[max(a, b)] = min(a, b)
-    
+
     return True
+
 
 result = 0
 
-for a, b, cost in Edges:
-    if union(a, b):
-        result += cost
+for start, end, weight in edges:
+    if union(start, end):
+        result += weight
 
 print(result)
