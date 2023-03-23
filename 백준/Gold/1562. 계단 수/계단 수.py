2a8483fb9d2dc63d2bd62f0481a583ef48ed1b73
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-23 22:57:24 +0900

    [Gold I] Title: 계단 수, Time: 860 ms, Memory: 38580 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.py
+++ 백준/Gold/1562. 계단 수/계단 수.py
@@ -1,25 +1,27 @@
 import sys
 
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
-def main():
-    N = int(input())
-    dp = [[[0] * 10 for _ in range(1 << 10)] for _ in range(N + 1)]
-    
-    for i in range(1, 10):
-        dp[1][1 << i][i] = 1
-        
-    for i in range(2, N + 1):
-        for j in range(1 << 10):
-            for k in range(10):
-                if k == 0:
-                    dp[i][j | (1 << k)][k] += dp[i - 1][j][k + 1]
-                elif k == 9:
-                    dp[i][j | (1 << k)][k] += dp[i - 1][j][k - 1]
-                else:
-                    dp[i][j | (1 << k)][k] += dp[i - 1][j][k - 1] + dp[i - 1][j][k + 1]
-    print(sum(dp[N][(1 << 10) - 1]) % 1000000000)
+MOD = 1000000000
+N = int(input())
+dp = [[[0] * 1024 for _ in range(10)] for _ in range(101)]
+full = 1023
+for j in range(1, 10):
+    dp[1][j][1 << j] = 1
 
-if __name__ == '__main__':
-    main()
+for i in range(2, N + 1):
+    for j in range(10):
+        for k in range(1024):
+            if j == 0:
+                dp[i][0][k | (1 << 0)] = (dp[i][0][k | (1 << 0)] + dp[i - 1][1][k]) % MOD
+            elif j == 9:
+                dp[i][9][k | (1 << 9)] = (dp[i][9][k | (1 << 9)] + dp[i - 1][8][k]) % MOD
+            else:
+                dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i - 1][j - 1][k]) % MOD
+                dp[i][j][k | (1 << j)] = (dp[i][j][k | (1 << j)] + dp[i - 1][j + 1][k]) % MOD
+
+result = 0
+for j in range(10):
+    result = (result + dp[N][j][full]) % MOD
+print(result)
