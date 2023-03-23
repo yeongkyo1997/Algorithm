f444996f03261141e031b2a863dc03240f4b6afc
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-24 03:02:56 +0900

    [Silver V] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,20 +1,11 @@
 import sys
 
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
 
-def main():
-    N = int(input())
-    arr = map(int, input().split())
-    cnt = 0
-    for i in arr:
-        if isPrime(i):
-            cnt += 1
-    print(cnt)
-
-
-def isPrime(n):
-    if n == 1:
+def is_prime(n):
+    if n < 2:
         return False
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
@@ -22,5 +13,10 @@ def isPrime(n):
     return True
 
 
-if __name__ == '__main__':
-    main()
+n = int(input())
+arr = list(map(int, input().split()))
+cnt = 0
+for i in arr:
+    if is_prime(i):
+        cnt += 1
+print(cnt)
