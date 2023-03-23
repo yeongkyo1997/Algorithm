95d64cb59a7ccec5a4a14127ca06c128ae1c68f4
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-24 02:52:47 +0900

    [Silver V] Title: 단어 정렬, Time: 76 ms, Memory: 36296 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,20 +1,12 @@
 import sys
 
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
-
-def main():
-    N = int(input())
-    arr = set()
-
-    for _ in range(N):
-        arr.add(input())
-
-    arr = sorted(arr, key=lambda x: (len(x), x))
-
-    for i in arr:
-        print(i)
-
-
-if __name__ == '__main__':
-    main()
+n = int(input())
+arr = set()
+for _ in range(n):
+    arr.add(input())
+arr = list(arr)
+arr.sort(key=lambda x: (len(x), x))
+print('\n'.join(arr))
