1cc46f573f9bb4714113a80121b4fa9bb58768c2
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-24 03:13:20 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,13 +1,9 @@
 import math
 import sys
 
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
-def main():
-    a, b = map(int, input().split())
-    print(math.gcd(a, b))
-    print(math.lcm(a, b))
-
-
-if __name__ == '__main__':
-    main()
+a, b = map(int, input().split())
+print(math.gcd(a, b))
+print(math.lcm(a, b))
