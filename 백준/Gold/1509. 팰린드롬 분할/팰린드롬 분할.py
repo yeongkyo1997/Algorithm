b7f4db63c69c8982cca3184aa6e5a48f792fe95a
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-23 23:05:50 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 1756 ms, Memory: 80572 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.py
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.py
@@ -1,30 +1,104 @@
-import sys
+# #include <iostream>
+# #include <cstring>
+#
+# #define INF 1e9+7
+#
+# using namespace std;
+#
+# bool dp[2501][2501];
+# string str;
+# int N, res[2501];
+#
+# bool is_palin(int st, int ed){
+# if(ed > N) return false;
+#
+# if(str[st] == str[ed]){
+# if(dp[st+1][ed-1] || ed-st == 1){
+# return true;
+# }
+# }
+# return false;
+# }
+#
+# void fill_dptable(){
+# for(int d = 0; d < N; d++){
+# for(int i = 1; i+d <= N; i++){
+# if(d == 0){
+# dp[i][i] = true;
+# continue;
+# }
+# if(is_palin(i, i+d)){
+# dp[i][i+d] = true;
+# }
+# }
+# }
+# }
+#
+# void solve(){
+#     fill_dptable();
+#
+# res[0] = 0;
+# for (int i = 1; i <= N; i++) {
+#     res[i] = INF;
+# for (int j = 1; j <= i; j++) {
+# if (dp[j][i]) {
+# if (res[i] > res[j-1]+1) {
+# res[i] = res[j-1]+1;
+# }
+# }
+# }
+# }
+# cout << res[N];
+# }
+#
+# int main(){
+#     cin >> str;
+# N = str.size();
+# str = " " + str;
+# solve();
+#
+# return 0;
+# }
 
-sys.setrecursionlimit(10000)
-input = lambda: sys.stdin.readline().rstrip()
+# cpp to py3
 
-def main():
-    string = input()
-    N = len(string)
-    graph = [[0] * N for _ in range(N)]
-    for i in range(N):
-        graph[i][i] = 1
-    for i in range(N - 1):
-        if string[i] == string[i + 1]:
-            graph[i][i + 1] = 1
-    for i in range(N - 2, -1, -1):
-        for j in range(i + 2, N):
-            if string[i] == string[j] and graph[i + 1][j - 1]:
-                graph[i][j] = 1
-    dp = [0] * N
-    for i in range(N):
-        if graph[0][i]:
-            dp[i] = 1
-        else:
-            for j in range(i):
-                if graph[j + 1][i]:
-                    dp[i] = min(dp[i], dp[j] + 1) if dp[i] else dp[j] + 1
-    print(dp[-1])
+INF = 1e9 + 7
+str = input()
+N = len(str)
+str = " " + str
+dp = [[False for _ in range(N + 1)] for _ in range(N + 1)]
+res = [0 for _ in range(N + 1)]
 
-if __name__ == '__main__':
-    main()
+
+def is_palin(st, ed):
+    if ed > N:
+        return False
+    if str[st] == str[ed]:
+        if dp[st + 1][ed - 1] or ed - st == 1:
+            return True
+    return False
+
+
+def fill_dptable():
+    for d in range(N):
+        for i in range(1, N + 1):
+            if d == 0:
+                dp[i][i] = True
+                continue
+            if is_palin(i, i + d):
+                dp[i][i + d] = True
+
+
+def solve():
+    fill_dptable()
+    res[0] = 0
+    for i in range(1, N + 1):
+        res[i] = INF
+        for j in range(1, i + 1):
+            if dp[j][i]:
+                if res[i] > res[j - 1] + 1:
+                    res[i] = res[j - 1] + 1
+    print(res[N])
+
+
+solve()
