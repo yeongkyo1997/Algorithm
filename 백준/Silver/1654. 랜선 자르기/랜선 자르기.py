ba651fd443f864749e91261b30cb3d5ac4586875
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-24 02:55:50 +0900

    [Silver II] Title: 랜선 자르기, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -4,30 +4,19 @@ sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
 
-def main():
-    K, N = map(int, input().split())
-
-    arr = []
-    for i in range(K):
-        arr.append(int(input()))
-
-    left = 1
-    right = 2 ** 32
-
+def binary_search(left, right):
     while left <= right:
         mid = (left + right) // 2
-        total = 0
-
-        for i in arr:
-            total += i // mid
-
-        if N <= total:
+        cnt = 0
+        for i in range(K):
+            cnt += arr[i] // mid
+        if cnt >= N:
             left = mid + 1
         else:
             right = mid - 1
+    return right
 
-    print(left - 1)
 
-
-if __name__ == '__main__':
-    main()
+K, N = map(int, input().split())
+arr = [int(input()) for _ in range(K)]
+print(binary_search(1, max(arr)))
