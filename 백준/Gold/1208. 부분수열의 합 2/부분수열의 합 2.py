8a1718e464fb00bd32adacab6695adc908401ef9
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-23 22:48:55 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 940 ms, Memory: 115480 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.py
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.py
@@ -1,52 +1,33 @@
+import collections
 import sys
 
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
-def main():
-    N, S = map(int, input().split())
-    A = list(map(int, input().split()))
-    left = A[:N // 2]
-    right = A[N // 2:]
-    left_sum = []
-    right_sum = []
-    for i in range(1 << len(left)):
-        temp = 0
-        for j in range(len(left)):
-            if i & (1 << j):
-                temp += left[j]
-        left_sum.append(temp)
-    for i in range(1 << len(right)):
-        temp = 0
-        for j in range(len(right)):
-            if i & (1 << j):
-                temp += right[j]
-        right_sum.append(temp)
-    left_sum.sort()
-    right_sum.sort(reverse=True)
-    left_idx = 0
-    right_idx = 0
-    result = 0
-    while left_idx < len(left_sum) and right_idx < len(right_sum):
-        if left_sum[left_idx] + right_sum[right_idx] == S:
-            left_cnt = 1
-            right_cnt = 1
-            left_idx += 1
-            right_idx += 1
-            while left_idx < len(left_sum) and left_sum[left_idx] == left_sum[left_idx - 1]:
-                left_cnt += 1
-                left_idx += 1
-            while right_idx < len(right_sum) and right_sum[right_idx] == right_sum[right_idx - 1]:
-                right_cnt += 1
-                right_idx += 1
-            result += left_cnt * right_cnt
-        elif left_sum[left_idx] + right_sum[right_idx] < S:
-            left_idx += 1
-        else:
-            right_idx += 1
-    if S == 0:
-        result -= 1
-    print(result)
 
-if __name__ == '__main__':
-    main()
+
+N, S = map(int, input().split())
+A = [*map(int, input().split())]
+
+
+def rightSeq(mid, sum):
+    if mid == N:
+        subsum[sum] += 1
+        return
+    rightSeq(mid + 1, sum + A[mid])
+    rightSeq(mid + 1, sum)
+
+
+def leftSeq(st, sum):
+    if st == N // 2:
+        cnt[0] += subsum[S - sum]
+        return
+    leftSeq(st + 1, sum + A[st])
+    leftSeq(st + 1, sum)
+
+
+subsum = collections.defaultdict(int)
+cnt = [0]
+rightSeq(N // 2, 0)
+leftSeq(0, 0)
+print(cnt[0] - 1 if S == 0 else cnt[0])
