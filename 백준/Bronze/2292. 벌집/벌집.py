09f57ad57574cc0c174503a3f13f09998081b0a7
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-24 03:12:11 +0900

    [Bronze II] Title: 벌집, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -5,15 +5,12 @@ input = lambda: sys.stdin.readline().rstrip()
 
 def main():
     n = int(input())
-    if n == 1:
-        print(1)
-        return
-    n -= 1
-    cnt = 1
-    while n > 0:
-        n -= 6 * cnt
-        cnt += 1
-    print(cnt)
+    i = 1
+    while True:
+        if n <= 1 + 3 * (i - 1) * i:
+            print(i)
+            break
+        i += 1
 
 
 if __name__ == '__main__':
