89a696ca88463000453d686e4b5ebf22871afb3f
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-03-28 09:52:40 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14616 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    private static int N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N + 1][3];
+        int[][] cost = new int[N + 1][3];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+        }
+
+        bw.write(Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2])) + "");
+        bw.close();
+    }
+}
