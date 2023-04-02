9880f83c5d270b09425913e929dca64db5f63a2b
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-02 11:39:17 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 116 ms, Memory: 20,628 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -9,83 +9,70 @@ public class Solution {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
-    static int N, K; // 지도의 크기, 최대 공사 가능 깊이
-    static int[][] map; // 지도
-    static boolean[][] visited; // 방문 여부
-    static int[] dx = {0, 0, -1, 1}; // 상하좌우
-    static int[] dy = {-1, 1, 0, 0}; // 상하좌우
-    static int max = 0; // 최대 등산로 길이
-    static int max_height = 0; // 최대 높이
+    static int N, K;
+    static int[][] map;
+    static int[][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int maxCnt;
+    static int max;
 
     public static void main(String[] args) throws Exception {
         int T = Integer.parseInt(br.readLine());
-
         for (int t = 1; t <= T; t++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
             map = new int[N][N];
-            visited = new boolean[N][N];
-            max = 0; // 최대 등산로 길이 초기화
-            max_height = 0; // 최대 높이 초기화
+            visited = new int[N][N];
+            maxCnt = -9999999;
+            max = -9999999;
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-
                 for (int j = 0; j < N; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
-                    if (map[i][j] > max_height) max_height = map[i][j]; // 최대 높이 갱신
+                    if (max < map[i][j]) max = map[i][j];
                 }
             }
 
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (map[i][j] == max_height) { // 최대 높이에서 시작
-                        visited[i][j] = true; // 방문 처리
-                        dfs(i, j, 1, false); // 공사 여부에 따라 다음 위치로 이동
-                        visited[i][j] = false; // 방문 처리 해제
+                    if (map[i][j] == max) {
+                        visited[i][j] = 1;
+                        dfs(i, j, K, 0, 1);
+                        visited[i][j] = 0;
                     }
                 }
             }
-
-            bw.write("#" + t + " " + max + "\n");
+            bw.write("#" + t + " " + maxCnt + "\n");
         }
+        bw.flush();
         bw.close();
     }
 
-    /*
-     * x, y: 현재 위치
-     * cnt: 현재까지의 등산로 길이
-     * flag: 공사 여부
-     */
-    public static void dfs(int x, int y, int cnt, boolean flag) {
-        if (max < cnt) max = cnt; // 최대 등산로 길이 갱신
-
+    public static void dfs(int row, int col, int k, int check, int cnt) {
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-            if (visited[nx][ny]) continue;
-
-            if (map[nx][ny] < map[x][y]) {
-                visited[nx][ny] = true;
-                dfs(nx, ny, cnt + 1, flag); // 공사 여부에 따라 다음 위치로 이동
-                visited[nx][ny] = false;
-            } else {
-                if (!flag) { // 공사를 하지 않았다면
-                    for (int k = 1; k <= K; k++) { // 최대 공사 가능 깊이만큼 공사
-                        if (map[nx][ny] - k < map[x][y]) { // 공사 후 이동 가능한 경우
-                            int temp = map[nx][ny]; // 원래 높이 저장
-                            map[nx][ny] -= k; // 공사
-                            visited[nx][ny] = true; // 방문 처리
-                            dfs(nx, ny, cnt + 1, true); // 공사 여부에 따라 다음 위치로 이동
-                            visited[nx][ny] = false; // 방문 처리 해제
-                            map[nx][ny] = temp; // 원래 높이로 복구
-                        }
+            if (0 <= row + dx[i] && row + dx[i] < N && 0 <= col + dy[i] && col + dy[i] < N && map[row][col] > map[row + dx[i]][col + dy[i]] && visited[row + dx[i]][col + dy[i]] == 0) {
+                int new_cnt = cnt + 1;
+                visited[row + dx[i]][col + dy[i]] = 1;
+                dfs(row + dx[i], col + dy[i], k, check, new_cnt);
+                visited[row + dx[i]][col + dy[i]] = 0;
+            } else if (0 <= row + dx[i] && row + dx[i] < N && 0 <= col + dy[i] && col + dy[i] < N && map[row][col] <= map[row + dx[i]][col + dy[i]] && check == 0 && visited[row + dx[i]][col + dy[i]] == 0) {
+                for (int j = 1; j <= k; j++) {
+                    map[row + dx[i]][col + dy[i]] -= j;
+                    if (map[row][col] > map[row + dx[i]][col + dy[i]]) {
+                        visited[row + dx[i]][col + dy[i]] = 1;
+                        int new_cnt = cnt + 1;
+                        dfs(row + dx[i], col + dy[i], k - j, 1, new_cnt);
+                        visited[row + dx[i]][col + dy[i]] = 0;
                     }
+                    map[row + dx[i]][col + dy[i]] += j;
                 }
             }
+        }
+        if (maxCnt < cnt) {
+            maxCnt = cnt;
         }
     }
 }
