7f7a17f075954b474fcf23dffe77d01bdadb546c
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-02 10:56:20 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 641 ms, Memory: 22,464 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,78 +1,72 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+// 비트마스킹을 이용한 조합
 public class Solution {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static int N, min;
-    static int[][] map;
-    static boolean[] visited;
 
-    public static void main(String[] args) throws IOException {
+    static int N;  // 재료의 수
+    static int[][] arr; // 재료의 시너지 정보
+    static int min; // 최소 맛 차이
+
+    public static void main(String[] args) throws Exception {
         int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
+
+        for (int t = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
-            map = new int[N][N];
-            visited = new boolean[N];
-            min = Integer.MAX_VALUE;
+            arr = new int[N][N];
+            min = 10000000;
+
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
+
                 for (int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            solution2(1, 0, 0);
-            bw.write("#" + tc + " " + min + "\n");
+            solve(0, 0, 0);
+            bw.write("#" + t + " " + min + "\n");
         }
+        bw.flush();
         bw.close();
     }
 
-    // visited 사용
-    public static void solution(int start, int depth) {
-        if (depth == N / 2) {
-            int s1 = 0, s2 = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (visited[i] && visited[j]) {
-                        s1 += map[i][j];
-                    } else if (!visited[i] && !visited[j]) {
-                        s2 += map[i][j];
-                    }
+    /*
+     * 맛 차이의 절댓값을 구하는 함수
+     * a: 고른 재료
+     * b: 고르지 않은 재료
+     */
+    static void taste(int a, int b) {
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if ((a & (1 << i)) != 0 && (a & (1 << j)) != 0) { // 고른 걸 더해주고
+                    result += arr[i][j];
+                } else if ((b & (1 << i)) != 0 && (b & (1 << j)) != 0) { // 안 고른 걸 빼준다.
+                    result -= arr[i][j];
                 }
             }
-            min = Math.min(min, Math.abs(s1 - s2));
-            return;
         }
-        for (int i = start; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                solution(i + 1, depth + 1);
-                visited[i] = false;
-            }
-        }
+        min = Math.min(min, Math.abs(result));
     }
 
-    // solution2 비트마스킹 사용
-    public static void solution2(int start, int depth, int flag) {
-        if (depth == N / 2) {
-            int s1 = 0, s2 = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if ((flag & 1 << i) != 0 && (flag & 1 << j) != 0) {
-                        s1 += map[i][j];
-                    } else if ((flag & 1 << i) == 0 && (flag & 1 << j) == 0) {
-                        s2 += map[i][j];
-                    }
-                }
-            }
-            min = Math.min(min, Math.abs(s1 - s2));
+    /*
+     * cur: 현재 재료의 번호
+     * a: 고른 재료
+     * b: 고르지 않은 재료
+     */
+    static void solve(int cur, int a, int b) {
+        if (cur == N) { // 모든 재료를 고른 경우
+            taste(a, b);
             return;
         }
-        for (int i = start; i < N; i++) {
-            if ((flag & 1 << i) == 0) {
-                solution2(i + 1, depth + 1, flag | 1 << i);
-            }
-        }
+        solve(cur + 1, a | (1 << cur), b); // 선택하는 경우
+        solve(cur + 1, a, b | (1 << cur)); // 선택하지 않는 경우
     }
 }
