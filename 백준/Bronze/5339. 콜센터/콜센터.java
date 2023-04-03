26ce6be8c50db67c076ef4b6f28f7063eb6b0370
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-03 17:37:19 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 14004 KB -BaekjoonHub

--- 백준/Bronze/5339. 콜센터/콜센터.java
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -1,25 +1,22 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
 
-	public static void main(String[] args) throws IOException {
-		bw.write("     /~\\\n");
-		bw.write("    ( oo|\n");
-		bw.write("    _\\=/_\n");
-		bw.write("   /  _  \\\n");
-		bw.write("  //|/.\\|\\\\\n");
-		bw.write(" ||  \\ /  ||\n");
-		bw.write("============\n");
-		bw.write("|          |\n");
-		bw.write("|          |\n");
-		bw.write("|          |\n");
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        bw.write("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+        bw.close();
+    }
 }
