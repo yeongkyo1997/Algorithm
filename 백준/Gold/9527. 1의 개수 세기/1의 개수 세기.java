6c527c5534fa442ed1c92d074b617beba725f613
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-06 07:54:59 +0900

    [Gold II] Title: 1의 개수 세기, Time: 128 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    static long[] pow = new long[55];
+    static long A, B;
+
+    static long getOne(long x) {
+        long ret = x & 1;
+
+        for (int i = 54; i > 0; i--) {
+            if ((x & (1L << i)) != 0) {
+                ret += pow[i - 1] + (x - (1L << i) + 1);
+                x -= 1L << i;
+            }
+        }
+
+        return ret;
+    }
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        A = Long.parseLong(st.nextToken());
+        B = Long.parseLong(st.nextToken());
+
+        pow[0] = 1;
+        IntStream.range(1, 55).forEach(i -> pow[i] = 2 * pow[i - 1] + (1L << i));
+
+        bw.write(String.valueOf(getOne(B) - getOne(A - 1)));
+        bw.close();
+    }
+}
