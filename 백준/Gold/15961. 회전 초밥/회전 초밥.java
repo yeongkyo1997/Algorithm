a00758bebf79e940d0cb779fd3815d15f7035cf0
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-05 12:21:05 +0900

    [Gold IV] Title: 회전 초밥, Time: 580 ms, Memory: 171564 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -9,9 +9,9 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
-    static int N, D, K, C;
-    static int[] arr;
-    static int[] check;
+    static int N, D, K, C; // 접시의 수, 초밥의 가짓수, 연속해서 먹는 접시의 수, 쿠폰 번호
+    static int[] arr; // 초밥의 종류
+    static int[] check; // 초밥의 종류를 체크
 
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
@@ -28,29 +28,29 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
 
 
-        int cnt = 0;
-        int max;
-        for (int i = 0; i < K; i++) {
-            if (check[arr[i]] == 0) cnt++;
-            check[arr[i]]++;
+        int cnt = 0; // 먹은 초밥의 가짓수
+        int max; // 먹은 초밥의 가짓수의 최대값
+
+        for (int i = 0; i < K; i++) { // 처음 K개의 초밥을 먹음
+            if (check[arr[i]] == 0) cnt++; // 먹은 초밥의 가짓수를 체크
+            check[arr[i]]++; // 먹은 초밥의 종류를 체크
         }
 
-        if (check[C] == 0) max = cnt + 1;
-        else max = cnt;
+        if (check[C] == 0) max = cnt + 1; // 쿠폰 번호의 초밥을 먹지 않았을 경우
+        else max = cnt; // 쿠폰 번호의 초밥을 먹었을 경우
 
 
         for (int i = 1; i < N; i++) {
-            check[arr[i - 1]]--;
-            if (check[arr[i - 1]] == 0) cnt--;
+            check[arr[i - 1]]--; // i번째 초밥을 먹기 전에 먹었던 초밥을 체크 해제
+            if (check[arr[i - 1]] == 0) cnt--; // 먹은 초밥의 가짓수를 체크
 
 
-            if (check[arr[(i + K - 1) % N]] == 0) {
-                cnt++;
-            }
-            check[arr[(i + K - 1) % N]]++;
+            if (check[arr[(i + K - 1) % N]] == 0) cnt++; // i번째 초밥을 먹은 후에 먹은 초밥의 가짓수를 체크
 
-            if (check[C] == 0) max = Math.max(max, cnt + 1);
-            else max = Math.max(max, cnt);
+            check[arr[(i + K - 1) % N]]++;  // i번째 초밥을 먹은 후에 먹은 초밥의 종류를 체크
+
+            if (check[C] == 0) max = Math.max(max, cnt + 1); // 쿠폰 번호의 초밥을 먹지 않았을 경우
+            else max = Math.max(max, cnt); // 쿠폰 번호의 초밥을 먹었을 경우
 
         }
 
