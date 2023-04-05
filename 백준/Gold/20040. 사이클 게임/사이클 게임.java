58215deb91ac19d5fc1517cd5c492ac84b71ad0f
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-05 22:34:21 +0900

    [Gold IV] Title: 사이클 게임, Time: 748 ms, Memory: 219040 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int n, m;
+    static int[] parent;
+    static int finish = 0;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        parent = new int[n];
+
+        IntStream.range(0, n).forEach(i -> parent[i] = i);
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            union(a, b, i + 1);
+        }
+
+        bw.write(String.valueOf(finish));
+        bw.close();
+    }
+
+    static int find(int a) {
+        if (a == parent[a]) return a;
+
+        else {
+            int y = find(parent[a]);
+            parent[a] = y;
+            return y;
+        }
+    }
+
+    static void union(int a, int b, int indx) {
+        a = find(a);
+        b = find(b);
+        if (a != b) parent[Math.max(a, b)] = Math.min(a, b);
+        else if (finish == 0) finish = indx;
+    }
+}
