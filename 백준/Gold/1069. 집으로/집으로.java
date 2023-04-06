314ba92852ed4e0ab370a7240d5d034bc5e4ef2f
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-06 09:57:46 +0900

    [Gold III] Title: 집으로, Time: 132 ms, Memory: 14664 KB -BaekjoonHub

---
+++ 백준/Gold/1069. 집으로/집으로.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    static int x, y, d, t;
+    static double dist;
+    static int jump;
+    static double remain;
+    static double result;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+        dist = Math.sqrt(x * x + y * y);
+        jump = (int) (dist / d);
+        remain = dist - jump * d;
+        result = Math.min(dist, remain + jump * t);
+        result = Math.min(result, (jump + 1) * d - dist + (jump + 1) * t);
+        if (jump > 0) result = Math.min(result, (jump + 1) * t);
+        else if (dist < d) result = Math.min(result, t * 2.0);
+
+        bw.write(String.format("%.9f", result));
+        bw.flush();
+    }
+}
