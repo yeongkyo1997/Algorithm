6948e07a35e7156c2569a8e5357800acc518804b
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-06 10:08:42 +0900

    [Gold III] Title: 방 번호, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Gold/1082. 방 번호/방 번호.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    static int[] price = new int[10];
+    static int n, m;
+    static int[] r = new int[51];
+    static int minPrice = 50;
+
+    static int minIndex;
+    static int minPrice2 = 50;
+    static int minIndex2;
+    static int idx;
+
+    public static void main(String[] args) throws Exception {
+        n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            price[i] = Integer.parseInt(st.nextToken());
+
+            if (i > 0 && price[i] <= minPrice) {
+                minPrice = price[i];
+                minIndex = i;
+            }
+
+            if (price[i] <= minPrice2) {
+                minPrice2 = price[i];
+                minIndex2 = i;
+            }
+        }
+
+        m = Integer.parseInt(br.readLine());
+
+        if (m < minPrice) {
+            bw.write("0");
+            bw.flush();
+            return;
+        }
+
+        r[0] = minIndex;
+        m -= minPrice;
+        idx++;
+
+        while (m >= minPrice2) {
+            m -= minPrice2;
+            r[idx] = minIndex2;
+            idx++;
+        }
+
+        for (int i = 0; i < idx; i++) {
+            for (int j = n - 1; j > r[i]; j--) {
+                if (m + price[r[i]] - price[j] >= 0) {
+                    m += price[r[i]];
+                    m -= price[j];
+                    r[i] = j;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < idx; i++) bw.write(String.valueOf(r[i]));
+        bw.close();
+    }
+}
