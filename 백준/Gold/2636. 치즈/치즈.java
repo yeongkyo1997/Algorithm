451c2e9132c60a8cfb92def7340915dd9da603dc
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-06 10:50:17 +0900

    [Gold IV] Title: 치즈, Time: 188 ms, Memory: 20816 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -14,12 +14,12 @@ public class Main {
     static int[] row = {-1, 1, 0, 0};
     static int[] col = {0, 0, -1, 1};
 
-    static void airOut(int[][] ac, int sero, int garo) {
-        ac[sero][garo] = 1;
+    static void airOut(int[][] ac, int r, int c) {
+        ac[r][c] = 1;
 
         for (int i = 0; i < 4; i++) {
-            int ny = sero + row[i];
-            int nx = garo + col[i];
+            int ny = r + row[i];
+            int nx = c + col[i];
 
             if (ny >= 0 && ny < n && nx >= 0 && nx < m) {
                 if (map[ny][nx] == 0 && ac[ny][nx] == 0) airOut(ac, ny, nx);
@@ -39,32 +39,32 @@ public class Main {
         }
 
         int time = 0;
-        int pre_rcn = 0;
+        int preCnt = 0;
 
         while (true) {
 
-            int[][] air_check = new int[101][101];
+            int[][] airCheck = new int[101][101];
 
 
-            airOut(air_check, 0, 0);
+            airOut(airCheck, 0, 0);
 
 
-            int[][] remove_ch = new int[10000][2];
-            int remove_ch_num = 0;
+            int[][] removeCh = new int[10000][2];
+            int removeChNum = 0;
 
 
             for (int i = 1; i < n - 1; i++) {
                 for (int j = 1; j < m - 1; j++) {
                     if (map[i][j] == 1) {
                         for (int d = 0; d < 4; d++) {
-                            int cy = i + row[d];
-                            int cx = j + col[d];
+                            int dy = i + row[d];
+                            int dx = j + col[d];
 
-                            if (cy >= 0 && cy < n && cx >= 0 && cx < m) {
-                                if (air_check[cy][cx] == 1) {
+                            if (dy >= 0 && dy < n && dx >= 0 && dx < m) {
+                                if (airCheck[dy][dx] == 1) {
 
-                                    remove_ch[remove_ch_num][0] = i;
-                                    remove_ch[remove_ch_num++][1] = j;
+                                    removeCh[removeChNum][0] = i;
+                                    removeCh[removeChNum++][1] = j;
                                     break;
                                 }
                             }
@@ -73,16 +73,16 @@ public class Main {
                 }
             }
 
-            if (remove_ch_num == 0) break;
-            pre_rcn = remove_ch_num;
+            if (removeChNum == 0) break;
+            preCnt = removeChNum;
 
 
-            for (int r = 0; r < remove_ch_num; r++)
-                map[remove_ch[r][0]][remove_ch[r][1]] = 0;
+            for (int r = 0; r < removeChNum; r++)
+                map[removeCh[r][0]][removeCh[r][1]] = 0;
 
             time++;
         }
-        bw.write(time + "\n" + pre_rcn);
+        bw.write(time + "\n" + preCnt);
         bw.close();
     }
 }
