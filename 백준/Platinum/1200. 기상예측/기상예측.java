06fbc5f6e8ec45c38cbb857c6151ed7ca8f94059
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-06 11:35:11 +0900

    [Platinum III] Title: 기상예측, Time: 432 ms, Memory: 19868 KB -BaekjoonHub

--- 백준/Platinum/1200. 기상예측/기상예측.java
+++ 백준/Platinum/1200. 기상예측/기상예측.java
@@ -16,38 +16,40 @@ public class Main {
 
     static int solve(int bit) {
         for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= m; j++) {
+            for (int j = 1; j <= m; j++)
                 b[i][j] = 0;
-            }
         }
+
         int start = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= m; j++) {
+            for (int j = 1; j <= m; j++)
                 b[start][j] += a[i][j];
-            }
             if (((bit >> i) & 1) == 1) start++;
         }
+
         for (int i = 0; i <= start; i++) {
-            for (int j = 1; j <= m; j++) {
+            for (int j = 1; j <= m; j++)
                 b[i][j] += b[i][j - 1];
-            }
+
         }
         for (int i = 1; i <= m; i++) {
             for (int j = i; j <= m; j++) {
                 int ret = 0;
-                for (int k = 0; k <= start; k++) {
+
+                for (int k = 0; k <= start; k++)
                     ret = Math.max(ret, b[k][j] - b[k][i - 1]);
-                }
+
                 cst[i][j] = ret;
             }
         }
+
         for (int i = 1; i <= m; i++) {
             for (int j = 0; j <= s; j++) {
                 dp[i][j] = cst[1][i];
+
                 if (j != 0) {
-                    for (int k = 0; k < i; k++) {
+                    for (int k = 0; k < i; k++)
                         dp[i][j] = Math.min(dp[i][j], Math.max(dp[k][j - 1], cst[k + 1][i]));
-                    }
                 }
             }
         }
@@ -60,23 +62,24 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
         s = Integer.parseInt(st.nextToken());
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= m; j++) {
+            for (int j = 1; j <= m; j++)
                 a[i][j] = Integer.parseInt(st.nextToken());
-            }
         }
-        int ret = 1000000000;
+
+        int result = 1000000000;
         for (int i = 0; i < (1 << (n - 1)); i++) {
             int c = 0;
-            for (int j = 0; j < n - 1; j++) {
+
+            for (int j = 0; j < n - 1; j++)
                 if (((i >> j) & 1) == 1) c++;
-            }
-            if (c == r) {
-                ret = Math.min(ret, solve(i));
-            }
+
+            if (c == r) result = Math.min(result, solve(i));
         }
-        bw.write(ret + "\n");
+
+        bw.write(result + "\n");
         bw.close();
     }
 }
