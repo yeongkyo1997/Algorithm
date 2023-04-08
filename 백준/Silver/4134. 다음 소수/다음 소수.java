d92803ac5baa488aa3f8c71e88eb4d64aee2750d
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-08 10:31:32 +0900

    [Silver IV] Title: 다음 소수, Time: 476 ms, Memory: 15252 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    static boolean isPrime(long n) {
+        if (n < 2) return false;
+
+        for (long i = 2; i * i < n + 1; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            long n = Long.parseLong(br.readLine());
+
+            while (true) {
+                if (isPrime(n)) {
+                    bw.write(n + "\n");
+                    bw.flush();
+                    break;
+                }
+                n++;
+            }
+        }
+        bw.close();
+    }
+}
