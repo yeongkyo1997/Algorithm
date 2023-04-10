42eb162a492a20483b676b24c68cc43bf68ffff5
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-10 09:06:10 +0900

    commit

---
+++ Algorithm/src/Main_10093_숫자.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main_10093_숫자 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        if (a > b) {
+            long temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (a == b) bw.write("0\n");
+        else {
+            bw.write((b - a - 1) + "\n");
+            for (long i = a + 1; i < b; i++)
+                bw.write(i + " ");
+        }
+        bw.close();
+    }
+}
