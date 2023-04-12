e08ded7f8f64ea42a57e6580345ac655f3b630fe
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-12 15:14:27 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,9 +1,22 @@
-select user.user_id, user.nickname, concat_ws(" ", user.city, user.street_address1, user.street_address2) as 전체주소, CONCAT(LEFT(user.tlno, 3), '-', MID(user.tlno, 4, 4), '-', RIGHT(user.tlno, 4))
-from used_goods_user user
-where user.user_id in (
-    select writer_id
-    from used_goods_board
-    group by 1
-    having count(writer_id) >= 3
-)
-order by 1 desc
+SELECT 
+    user.user_id,
+    user.nickname,
+    CONCAT_WS(' ',
+            user.city,
+            user.street_address1,
+            user.street_address2) AS 전체주소,
+    CONCAT(LEFT(user.tlno, 3),
+            '-',
+            MID(user.tlno, 4, 4),
+            '-',
+            RIGHT(user.tlno, 4))
+FROM
+    used_goods_user user
+WHERE
+    user.user_id IN (SELECT 
+            writer_id
+        FROM
+            used_goods_board
+        GROUP BY 1
+        HAVING COUNT(writer_id) >= 3)
+ORDER BY 1 DESC
