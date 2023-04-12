53c7c04ac9d1e7110a3677939d1ee0a718de77b6
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-12 15:07:02 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,9 +1,18 @@
-select a.apnt_no, p.pt_name, a.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
-from appointment a join patient p
-on a.pt_no = p.pt_no
-join doctor d
-on a.mddr_id = d.dr_id
-where a.apnt_cncl_yn = "N" and 
-date_format(apnt_ymd, "%Y-%m-%d") = "2022-04-13"
-and a.mcdp_cd = "CS"
-order by a.apnt_ymd
+SELECT 
+    a.apnt_no,
+    p.pt_name,
+    a.pt_no,
+    a.mcdp_cd,
+    d.dr_name,
+    a.apnt_ymd
+FROM
+    appointment a
+        JOIN
+    patient p ON a.pt_no = p.pt_no
+        JOIN
+    doctor d ON a.mddr_id = d.dr_id
+WHERE
+    a.apnt_cncl_yn = 'N'
+        AND DATE_FORMAT(apnt_ymd, '%Y-%m-%d') = '2022-04-13'
+        AND a.mcdp_cd = 'CS'
+ORDER BY a.apnt_ymd
