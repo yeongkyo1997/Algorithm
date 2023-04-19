f5ff89d4b64fe4f36779ee2c8113bfa350d63ca8
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-19 22:51:12 +0900

    [Ruby V] Title: 친구, Time: 440 ms, Memory: 38052 KB -BaekjoonHub

--- 백준/Ruby/10075. 친구/친구.java
+++ 백준/Ruby/10075. 친구/친구.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 import java.util.stream.IntStream;
 
@@ -7,45 +10,36 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
-    static int N;
+    static int n;
     static int[] trust, host, protocol;
-    static int[][] dp;
 
-    public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        trust = new int[N];
-        host = new int[N];
-        protocol = new int[N];
-        dp = new int[N][2];
+    public static void main(String[] args) throws Exception {
+        n = Integer.parseInt(br.readLine());
+        trust = new int[100005];
+        host = new int[100005];
+        protocol = new int[100005];
 
         st = new StringTokenizer(br.readLine());
-        IntStream.range(0, N).forEach(i -> trust[i] = Integer.parseInt(st.nextToken()));
+        IntStream.range(0, n).forEach(i -> trust[i] = Integer.parseInt(st.nextToken()));
 
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i <= n - 1; i++) {
             host[i] = Integer.parseInt(st.nextToken());
             protocol[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < N; i++) {
-            dp[i][0] = trust[i];
-        }
-
-        for (int i = N - 1; i > 0; i--) {
+        int result = 0;
+        for (int i = n - 1; i >= 1; i--) {
             if (protocol[i] == 0) {
-                dp[host[i]][0] += dp[i][1];
-                dp[host[i]][1] += Math.max(dp[i][0], dp[i][1]);
-            } else if (protocol[i] == 1) {
-                dp[host[i]][0] = Math.max(dp[host[i]][0] + Math.max(dp[i][0], dp[i][1]), dp[host[i]][1] + dp[i][0]);
-                dp[host[i]][1] += dp[i][1];
-            } else {
-                dp[host[i]][0] = Math.max(dp[host[i]][0] + dp[i][1], dp[host[i]][1] + dp[i][0]);
-                dp[host[i]][1] += dp[i][1];
+                result += trust[i];
+                trust[host[i]] = Math.max(0, trust[host[i]] - trust[i]);
             }
+
+            if (protocol[i] == 1) trust[host[i]] += trust[i];
+            if (protocol[i] == 2) trust[host[i]] = Math.max(trust[host[i]], trust[i]);
         }
 
-        bw.write(Math.max(dp[0][0], dp[0][1]) + "\n");
+        bw.write(String.valueOf(trust[0] + result));
         bw.close();
     }
 }
