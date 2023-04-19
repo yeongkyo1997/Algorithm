79a542ac0fec1434744eedf866b8430f5f1cf3a9
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-19 09:06:53 +0900

    commit

---
+++ Algorithm/src/Main_1000.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main_1000 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
