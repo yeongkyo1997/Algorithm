3d7109414c69afd0c6afacc27b24726c13d85c08
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-19 09:07:22 +0900

    commit

--- Algorithm/src/Main_1002_터렛.java
+++ Algorithm/src/Main_1002_터렛.java
@@ -1,28 +1,9 @@
-//import math
-//
-//n = int(input())
-//
-//        for _ in range(n):
-//        x1, y1, r1, x2, y2, r2 = map(int, input().split())
-//        distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-//
-//        if distance == 0 and r1 == r2:
-//        print(-1)
-//        elif abs(r1 - r2) == distance or r1 + r2 == distance:
-//        print(1)
-//        elif abs(r1 - r2) < distance < (r1 + r2):
-//        print(2)
-//        else:
-//        print(0)
-
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-//py3 to java
 public class Main_1002_터렛 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
