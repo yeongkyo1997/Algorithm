b608b369a7f18e48fc5d992f6c975b6035bb795d
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-20 17:37:31 +0900

    commit

--- Algorithm/src/dataStructure/SegmentTree.java
+++ Algorithm/src/dataStructure/SegmentTree.java
@@ -1,61 +1,29 @@
 package dataStructure;
 
 public class SegmentTree {
-    static long[] tree;
-    static long[] data;
+    private long[] tree;
 
-
-//    long init(long[] arr, int node, int start, int end) {
-//        int mid = (start + end) / 2;
-//        if (start == end) return tree[node] = arr[start];
-//        else return tree[node] = init(arr, node * 2, start, mid) + init(arr, node, mid + 1, end);
-//    }
-
-//    long sum(int node, int start, int end, int left, int right) {
-//        int mid = (start + end) / 2;
-//        if (end < left || right < start) return 0;
-//        else if (left <= start && end <= right) return tree[node];
-//        else return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid + 1, end, left, right);
-//
-//    }
-
-    static long init(long[] arr, int node, int start, int end) {
-        int mid = (start + end) / 2;
-
-        if (start == end) return tree[node] = arr[start];
-
-        else return tree[node] = init(arr, node, start, mid) + init(arr, node, mid + 1, end);
+    public SegmentTree(int n) {
+        double treeHeight = Math.ceil(Math.log(n) / Math.log(2)) + 1;
+        long treeNodeCnt = Math.round(Math.pow(2, treeHeight));
+        tree = new long[Math.toIntExact(treeNodeCnt)];
     }
 
-    static long sum(int node, int start, int end, int left, int right) {
-        int mid = (start + end) / 2;
+    long init(long[] arr, int node, int start, int end) {
+        if (start == end) {
+            return tree[node] = arr[start];
+        } else {
+            int mid = (start + end) / 2;
+            return tree[node] = init(arr, node * 2, start, mid) + init(arr, node * 2 + 1, mid, end);
+        }
+    }
+
+    long sum(int node, int start, int end, int left, int right) {
         if (end < left || right < start) return 0;
-        else if (left <= start && end < right) return tree[node];
-        else return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid + 1, end, left, right);
-    }
-
-    //        void update(int node, int start, int end, int idx, int diff) {
-//        if (idx < start || end < idx) return;
-//        else {
-//            int mid = (start + end) / 2;
-//            tree[node] = tree[node] + diff;
-//
-//            if (start != end) {
-//                update(node * 2, start, mid, idx, diff);
-//                update(node * 2 + 1, mid + 1, end, idx, diff);
-//            }
-//        }
-//    }
-    static void update(int node, int start, int end, int idx, int diff) {
-        if (idx < start || end < idx) return;
+        else if (left <= start && end <= right) return tree[node];
         else {
             int mid = (start + end) / 2;
-            tree[node] = tree[node] + diff;
-
-            if (start != end) {
-                update(node * 2, start, mid, idx, diff);
-                update(node * 2 + 1, mid + 1, end, idx, diff);
-            }
+            return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid, end, left, right);
         }
     }
 }
