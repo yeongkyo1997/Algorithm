6cce7551ceebd724bca08a82f7293b72e4efae5b
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-20 17:55:48 +0900

    commit

--- Algorithm/src/B형_준비/Solution_리스트_복사.java
+++ Algorithm/src/B형_준비/Solution_리스트_복사.java
@@ -2,6 +2,7 @@ package B형_준비;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -111,7 +112,7 @@ public class Solution_리스트_복사 {
     public static void main(String[] args) throws Exception {
         int TC, MARK;
 
-        //System.setIn(new java.io.FileInputStream("res/sample_input.txt"));
+        System.setIn(new java.io.FileInputStream("C:\\SSAFY\\Algorithm\\Algorithm\\src\\B형_준비\\sample_input.txt"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -135,18 +136,27 @@ public class Solution_리스트_복사 {
         }
 
         public void makeList(char[] mName, int mLength, int[] mListValue) {
-            map.put(mName.toString(), new User(new int[mLength], new HashMap<>()));
+            map.put(Arrays.toString(mName), new User(mListValue, new HashMap<>()));
         }
 
         public void copyList(char[] mDest, char[] mSrc, boolean mCopy) {
-
+            if (mCopy) {
+                map.put(Arrays.toString(mDest), map.get(Arrays.toString(mSrc)));
+            } else {
+                map.put(Arrays.toString(mDest), new User(map.get(Arrays.toString(mSrc)).ref, new HashMap<>()));
+            }
         }
 
         public void updateElement(char[] mName, int mIndex, int mValue) {
+            User user = map.get(Arrays.toString(mName));
+            user.map.put(mIndex, mValue);
         }
 
         public int element(char[] mName, int mIndex) {
-            return 0;
+            User user = map.get(Arrays.toString(mName));
+
+            if (user.map.containsKey(mIndex)) return user.map.get(mIndex);
+            else return user.ref[mIndex];
         }
 
         static class User {
