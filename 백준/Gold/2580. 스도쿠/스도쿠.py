2ba2e21ba429f80d6ed4aca82b9c831607c27b11
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-24 15:09:20 +0900

    [Gold IV] Title: 스도쿠, Time: 2300 ms, Memory: 133504 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -0,0 +1,44 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+arr = [list(map(int, input().split())) for _ in range(9)]
+point = []
+
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] == 0:
+            point.append((i, j))
+
+
+def check(p, num):
+    for i in range(0, 9):
+        if arr[p[0]][i] == num:
+            return False
+        if arr[i][p[1]] == num:
+            return False
+
+    for i in range(p[0] // 3 * 3, p[0] // 3 * 3 + 3):
+        for j in range(p[1] // 3 * 3, p[1] // 3 * 3 + 3):
+            if arr[i][j] == num:
+                return False
+
+    return True
+
+
+def solve(depth):
+    if depth == len(point):
+        for i in range(9):
+            for j in range(9):
+                print(arr[i][j], end=' ')
+            print()
+        exit(0)
+
+    for i in range(1, 10):
+        if check(point[depth], i):
+            arr[point[depth][0]][point[depth][1]] = i
+            solve(depth + 1)
+            arr[point[depth][0]][point[depth][1]] = 0
+
+
+solve(0)
