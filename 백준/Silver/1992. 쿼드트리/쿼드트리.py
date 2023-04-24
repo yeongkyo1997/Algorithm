be6dfe9e16bbf367cd133713d6aa740744bc5c9f
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-24 14:30:07 +0900

    [Silver I] Title: 쿼드트리, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -3,42 +3,33 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
+
 arr = [list(map(int, list(input()))) for _ in range(N)]
 
 
-def main():
-    global arr
-    quardTree(0, 0, N)
+def check(n, x, y):
+    val = arr[x][y]
 
-
-def check(x, y, N):
-    global arr
-    cur = arr[x][y]
-
-    for i in range(x, x + N):
-        for j in range(y, y + N):
-            if cur != arr[i][j]:
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if val != arr[i][j]:
                 return False
     return True
 
 
-def quardTree(x, y, N):
-    global arr
-    if check(x, y, N):
-        print(arr[x][y], end='')
-        return
-    if N == 1:
+def solve(n, x, y):
+    if check(n, x, y):
         print(arr[x][y], end='')
         return
 
+    mid = n // 2
+
     print('(', end='')
-    mid = N // 2
-    quardTree(x, y, mid)
-    quardTree(x, y + mid, mid)
-    quardTree(x + mid, y, mid)
-    quardTree(x + mid, y + mid, mid)
+    solve(mid, x, y)
+    solve(mid, x, y + mid)
+    solve(mid, x + mid, y)
+    solve(mid, x + mid, y + mid)
     print(')', end='')
 
 
-if __name__ == '__main__':
-    main()
+solve(N, 0, 0)
