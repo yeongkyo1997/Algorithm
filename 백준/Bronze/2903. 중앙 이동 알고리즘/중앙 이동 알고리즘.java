8e297fee447f4f9224149d4914572b15bf244761
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-25 08:53:49 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 152 ms, Memory: 15764 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        int n = 2;
+        int a = 1;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            n += a;
+            a *= 2;
+        }
+
+        bw.write((int) Math.pow(n, 2) + "\n");
+        bw.close();
+    }
+}
