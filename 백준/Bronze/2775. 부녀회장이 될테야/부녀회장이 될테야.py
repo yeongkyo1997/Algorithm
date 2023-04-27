6af7c81b0cfa79b43cac500d188805ec78ccfc76
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-27 09:27:26 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -3,17 +3,31 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 
-def main():
-    T = int(input())
-    for i in range(T):
-        k = int(input())
-        n = int(input())
-        arr = [i for i in range(1, n + 1)]
-        for j in range(k):
-            for l in range(1, n):
-                arr[l] += arr[l - 1]
-        print(arr[-1])
+def solve(K, N):
+    arr = [0] * (N + 1)
+    SUM = 0
+
+    if K == 1:
+        SUM += sum(range(1, N + 1))
+    else:
+        for j in range(0, N + 1):
+            arr[N - j] = 1 + j
+
+        for j in range(2, K):
+            for k in range(1, N):
+                arr[N - k] += arr[N - k + 1]
+
+        for j in range(0, N + 1):
+            SUM += arr[j] * j
+
+    return SUM
 
 
 if __name__ == '__main__':
-    main()
+    cnt = int(input())
+
+    for i in range(cnt):
+        K = int(input())
+        N = int(input())
+
+        print(solve(K, N))
