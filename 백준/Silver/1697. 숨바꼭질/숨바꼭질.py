737c6a891acd19690dd29a56ebf38e1a6c8f9ebb
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-27 10:29:21 +0900

    [Silver I] Title: 숨바꼭질, Time: 484 ms, Memory: 34084 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,34 +1,32 @@
-import collections
 import sys
 
-sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
 
-def main():
-    N, K = map(int, input().split())
-
-    visited = [False] * 100001
-
-    queue = collections.deque()
-    queue.append((N, 0))
-
-    while queue:
-        cur, depth = queue.popleft()
-        if cur == K:
-            print(depth)
-            break
-
-        if cur + 1 <= 100000 and not visited[cur + 1]:
-            visited[cur + 1] = True
-            queue.append((cur + 1, depth + 1))
-        if 0 <= cur - 1 and not visited[cur - 1]:
-            visited[cur - 1] = True
-            queue.append((cur - 1, depth + 1))
-        if 0 <= cur * 2 <= 100000 and not visited[cur * 2]:
-            visited[cur * 2] = True
-            queue.append((cur * 2, depth + 1))
+def check(n):
+    if n < 0 or n > 100000 or visited[n]:
+        return False
+    return True
 
 
-if __name__ == '__main__':
-    main()
+def bfs():
+    while q:
+        data, depth = q.pop(0)
+        if data == K:
+            return depth
+        if check(data - 1):
+            visited[data - 1] = True
+            q.append([data - 1, depth + 1])
+        if check(data + 1):
+            visited[data + 1] = True
+            q.append([data + 1, depth + 1])
+        if check(data * 2):
+            visited[data * 2] = True
+            q.append([data * 2, depth + 1])
+
+
+N, K = map(int, input().split())
+visited = [False] * 100001
+q = [[N, 0]]
+visited[N] = True
+print(bfs())
