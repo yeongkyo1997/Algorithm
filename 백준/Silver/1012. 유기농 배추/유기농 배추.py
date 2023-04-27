2aa330758e8f7733e4bf9bb151bd2bb896c896f4
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-27 09:37:58 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 31620 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,38 +1,42 @@
 import sys
 
-sys.setrecursionlimit(10000)
-input = sys.stdin.readline
+sys.setrecursionlimit(100000)
+input = lambda: sys.stdin.readline().rstrip()
 
-result = []
-
-
-def dfs(x, y):
-    dx = [0, 0, 1, -1]
-    dy = [-1, 1, 0, 0]
-    arr[x][y] = 0
-
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-
-        if 0 <= nx < row and 0 <= ny < col and arr[nx][ny]:
-            dfs(nx, ny)
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
 
-for t in range(int(input())):
-    row, col, num = map(int, input().split())
-    arr = [[0] * col for _ in range(row)]
-    for i in range(num):
+def dfs(y, x):
+    if check[y][x]:
+        return False
+    check[y][x] = True
+
+    for i in range(0, 4):
+        next_x = x + dx[i]
+        next_y = y + dy[i]
+
+        if 0 <= next_x < m and 0 <= next_y < n and arr[next_y][next_x]:
+            dfs(next_y, next_x)
+
+    return True
+
+
+for i in range(int(input())):
+    m, n, cabbage = map(int, input().split())
+    arr = [[0] * m for _ in range(n)]
+    check = [[0] * m for _ in range(n)]
+
+    for j in range(0, cabbage):
         x, y = map(int, input().split())
-        arr[x][y] = 1
+        arr[y][x] = 1
 
-    res = 0
+    result = 0
 
-    for i in range(row):
-        for j in range(col):
-            if arr[i][j]:
-                dfs(i, j)
-                res += 1
-    result.append(res)
+    for j in range(0, n):
+        for k in range(0, m):
+            if arr[j][k] and not check[j][k]:
+                if dfs(j, k):
+                    result += 1
 
-for i in result:
-    print(i)
+    print(result)
