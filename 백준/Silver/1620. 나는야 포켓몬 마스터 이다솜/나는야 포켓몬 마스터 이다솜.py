f9d083e14aa261232d01f5a31ee638e4bc0e9b61
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-27 10:13:58 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 240 ms, Memory: 47056 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -2,18 +2,18 @@ import sys
 
 input = lambda: sys.stdin.readline().rstrip()
 
+n, m = map(int, input().split())
+pocketMon = {}
+strArr = [''] * (n + 1)
 
-# BOJ 1620 나는야 포켓몬 마스터 이다솜
-def main():
-    n, m = map(int, input().split())
-    dic = {}
-    for i in range(1, n + 1):
-        name = input()
-        dic[name] = i
-        dic[str(i)] = name
-    for _ in range(m):
-        print(dic[input()])
+for i in range(1, n + 1):
+    name = input()
+    pocketMon[name] = i
+    strArr[i] = name
 
-
-if __name__ == '__main__':
-    main()
+for i in range(m):
+    string = input()
+    if string in pocketMon:
+        print(pocketMon[string])
+    else:
+        print(strArr[int(string)])
