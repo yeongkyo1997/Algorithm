07451b34466fa657476a230a8b17c9635453ce53
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-30 12:51:32 +0900

    [Silver II] Title: N과 M (9), Time: 120 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,27 +1,15 @@
+import itertools
+
 import sys
 
-input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+input = lambda: sys.stdin.readline().rstrip()
 
-n, m = map(int, input().split())
-arr = sorted(map(int, input().split()))
-result = []
-visited = [False] * n
+N, M = map(int, input().split())
 
+arr = set()
+for i in sorted(set(itertools.permutations(map(int, input().split()), M))):
+    arr.add(i)
 
-def dfs(depth):
-    if depth == m:
-        print(" ".join(map(str, result)))
-        return
-    ram = 0
-    
-    for i in range(n):
-        if not visited[i] and arr[i] != ram:
-            visited[i] = True
-            result.append(arr[i])
-            ram = arr[i]
-            dfs(depth + 1)
-            result.pop()
-            visited[i] = False
-
-
-dfs(0)
+for i in sorted(arr):
+    print(*i)
