1aa4391c883d447c8b7f1b570503d425f066de5c
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-04-30 16:41:04 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,11 +1,13 @@
 import sys
 
+sys.setrecursionlimit(10 ** 6)
 input = lambda: sys.stdin.readline().rstrip()
 
 
 def main():
     A, B, V = map(int, input().split())
-    print((V - B - 1) // (A - B) + 1)
+    day = (V - B - 1) // (A - B) + 1
+    print(day)
 
 
 if __name__ == '__main__':
