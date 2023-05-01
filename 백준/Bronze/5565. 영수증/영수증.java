b96a225904b2fb9ec5f6840c5b54784f9aebcb7e
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-01 10:21:24 +0900

    [Bronze III] Title: 영수증, Time: 120 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for (int i = 0; i < 9; i++) sum += Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(n - sum));
+        bw.close();
+    }
+}
