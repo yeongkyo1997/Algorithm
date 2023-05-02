fb48f1eac986f7de49e2007814e167a8fe0819fa
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-02 17:29:58 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    result = []
+    
+    while n != 1:
+        result.append(n)
+        
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = 3 * n + 1
+            
+    result.append(1)
+    return result
