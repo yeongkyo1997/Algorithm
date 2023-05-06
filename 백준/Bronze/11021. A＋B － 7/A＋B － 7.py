51e4d7c9f18b4476007c3bfa47d894f862fad7ba
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-06 17:35:20 +0900

    [Bronze V] Title: A+B - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,5 +1,12 @@
-T = int(input())
+import sys
 
-for i in range(T):
-    a, b = map(int, input().split())
-    print(f'Case #{i + 1}: {a + b}')
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def main():
+    for i in range(int(input())):
+        print(f'Case #{i + 1}: {sum(map(int, input().split()))}')
+
+
+if __name__ == '__main__':
+    main()
