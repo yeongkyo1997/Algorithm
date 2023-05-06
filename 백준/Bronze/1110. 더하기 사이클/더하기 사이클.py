869ba947c2d0371be6c5e13952bb7001c279c0fe
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-06 18:25:28 +0900

    [Bronze I] Title: 더하기 사이클, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,22 +1,13 @@
-import sys
+n = int(input())
 
-n = int(sys.stdin.readline())
+cnt = 0
+tmp = n
 
-cycle = 0
-
-def new_num(x):
-    if x < 10:
-        x = '0' + str(x)
-    else:
-        x = str(x)
-    s = int(x[0]) + int(x[1])
-    return int(x[1] + str(s)[-1])
-
-temp = n
 while True:
-    cycle += 1
-    temp = new_num(temp)
-    if temp == n:
+    cnt += 1
+    sum_ = (tmp // 10) + (tmp % 10)
+    tmp = (tmp % 10) * 10 + (sum_ % 10)
+    if tmp == n:
         break
 
-print(cycle)
+print(cnt)
