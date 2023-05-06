f316f5b536bfe4c38f2f02f4d039de2987c6b846
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-06 17:49:48 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.py
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.py
@@ -1,9 +1,2 @@
-import sys
-
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
-
-N = int(input())
-
-for i in range(N, 0, -1):
+for i in range(int(input()), 0, -1):
     print('*' * i)
