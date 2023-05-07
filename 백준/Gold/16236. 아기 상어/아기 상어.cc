ed6e49cfa46b712589be1075ba36f61dedf3b51b
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-07 10:29:15 +0900

    [Gold III] Title: 아기 상어, Time: 4 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -1,124 +1,83 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <tuple>
+
 using namespace std;
- 
-struct Shark{
-    int r,c;
-    int size = 2;
-    int cnt = 0;
-};
- 
-struct Fish{
-    int r,c;
-    int size;
-};
- 
-int N;
-int sea[20][20];
-Shark shark;
-vector<Fish> fishs;
-int ans = 0;
-bool notMove = false;
- 
-int dr[4] = {-1,1,0,0};
-int dc[4] = {0,0,-1,1};
- 
-void initTargetDist(int (*dist)[20]){
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            dist[i][j] = 0;
-        }
-    }
+
+int n;
+int space[20][20];
+int dx[] = {-1, 0, 1, 0};
+int dy[] = {0, -1, 0, 1};
+
+bool valid(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < n;
 }
-void calcDist(int (*targetDist)[20] ){
-    queue<pair<int, int>> q;
-    q.push({shark.r,shark.c});
-    targetDist[shark.r][shark.c] = 1;
-    while(!q.empty()){
-        int curR = q.front().first;
-        int curC = q.front().second;
-        q.pop();
-        for(int i = 0; i < 4; i++){
-            int nr = curR + dr[i];
-            int nc = curC + dc[i];
-            if(nr < 0 || nr > N-1 || nc < 0 || nc > N-1) continue;
-            if(targetDist[nr][nc] != 0) continue;
-            if(sea[nr][nc] > shark.size) continue;
-            q.push({nr,nc});
-            targetDist[nr][nc] = targetDist[curR][curC] + 1;
-        }
-    }
-}
- 
-void eatFishs(){
-    int targetDist[20][20];
-    initTargetDist(targetDist);
-    calcDist(targetDist);
-    int fishNum = -1;
-    int finaldist = 10000;
-    for(int i = 0; i < fishs.size(); i++){
-        if(fishs[i].size == -1) continue;
-        if(fishs[i].size < shark.size){
-            int dist = targetDist[fishs[i].r][fishs[i].c];
-            if(dist == 0) continue;
-            if(finaldist > dist){
-                finaldist = dist;
-                fishNum = i;
+
+int main() {
+    cin >> n;
+
+    int shark_x, shark_y;
+    int shark_size = 2;
+    int eaten_fish = 0;
+    int time = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> space[i][j];
+            if (space[i][j] == 9) {
+                shark_x = i;
+                shark_y = j;
+                space[i][j] = 0;
             }
         }
     }
-    if(finaldist == 10000){
-        notMove = true;
-        return;
-    }
-    ans += (finaldist-1);
-    shark.r = fishs[fishNum].r;
-    shark.c = fishs[fishNum].c;
-    shark.cnt++;
-    if(shark.cnt == shark.size){
-        shark.size++;
-        shark.cnt = 0;
-    }
-    fishs[fishNum].size = -1;
-    sea[shark.r][shark.c] = 0;
-}
- 
-bool checkingFishEat(){
-    for(int i = 0; i < fishs.size(); i++){
-        if(fishs[i].size > 0){
-            if(fishs[i].size - shark.size >= 0){
-                continue;
-            }
-            return false;
-        }
-    }
-    return true;
-}
- 
-int main(){
-    cin >> N;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cin >> sea[i][j];
-            if(sea[i][j] != 0){
-                if(sea[i][j] == 9){
-                    shark.r = i;
-                    shark.c = j;
-                    sea[i][j] = 0;
-                }else{
-                    Fish fish;
-                    fish.r = i;
-                    fish.c = j;
-                    fish.size = sea[i][j];
-                    fishs.push_back(fish);
+
+    while (true) {
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        visited[shark_x][shark_y] = true;
+        queue<tuple<int, int, int>> q;
+        q.push(make_tuple(shark_x, shark_y, 0));
+
+        int target_x = -1, target_y = -1, target_dist = -1;
+
+        while (!q.empty()) {
+            int x, y, dist;
+            tie(x, y, dist) = q.front();
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i], ny = y + dy[i];
+                if (valid(nx, ny) && !visited[nx][ny] && space[nx][ny] <= shark_size) {
+                    visited[nx][ny] = true;
+                    q.push(make_tuple(nx, ny, dist + 1));
+
+                    if (space[nx][ny] > 0 && space[nx][ny] < shark_size) {
+                        if (target_dist == -1 || target_dist > dist + 1 || (target_dist == dist + 1 && (target_x > nx || (target_x == nx && target_y > ny)))) {
+                            target_x = nx;
+                            target_y = ny;
+                            target_dist = dist + 1;
+                        }
+                    }
                 }
             }
         }
+
+        if (target_dist == -1) break;
+
+        shark_x = target_x;
+        shark_y = target_y;
+        space[shark_x][shark_y] = 0;
+        time += target_dist;
+        eaten_fish++;
+
+        if (eaten_fish == shark_size) {
+            shark_size++;
+            eaten_fish = 0;
+        }
     }
-    while(true){
-        if(checkingFishEat() || notMove) break;
-        eatFishs();
-    }
-    cout << ans << endl;
+
+    cout << time << '\n';
+
+    return 0;
 }
