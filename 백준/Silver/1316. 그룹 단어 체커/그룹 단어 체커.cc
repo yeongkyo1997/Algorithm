fef1d32334b25f0d8fd1cc7efc7b5792d33e843a
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-07 09:48:41 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -1,53 +1,31 @@
-#include<iostream>
- 
-using namespace std;
- 
-bool wordCheck(string str){
-    bool alpabet[26] = {false}; //알파벳 개수 만큼 배열을 만듭니다
-                                //알파벳에 해당하는 위치를 index 0~25에 맞게 할당하기 위해
- 
-    for(int i=0; i<str.length(); i++){
- 
-        if(alpabet[str[i]-'a']){ //true 이면 이미 있었으므로 false
-            return false;
- 
-        }else{                   //false 이면 처음 오는 알파벳 이므로 check
- 
-            char tmp = str[i];
-            alpabet[str[i]-'a'] = true; //알파벳이 왔으면 true로 바꿔줍니다
- 
-            while(1){
-                if(tmp != str[++i]){    //현재 알파벳과 다른 알파벳이 올때까지 탐색합니다
-                    i--;
-                    break;
-                }
-            }
- 
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isGroupWord(char word[]) {
+    bool alpha[26] = { false };
+    int prev = -1;
+    for (int i = 0; word[i] != '\0'; i++) {
+        int curr = word[i] - 'a';
+        if (prev != curr) {
+            if (alpha[curr])
+                return false;
+            alpha[curr] = true;
+            prev = curr;
         }
- 
     }
     return true;
 }
- 
- 
- 
-int main(void){
- 
+
+int main() {
     int n;
-    cin >> n;
- 
-    int count =0;
- 
-    for(int i=0; i<n; i++){
-        string str;
-        cin >> str;
- 
-        if(wordCheck(str)){
-            count++;
-        }
- 
+    scanf("%d", &n);
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        char word[101];
+        scanf("%s", word);
+        if (isGroupWord(word))
+            ans++;
     }
-    cout << count;
- 
+    printf("%d\n", ans);
     return 0;
 }
