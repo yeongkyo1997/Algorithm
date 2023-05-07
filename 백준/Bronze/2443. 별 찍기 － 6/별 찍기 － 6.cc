05e75414fa990b977b8cdef5f5f6abba548ce38d
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-07 11:06:52 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -1,22 +1,20 @@
-// https://www.acmicpc.net/problem/2443
-
-#include <iostream>
-
-using namespace std;
+#include <stdio.h>
 
 int main() {
     int n;
-    cin >> n;
+    scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i; j++)
-            cout << " ";
-        for (int j = i; j < n; j++) {
-            cout << "*";
+    for (int i = 1; i <= n; i++) { 
+        for (int j = 1; j < i; j++) {
+            printf(" ");
         }
-        for (int j = i; j < n - 1; j++) {
-            cout << "*";
+
+        for (int j = 1; j <= 2*n-(2*i-1); j++) {
+            printf("*");
         }
-        cout << endl;
+
+        printf("\n"); 
     }
+
+    return 0;
 }
