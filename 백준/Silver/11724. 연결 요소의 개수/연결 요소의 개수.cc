21bb05de504eb5dce3ee2ce3518475be69a20f0c
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-07 15:02:31 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 300 ms, Memory: 6436 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -1,59 +1,45 @@
 #include <iostream>
-
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
-const int MAX = 1000 + 1;
+const int MAX = 1000 + 5;
 
-int M, N;
-vector<int> graph[MAX];
+vector<int> adj[MAX];
 bool visited[MAX];
 
-void DFS(int node) {
-
-	visited[node] = true;
-
-
-
-	for (int i = 0; i < graph[node].size(); i++) {
-
-		int next = graph[node][i];
-
-		if (!visited[next])
-			DFS(next);
-
-	}
-
+void dfs(int v) {
+    visited[v] = true;
+    for (int i = 0; i < adj[v].size(); i++) {
+        int next = adj[v][i];
+        if (!visited[next]) {
+            dfs(next);
+        }
+    }
 }
 
+int main() {
+    int N, M;
+    cin >> N >> M;
 
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
 
-int main(void) {
+    int cnt = 0;
+    memset(visited, false, sizeof(visited));
+    for (int i = 1; i <= N; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            cnt++;
+        }
+    }
 
-	cin >> N >> M;
+    cout << cnt << endl;
 
-
-
-	for (int i = 0; i < M; i++) {
-
-		int node1, node2;
-
-		cin >> node1 >> node2;
-
-		graph[node1].push_back(node2);
-
-		graph[node2].push_back(node1);
-
-	}
-
-	int cnt = 0;
-
-	for (int i = 1; i <= N; i++)
-		if (!visited[i]) {
-			DFS(i);
-			cnt++;
-		}
-
-	cout << cnt << endl;
+    return 0;
 }
