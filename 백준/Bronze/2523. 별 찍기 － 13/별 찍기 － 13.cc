1fa18f4d6792eca4497d21a6be6f1512b6c557c8
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-07 10:21:16 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -1,5 +1,14 @@
-#include <iostream>
+#include<stdio.h>
 
-using namespace std; 
-int main() { int n; cin >> n; for (int i = 0; i < n; i++) { for (int j = 0; j <= i; j++) { cout << "*"; } cout << '\n'; } for (int i = n - 1; i > 0; i--) { for (int j = 0; j < i; j++) { cout << "*"; } cout << '\n'; } }
-
+int main() {
+    int n, i, j, k;
+    scanf("%d", &n);
+    for(i=0; i<2*n-1; i++) {
+        k = i < n ? i : 2*n-2-i;
+        for(j=0; j<k+1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
