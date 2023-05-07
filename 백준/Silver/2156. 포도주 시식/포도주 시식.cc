f61f8e96264e91793df9ede8411f2a954d6bfa3a
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-07 09:53:26 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -1,22 +1,30 @@
-#include <cstdio>
-#include <algorithm>
-using namespace std;
+#include <stdio.h>
 
-int n, d[10001], p[10001];
-
-void solve() {
-	d[1] = p[1], d[2] = p[1] + p[2];
-	for (int i = 3; i <= n; i++) {
-		d[i] = d[i - 1];
-		d[i] = max(d[i], d[i - 2] + p[i]);
-		d[i] = max(d[i], d[i - 3] + p[i - 1] + p[i]);
-	}
-	printf("%d\n", d[n]);
+int max(int a, int b) {
+    return a > b ? a : b;
 }
 
 int main() {
-	scanf("%d", &n);
-	for (int i = 1; i <= n; i++) scanf("%d", &p[i]);
-	solve();
-	return 0;
+    int n, i;
+    scanf("%d", &n);
+
+    int wine[n + 1], dp[n + 1];
+    wine[0] = dp[0] = 0;
+
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &wine[i]);
+    }
+
+    dp[1] = wine[1];
+    if (n > 1) {
+        dp[2] = wine[1] + wine[2];
+    }
+
+    for (i = 3; i <= n; i++) {
+        dp[i] = max(dp[i - 1], max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]));
+    }
+
+    printf("%d\n", dp[n]);
+
+    return 0;
 }
