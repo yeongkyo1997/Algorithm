195b8037d34baf1a9c3893a7856cab633a68ff91
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-08 15:10:11 +0900

    [Platinum V] Title: 약 팔기, Time: 120 ms, Memory: 13920 KB -BaekjoonHub

--- 백준/Platinum/15311. 약 팔기/약 팔기.java
+++ 백준/Platinum/15311. 약 팔기/약 팔기.java
@@ -1,16 +1,16 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        out.write("2000\n");
+    public static void main(String[] args) throws IOException {
+        bw.write("2000\n");
         for (int i = 1; i <= 1000; i++) {
-            out.write("1 ");
+            bw.write("1 ");
         }
         for (int i = 1000; i >= 1; i--) {
-            out.write("1000 ");
+            bw.write("1000 ");
         }
-        out.flush();
+        bw.close();
     }
 }
