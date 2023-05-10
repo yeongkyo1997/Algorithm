5c5155e4bb6b941fffc69bfd9c37c27c0be0986b
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-10 10:36:04 +0900

    [Silver V] Title: UCPC는 무엇의 약자일까?, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/15904. UCPC는 무엇의 약자일까？/UCPC는 무엇의 약자일까？.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+
+        String str = br.readLine();
+
+
+        int n1, n2, n3, n4;
+
+        n1 = str.indexOf('U');
+        n2 = str.indexOf('C', n1);
+        n3 = str.indexOf('P', n2);
+        n4 = str.indexOf('C', n3);
+
+
+        bw.write(n1 == -1 || n2 == -1 || n3 == -1 || n4 == -1 ? "I hate UCPC" : "I love UCPC");
+        bw.close();
+    }
+}
