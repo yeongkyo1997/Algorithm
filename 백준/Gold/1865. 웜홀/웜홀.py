039ab9909ce17c63fc33a51994dc75b16cdb9f77
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-15 20:08:20 +0900

    [Gold III] Title: 웜홀, Time: 272 ms, Memory: 118480 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,39 +1,48 @@
 import sys
 
-input = sys.stdin.readline
-INF = int(1e9)
+INF = 987654321
 
-def solve(start):
-    dist = [INF] * (N + 1)
-    dist[start] = 0
-    
-    for i in range(N):
-        for edge in graph:
-            cur = edge[0]
-            nxt = edge[1]
-            cost = edge[2]
-            
-            if dist[nxt] > cost + dist[cur]:
-                dist[nxt] = cost + dist[cur]
-                if i == N - 1:
-                    return True
-    
+class Edge:
+    def __init__(self, node, time):
+        self.node = node
+        self.time = time
+
+def bellman_ford(N, dis, road):
+    dis[1] = 0
+
+    for i in range(1, N):
+        for j in range(1, len(road)):
+            for next in road[j]:
+                if dis[j] + next.time < dis[next.node]:
+                    dis[next.node] = dis[j] + next.time
+
+    for j in range(1, len(road)):
+        for next in road[j]:
+            if dis[j] + next.time < dis[next.node]:
+                return True
+
     return False
 
-for _ in range(int(input())):
-    N, M, W = map(int, input().split())
-    graph = []
-    
-    for _ in range(M):
-        S, E, T = map(int, input().split())
-        graph.append((S, E, T))
-        graph.append((E, S, T))
-    
-    for _ in range(W):
-        S, E, T = map(int, input().split())
-        graph.append((S, E, -T))
-    
-    if solve(1):
-        print("YES")
-    else:
-        print("NO")
+def main():
+    tc = int(sys.stdin.readline())
+    for i in range(tc):
+        n, m, w = map(int, sys.stdin.readline().split())
+        dis = [INF] * (n + 1)
+        road = [ [] for _ in range(n + 1)]
+
+        for j in range(m):
+            a, b, c = map(int, sys.stdin.readline().split())
+            road[a].append(Edge(b, c))
+            road[b].append(Edge(a, c))
+
+        for j in range(w):
+            a, b, c = map(int, sys.stdin.readline().split())
+            road[a].append(Edge(b, -c))
+
+        if bellman_ford(n, dis, road):
+            print("YES")
+        else:
+            print("NO")
+
+if __name__ == "__main__":
+    main()
