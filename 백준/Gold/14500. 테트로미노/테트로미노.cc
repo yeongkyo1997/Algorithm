5d109c9ca876fcfbc378fd141f57b5f04c9f6c75
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-05-16 09:30:20 +0900

    [Gold IV] Title: 테트로미노, Time: 64 ms, Memory: 3436 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -1,59 +1,86 @@
-#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
 
-int n, m, ans;
-int a[500][500];
-const int b[19][4][2] = {
-    { {0,0}, {0,1}, {1,0}, {1,1} }, // ㅁ
-    { {0,0}, {0,1}, {0,2}, {0,3} }, // ㅡ
-    { {0,0}, {1,0}, {2,0}, {3,0} },
-    { {0,0}, {0,1}, {0,2}, {1,0} }, // ㄴ
-    { {0,2}, {1,0}, {1,1}, {1,2} },
-    { {0,0}, {1,0}, {1,1}, {1,2} },
-    { {0,0}, {0,1}, {0,2}, {1,2} },
-    { {0,0}, {1,0}, {2,0}, {2,1} },
-    { {0,0}, {0,1}, {1,1}, {2,1} },
-    { {0,0}, {0,1}, {1,0}, {2,0} },
-    { {0,1}, {1,1}, {2,0}, {2,1} },
-    { {0,0}, {1,0}, {1,1}, {2,1} }, // Z
-    { {0,1}, {1,0}, {1,1}, {2,0} },
-    { {0,1}, {0,2}, {1,0}, {1,1} },
-    { {0,0}, {0,1}, {1,1}, {1,2} },
-    { {0,0}, {0,1}, {0,2}, {1,1} }, // ㅗ
-    { {0,1}, {1,0}, {1,1}, {1,2} },
-    { {0,1}, {1,0}, {1,1}, {2,1} },
-    { {0,0}, {1,0}, {1,1}, {2,0} }
-};
+using namespace std;
 
-void tetromino(int x, int y) {
-    for (int i=0; i<19; i++) {
-        int res = 0;
-        for (int j=0; j<4; j++) {
-            int nx = x + b[i][j][0];
-            int ny = y + b[i][j][1];
-            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                res += a[nx][ny];
-            }
+int n, m, a[501][501], result;
+bool check[501][501];
+int dx[] = {0, 0, 1, -1};
+int dy[] = {-1, 1, 0, 0};
+
+int ex[4][4] = {{0, 0, 0, 1}, {0, 1, 2, 1}, {0, 0, 0, -1}, {0, -1, 0, 1}};
+int ey[4][4] = {{0, 1, 2, 1}, {0, 0, 0, 1}, {0, 1, 2, 1}, {0, 1, 1, 1}};
+
+void dfs(int x, int y, int sum_value, int length) {
+    if (length >= 4) {
+        result = max(result, sum_value);
+        return;
+    }
+
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (nx < 1 || nx > n || ny < 1 || ny > m)
+            continue;
+
+        if (!check[nx][ny]) {
+            check[nx][ny] = true;
+            dfs(nx, ny, sum_value + a[nx][ny], length + 1);
+            check[nx][ny] = false;
         }
-        if (ans < res) ans = res;
     }
 }
 
-void solve() {
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            tetromino(i, j);
+void check_exshape(int x, int y) {
+    for (int i = 0; i < 4; i++) {
+        bool isOut = false;
+        int sum_value = 0;
+
+        for (int j = 0; j < 4; j++) {
+            int nx = x + ex[i][j];
+            int ny = y + ey[i][j];
+
+            if (nx < 1 || nx > n || ny < 1 || ny > m) {
+                isOut = true;
+                break;
+            } else {
+                sum_value += a[nx][ny];
+            }
+        }
+
+        if (!isOut) {
+            result = max(result, sum_value);
         }
     }
-    printf("%d\n", ans);
 }
 
 int main() {
-    scanf("%d %d", &n, &m);
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            scanf("%d", &a[i][j]);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+
+    memset(check, false, sizeof(check));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> a[i][j];
         }
     }
-    solve();
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            check[i][j] = true;
+            dfs(i, j, a[i][j], 1);
+            check[i][j] = false;
+            check_exshape(i, j);
+        }
+    }
+
+    cout << result << "\n";
+
     return 0;
 }
