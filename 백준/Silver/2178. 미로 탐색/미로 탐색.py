651981f7132e4def8e170cb62a02b7d2c07751d1
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-12-15 16:50:30 +0900

    [Silver I] Title: 미로 탐색, Time: 160 ms, Memory: 114148 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,28 +1,36 @@
-import collections
 import sys
+from collections import defaultdict, deque
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 N, M = map(int, input().split())
-arr = [list(map(int, list(input()))) for _ in range(N)]
-
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-q = collections.deque()
-q.append((0, 0, 1))
+arr = []
+for _ in range(N):
+    arr.append(list(map(int, list(input()))))
 
-while q:
-    x, y, depth = q.popleft()
 
-    if x == N - 1 and y == M - 1:
-        print(depth)
-        exit(0)
+def bfs():
+    q = deque()
+    q.append((0, 0, 1))
 
-    for i in range(4):
-        nx, ny, ndepth = x + dx[i], y + dy[i], depth + 1
+    while q:
+        x, y, depth = q.popleft()
 
-        if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 1:
-            arr[nx][ny] = 0
-            q.append((nx, ny, ndepth))
+        if x == N - 1 and y == M - 1:
+            print(depth)
+            exit(0)
+
+        for i in range(4):
+            nx, ny, ndepth = x + dx[i], y + dy[i], depth + 1
+
+            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 1:
+                q.append((nx, ny, ndepth))
+                arr[nx][ny] = 0
+
+
+bfs()
