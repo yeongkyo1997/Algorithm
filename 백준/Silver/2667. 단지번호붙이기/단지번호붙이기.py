162fa3a566af8de6dd92e80ccf23cb45fb9ce429
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-12-15 17:09:14 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 109404 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,40 +1,37 @@
 import sys
 
-sys.setrecursionlimit(10000)
-input = sys.stdin.readline
 
-
-def dfs(x, y):
-    global arr
-    a += 1
-    arr[x][y] = 0
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if nx >= 0 and ny >= 0 and nx < N and ny < N and arr[nx][ny] == 1:
-            arr[nx][ny] = 0
-            dfs(nx, ny)
+def input(): return sys.stdin.readline().rstrip()
 
 
 N = int(input())
-result = []
-
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-arr = [list(map(int, input().rstrip())) for _ in range(N)]
+result = []
+
+
+arr = []
+for _ in range(N):
+    arr.append(list(input()))
+
+
+def dfs(x, y):
+    arr[x][y] = '0'
+    result = 1
+
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+
+        if 0 <= nx < N and 0 <= ny < N and arr[nx][ny] == '1':
+            result += dfs(nx, ny)
+    return result
+
 
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == 1:
-            arr = 0
-            dfs(i, j)
-            result.append(arr)
+        if arr[i][j] == '1':
+            result.append(dfs(i, j))
 
 print(len(result))
-
-result = sorted(result)
-
-for i in result:
-    print(i)
+print(*sorted(result), sep='\n')
