2724632815e1cd5e9a877f493120c64b02eee62e
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-12-26 14:09:16 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,10 @@
+import itertools
+
+def solution(number):
+    result = 0
+    for i in itertools.combinations(number, 3):
+        if sum(i) == 0:
+            result += 1
+    
+    return result
+    
