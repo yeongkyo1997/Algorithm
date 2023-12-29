3b2c394c10d9f2e6728b451290fd5788d8ca03ba
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-12-29 22:49:19 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,12 @@
+def solution(cards1, cards2, goal):
+    i = j = 0
+
+    for word in goal:
+        if i < len(cards1) and cards1[i] == word:
+            i += 1
+        elif j < len(cards2) and cards2[j] == word:
+            j += 1
+        else:
+            return 'No'
+    
+    return 'Yes'
