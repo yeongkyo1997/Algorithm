5f9dd38f834256beb3262089258d83ec808df18e
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-12-30 16:09:08 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -1,5 +1,6 @@
 def solution(n):
-    if n % 2 == 0:
-        return sum(x ** 2 for x in range(1, n + 1) if x % 2 == 0)
+    if n % 2 != 0:
+        return sum(range(1, n + 1, 2))
     else:
-        return sum(x for x in range(1, n + 1) if x % 2 != 0)
+        return sum(map(lambda x : x ** 2, range(2, n + 1, 2)))
+            
