b783e0f05368ef29b0aa4439dc6f9f905793fe53
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-12-30 14:31:39 +0900

    [level 1] Title: 소수 찾기, Time: 325.75 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,9 +1,7 @@
 def solution(n):
-    return sum(1 for i in range(2, n + 1) if is_prime(i))
-    
-def is_prime(n):
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    
-    return True
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
