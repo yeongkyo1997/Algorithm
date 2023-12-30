1f77db6503d7983b0becc5780fd031a174b04123
yeongkyo1997 <yeongkyo1997@gmail.com>
2023-12-30 14:29:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.30 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,6 @@
-from collections import defaultdict
+import collections
+
 
 def solution(participant, completion):
-    lib = defaultdict(int)
-    
-    for c in completion:
-        lib[c] += 1
-        
-    for p in participant:
-        lib[p] -= 1
-        
-    for key, value in lib.items():
-        if value < 0:
-            return key
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
