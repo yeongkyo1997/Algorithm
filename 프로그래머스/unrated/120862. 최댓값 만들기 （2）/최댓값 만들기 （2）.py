61315a561b7c3065dd0a22bc371c5b4b370d1aa2
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-01 15:29:54 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,20 +1,4 @@
 def solution(numbers):
     numbers.sort(reverse=True)
     
-    plus, minus = [], []
-    
-    for i in numbers:
-        if i >= 0:
-            plus.append(i)
-        else:
-            minus.append(i)
-    
-    if len(plus) >= 2 and len(minus) >= 2:
-        return max(plus[0] * plus[1], minus[-1] * minus[-2])
-    else:
-        if len(plus) < 2 and len(minus) < 2:
-            return plus[0] * minus[0]
-        elif len(plus) < 2:
-            return minus[-1] * minus[-2]
-        else:
-            return plus[0] * plus[1]
+    return max(numbers[0] * numbers[1], numbers[-1] * numbers[-2])
