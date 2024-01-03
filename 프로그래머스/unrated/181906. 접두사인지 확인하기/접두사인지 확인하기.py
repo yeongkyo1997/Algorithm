04f12ccb2dbecb28ab4e1a73b5de46c58dc0bc2e
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-03 16:49:11 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,6 +1,3 @@
 def solution(my_string, is_prefix):
-    if my_string.startswith(is_prefix):
-        return 1
-    else:
-        return 0
-    
+    return int(my_string.startswith(is_prefix))
+      
