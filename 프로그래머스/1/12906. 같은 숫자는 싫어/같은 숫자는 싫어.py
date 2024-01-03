4742989120efaebde6d95e5d88fbe44c61f45177
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-03 17:35:16 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 69.07 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    result = [arr[0]]
+    
+    for i in arr[1:]:
+        if result[-1] != i:
+            result.append(i)
+            
+    return result
+            
+        
