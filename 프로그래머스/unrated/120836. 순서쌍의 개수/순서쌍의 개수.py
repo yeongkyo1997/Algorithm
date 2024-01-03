1164ecf0a44da4d90be68cff2f22a7facfc9c2a7
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-03 16:47:55 +0900

    [level unrated] Title: 순서쌍의 개수, Time: 56.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,6 +1,2 @@
 def solution(n):
-    result = 0
-    for i in range(1, n + 1):
-        if n % i == 0:
-            result += 1
-    return result
+    return sum(1 for i in range(1, n + 1) if n % i == 0)
