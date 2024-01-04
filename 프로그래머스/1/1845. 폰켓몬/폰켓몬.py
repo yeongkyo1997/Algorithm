516ac22272bb5348dbcd297759e85dd6d2461be0
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-04 12:44:49 +0900

    [level 1] Title: 폰켓몬, Time: 0.40 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,2 @@
-from collections import defaultdict, Counter
 def solution(nums):
-    size = len(nums) // 2
-    lib = defaultdict(int)
-    counter = Counter(nums)
-    
-    for key, val in counter.items():
-        lib[key] = val
-        
-    return min(len(lib.keys()), len(nums) // 2)
+    return min(len(set(nums)), len(nums) // 2)
