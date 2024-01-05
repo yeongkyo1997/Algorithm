0503b2d06662143ce7b17f2f75d17d17fc6e2df3
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-05 21:35:20 +0900

    [Silver I] Title: 미로 탐색, Time: 152 ms, Memory: 114208 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,36 +1,29 @@
 import sys
-from collections import defaultdict, deque
+from collections import deque, defaultdict
 
 
-def input(): return sys.stdin.readline().rstrip()
+def input(): return sys.stdin.readline().strip()
 
 
-N, M = map(int, input().split())
+n, m = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(n)]
+visited = defaultdict(lambda: [False] * m)
+
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-arr = []
-for _ in range(N):
-    arr.append(list(map(int, list(input()))))
+q = deque()
+q.append((0, 0, 1))
 
+while q:
+    x, y, depth = q.popleft()
+    if x == n - 1 and y == m - 1:
+        print(depth)
+        break
 
-def bfs():
-    q = deque()
-    q.append((0, 0, 1))
+    for i in range(4):
+        nx, ny, ndepth = x + dx[i], y + dy[i], depth + 1
 
-    while q:
-        x, y, depth = q.popleft()
-
-        if x == N - 1 and y == M - 1:
-            print(depth)
-            exit(0)
-
-        for i in range(4):
-            nx, ny, ndepth = x + dx[i], y + dy[i], depth + 1
-
-            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 1:
-                q.append((nx, ny, ndepth))
-                arr[nx][ny] = 0
-
-
-bfs()
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and arr[nx][ny] == 1:
+            q.append((nx, ny, ndepth))
+            visited[nx][ny] = True
