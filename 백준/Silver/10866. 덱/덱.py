607f6c569bc0cf2d450d6428f1f1f90f0236ff94
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-05 15:37:51 +0900

    [Silver IV] Title: 덱, Time: 192 ms, Memory: 116212 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,43 +1,31 @@
 import sys
 from collections import deque
 
-input = sys.stdin.readline
+input = lambda : sys.stdin.readline().strip()
 
 n = int(input())
-queue = deque()
-commands = [(input().strip()).split(" ") for _ in range(n)]
-for i in commands:
-    if len(i) == 1:
-        i.append(0)
-for command, x in commands:
-    if command == "push_front":
-        queue.appendleft(x)
-    elif command == "push_back":
-        queue.append(x)
-    elif command == "pop_front":
-        if queue:
-            print(queue.popleft())
-        else:
-            print(-1)
-    elif command == "pop_back":
-        if queue:
-            print(queue.pop())
-        else:
-            print(-1)
-    elif command == "size":
-        print(len(queue))
-    elif command == "empty":
-        if queue:
-            print(0)
-        else:
-            print(1)
-    elif command == "front":
-        if queue:
-            print(queue[0])
-        else:
-            print(-1)
-    elif command == "back":
-        if queue:
-            print(queue[-1])
-        else:
-            print(-1)
+q = deque()
+for _ in range(n):
+    commands = input().split()
+    command = commands[0]
+    try:
+        val = commands[1]
+    except:
+        pass
+
+    if command == 'push_front':
+        q.appendleft(val)
+    elif command == 'push_back':
+        q.append(val)
+    elif command == 'pop_front':
+        print(q.popleft() if q else -1)
+    elif command == 'pop_back':
+        print(q.pop() if q else -1)
+    elif command == 'size':
+        print(len(q))
+    elif command == 'empty':
+        print(int(bool(not q)))
+    elif command == 'front':
+        print(q[0] if q else -1)
+    else:
+        print(q[-1] if q else -1)
