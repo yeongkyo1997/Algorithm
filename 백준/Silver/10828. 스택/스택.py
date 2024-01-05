378a44e7909aa9066abff6ff97a8dce30d8b38ea
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-05 15:28:53 +0900

    [Silver IV] Title: 스택, Time: 164 ms, Memory: 111960 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,34 +1,28 @@
 import sys
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
 
 
-def main():
-    n = int(input())
-    stack = []
-    for _ in range(n):
-        cmd = input().split()
-        if cmd[0] == 'push':
-            stack.append(cmd[1])
-        elif cmd[0] == 'pop':
-            if stack:
-                print(stack.pop())
-            else:
-                print(-1)
-        elif cmd[0] == 'size':
-            print(len(stack))
-        elif cmd[0] == 'empty':
-            if stack:
-                print(0)
-            else:
-                print(1)
-        elif cmd[0] == 'top':
-            if stack:
-                print(stack[-1])
-            else:
-                print(-1)
+n = int(input())
 
+stack = []
+for _ in range(n):
+    command = input().split()
 
-if __name__ == '__main__':
-    main()
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        print(int(bool(not stack)))
+    else:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
