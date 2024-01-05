598d8e2569a33ae93e34de6f810e89d58157847a
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-05 20:02:03 +0900

    [Silver III] Title: 2×n 타일링, Time: 164 ms, Memory: 241212 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,22 +1,18 @@
-import collections
 import sys
+from functools import cache
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
-
-
-def main():
-    n = int(input())
-    dp = collections.defaultdict(lambda: 0)
-    dp[1] = 1
-    dp[2] = 2
-    MOD = 10007
-
-    for i in range(3, 1001):
-        dp[i] = dp[i - 1] % MOD + dp[i - 2] % MOD
-
-    print(dp[n] % 10007)
+sys.setrecursionlimit(10 ** 5)
+def input(): return sys.stdin.readline().strip()
 
 
-if __name__ == '__main__':
-    main()
+@cache
+def solve(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    return (solve(n - 1) + solve(n - 2)) % 10007
+
+
+print(solve(int(input())))
