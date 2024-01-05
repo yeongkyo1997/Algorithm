0170b7ea614900dfd93e7630cc3775fa5abb5d2a
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-05 18:32:19 +0900

    [Silver IV] Title: 설탕 배달, Time: 148 ms, Memory: 113848 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,15 +1,17 @@
-n = int(input())
-cnt = 0
-while n > 0:
-    if n % 5 == 0:
-        cnt += n / 5
-        n = 0
-        break
-    else:
-        cnt += 1
-        n -= 3
+import sys
+from collections import defaultdict
 
-if n < 0:
-    print(-1)
-else:
-    print(int(cnt))
+
+def input(): return sys.stdin.readline().strip()
+
+
+n = int(input())
+
+lib = defaultdict(lambda: float('inf'))
+
+lib[3], lib[5] = 0, 0
+
+for i in range(3, n + 1):
+    lib[i] = min([lib[i], lib[i - 3], lib[i - 5]]) + 1
+
+print(-1 if lib[n] == float('inf') else lib[n])
