f3e05d7cccda3812eef926f33097e060145c50c7
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-08 16:41:03 +0900

    [level unrated] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,11 @@
+def solution(ineq, eq, n, m):
+    if ineq == '<':
+        if eq == '=':
+            return int(n <= m)
+        else:
+            return int(n < m)
+    else:
+        if eq == '=':
+            return int(n >= m)
+        else:
+            return int(n > m)
