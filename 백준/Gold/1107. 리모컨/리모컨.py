3d1b78989fccffb7829e997a1bea716c6e60fb66
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-11 19:57:53 +0900

    [Gold V] Title: 리모컨, Time: 256 ms, Memory: 110768 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,38 +1,23 @@
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
 
-min_cnt = 0x3f3f3f3f
-num_int = 0
-btn_set = [1] * 10
+def input(): return sys.stdin.readline().strip()
 
 
-def find(num):
-    global min_cnt, num_int, btn_set
+N = int(input())
+M = int(input())
 
-    for i in range(10):
-        if btn_set[i]:
-            tmp_num = num + str(i)
-            min_cnt = min(abs(num_int - int(tmp_num)) + len(tmp_num), min_cnt)
+if M:
+    broken = list(map(int, input().split()))
+else:
+    broken = []
+result = abs(100 - N)
 
-            if len(tmp_num) < 6:
-                find(tmp_num)
-
-
-def main():
-    global min_cnt, num_int, btn_set
-    num_int = int(input())
-    n = int(input())
-    min_cnt = min(abs(100 - num_int), min_cnt)
-
-    for i in map(int, input().split()):
-        btn_set[i] = 0
-
-    if n < 10:
-        find("")
-
-    print(min_cnt)
-
-
-if __name__ == '__main__':
-    main()
+for i in range(1000001):
+    num = str(i)
+    for j in num:
+        if int(j) in broken:
+            break
+    else:
+        result = min(result, len(num) + abs(i - N))
+print(result)
