c685fdf454cf8f2c146ed7a18dfd5fbc555a5ccf
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-11 16:16:08 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,15 +1,10 @@
-import statistics
 import sys
 
-sys.setrecursionlimit(10000)
-input = lambda: sys.stdin.readline().rstrip()
 
-def main():
-    n = int(input())
-    arr = list(map(int, input().split()))
-    MAX = max(arr)
-    arr = map(lambda x: (x / MAX * 100), arr)
-    print(statistics.mean(arr))
+def input(): return sys.stdin.readline().strip()
 
-if __name__ == '__main__':
-    main()
+
+n = int(input())
+arr = list(map(int, input().split()))
+arr = list(map(lambda x: x / max(arr) * 100, arr))
+print(sum(arr) / len(arr))
