131d6574962cca1c958579da6a8c73a78d3a775c
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-11 17:10:20 +0900

    [Silver I] Title: 회의실 배정, Time: 368 ms, Memory: 122696 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,23 +1,26 @@
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
 
-N = int(input())
+def input(): return sys.stdin.readline().strip()
+
+
+n = int(input())
 
 arr = []
-for i in range(N):
-    start, end = map(int, input().split())
-    arr.append((end, start))
 
-arr.sort()
+for _ in range(n):
+    a, b = map(int, input().split())
+    arr.append((a, b))
 
-time = arr[0][0]
+arr.sort(key=lambda x: (x[1], x[0]))
+
+
+end = arr[0][1]
 
 result = 1
-
-for i in range(1, N):
-    if time <= arr[i][1]:
+for a, b in arr[1:]:
+    if end <= a:
         result += 1
-        time = arr[i][0]
+        end = b
 
 print(result)
