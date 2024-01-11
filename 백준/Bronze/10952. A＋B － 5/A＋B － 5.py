2b73d93593af83ca6a0ba5ae56cc2aee413a74be
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-11 16:39:09 +0900

    [Bronze V] Title: A+B - 5, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,15 +1,11 @@
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().strip()
 
 
-def main():
-    while True:
-        a, b = map(int, input().split())
-        if a == b == 0:
-            break
-        print(a + b)
-
-
-if __name__ == '__main__':
-    main()
+while True:
+    a, b = map(int, input().split())
+    if a == b == 0:
+        break
+    print(a + b)
