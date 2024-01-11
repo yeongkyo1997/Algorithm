2f06c8bd63342d9b4d833a615dcc42cc7c6eba36
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-11 15:43:24 +0900

    [Bronze V] Title: 검증수, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,5 +1,7 @@
-arr=list(map(int, input().split()))
-result = 0
-for n in arr:
-    result += n ** 2
-print(result % 10)
+import sys
+
+
+def input(): return sys.stdin.readline().strip()
+
+
+print(sum(map(lambda x: int(x) ** 2, input().split())) % 10)
