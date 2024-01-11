24092e49fdb34eb95c971ecb9a9135fc9f79acfd
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-11 19:32:42 +0900

    [Silver I] Title: 숨바꼭질, Time: 172 ms, Memory: 114960 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,32 +1,29 @@
 import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
-
-def check(n):
-    if n < 0 or n > 100000 or visited[n]:
-        return False
-    return True
+from collections import deque
 
 
-def bfs():
-    while q:
-        data, depth = q.pop(0)
-        if data == K:
-            return depth
-        if check(data - 1):
-            visited[data - 1] = True
-            q.append([data - 1, depth + 1])
-        if check(data + 1):
-            visited[data + 1] = True
-            q.append([data + 1, depth + 1])
-        if check(data * 2):
-            visited[data * 2] = True
-            q.append([data * 2, depth + 1])
+def input(): return sys.stdin.readline().strip()
 
 
 N, K = map(int, input().split())
+
+
+heap = [(0, N)]
+q = deque([(0, N)])
 visited = [False] * 100001
-q = [[N, 0]]
-visited[N] = True
-print(bfs())
+
+while heap:
+    depth, cur = q.popleft()
+
+    if cur == K:
+        print(depth)
+        break
+    if 0 <= cur * 2 <= 100000 and not visited[cur * 2]:
+        q.append((depth + 1, cur * 2))
+        visited[cur * 2] = True
+    if 0 <= cur + 1 <= 100000 and not visited[cur + 1]:
+        q.append((depth + 1, cur + 1))
+        visited[cur + 1] = True
+    if 0 <= cur - 1 <= 100000 and not visited[cur - 1]:
+        q.append((depth + 1, cur - 1))
+        visited[cur - 1] = True
