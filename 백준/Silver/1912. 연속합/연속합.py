536a6b1b5920d6ceab4f4d3a76d7d9345f396636
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-11 10:53:02 +0900

    [Silver II] Title: 연속합, Time: 128 ms, Memory: 120816 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def input(): return sys.stdin.readline().rstrip()
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(1, n):
+    arr[i] = max(arr[i], arr[i - 1] + arr[i])
+
+print(max(arr))
