2d02f39b216fedcfdc7d2800e5a28b59d1141c05
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-11 19:07:59 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2756 ms, Memory: 352084 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,45 +1,44 @@
-import heapq
 import sys
+from collections import defaultdict
+import heapq
 
-input = lambda: sys.stdin.readline().rstrip()
 
-# BOJ 7662 이중 우선순위 큐
-def main():
-    t = int(input())
-    for _ in range(t):
-        k = int(input())
-        min_heap = []
-        max_heap = []
-        visited = [0] * 1000001
-        for _ in range(k):
-            cmd, num = input().split()
-            num = int(num)
-            if cmd == 'I':
-                heapq.heappush(min_heap, (num, _))
-                heapq.heappush(max_heap, (-num, _))
-                visited[_] = 1
+def input(): return sys.stdin.readline().strip()
+
+
+t = int(input())
+
+for _ in range(t):
+    max_heap, min_heap = [], []
+    lib = defaultdict(int)
+    k = int(input())
+
+    for _ in range(k):
+        command, val = input().split()
+        val = int(val)
+
+        if command == 'I':
+            heapq.heappush(max_heap, -val)
+            heapq.heappush(min_heap, val)
+            lib[val] += 1
+        elif command == 'D':
+            if val == -1:
+                while min_heap and lib[min_heap[0]] == 0:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    lib[heapq.heappop(min_heap)] -= 1
             else:
-                if num == 1:
-                    while max_heap and not visited[max_heap[0][1]]:
-                        heapq.heappop(max_heap)
-                    if max_heap:
-                        visited[max_heap[0][1]] = 0
-                        heapq.heappop(max_heap)
-                else:
-                    while min_heap and not visited[min_heap[0][1]]:
-                        heapq.heappop(min_heap)
-                    if min_heap:
-                        visited[min_heap[0][1]] = 0
-                        heapq.heappop(min_heap)
-        while max_heap and not visited[max_heap[0][1]]:
-            heapq.heappop(max_heap)
-        while min_heap and not visited[min_heap[0][1]]:
-            heapq.heappop(min_heap)
-        if max_heap and min_heap:
-            print(-max_heap[0][0], min_heap[0][0])
-        else:
-            print('EMPTY')
+                while max_heap and lib[-max_heap[0]] == 0:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    lib[-heapq.heappop(max_heap)] -= 1
 
+    while max_heap and lib[-max_heap[0]] == 0:
+        heapq.heappop(max_heap)
+    while min_heap and lib[min_heap[0]] == 0:
+        heapq.heappop(min_heap)
 
-if __name__ == '__main__':
-    main()
+    if max_heap and min_heap:
+        print(-max_heap[0], min_heap[0])
+    else:
+        print('EMPTY')
