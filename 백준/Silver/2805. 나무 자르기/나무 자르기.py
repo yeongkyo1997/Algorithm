20b65f3703f8c07ab04c7ece84bcf1048d2a6bd7
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-11 14:27:45 +0900

    [Silver II] Title: 나무 자르기, Time: 484 ms, Memory: 257896 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,32 +1,27 @@
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 N, M = map(int, input().split())
-trees = list(map(int, input().split()))
+
+arr = list(map(int, input().split()))
 
 
-def isPossible(height):
-    taken = 0
-    for i in range(N):
-        if trees[i] >= height:
-            taken += (trees[i] - height)
-        if taken >= M:
-            return True
-    return False
+left, right = 1, 1000000000
 
+while left <= right:
+    mid = (left + right) // 2
+    cnt = 0
 
-def solve():
-    left = 0
-    right = 1000000000
-    while left <= right:
-        mid = (left + right) // 2
-        if isPossible(mid):
-            ret = mid
-            left = mid + 1
-        else:
-            right = mid - 1
-    return ret
+    for i in arr:
+        if i > mid:
+            cnt += i - mid
 
+    if cnt >= M:
+        left = mid + 1
+    else:
+        right = mid - 1
 
-print(solve())
+print(right)
