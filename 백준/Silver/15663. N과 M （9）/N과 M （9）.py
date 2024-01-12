0e06e78f6a25b029a6ffc3de15c4c2f713bc2684
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-12 21:53:18 +0900

    [Silver II] Title: N과 M (9), Time: 180 ms, Memory: 114176 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,15 +1,15 @@
-import itertools
-
 import sys
+from itertools import permutations
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 N, M = map(int, input().split())
 
-arr = set()
-for i in sorted(set(itertools.permutations(map(int, input().split()), M))):
-    arr.add(i)
+result = set()
+for i in permutations(map(int, input().split()), M):
+    result.add(i)
 
-for i in sorted(arr):
+for i in sorted(result):
     print(*i)
