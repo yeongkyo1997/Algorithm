135bec43a24f3666c88c0d1af35a81382202330b
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-12 21:44:19 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 111436 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,10 +1,11 @@
-import itertools
 import sys
+from itertools import combinations_with_replacement
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 N, M = map(int, input().split())
 
-for i in itertools.combinations_with_replacement(range(1, N + 1), M):
+for i in combinations_with_replacement(range(1, N + 1), M):
     print(*i)
