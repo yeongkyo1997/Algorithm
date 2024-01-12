80b36a49609219716f756b2e50dac73c1fa17368
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-12 22:33:29 +0900

    [Silver IV] Title: 한수, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,22 +1,24 @@
-def is_hansu(number):
-    if number < 100:
-        return True
+import sys
 
-    digits = [int(d) for d in str(number)]
-    diff = digits[1] - digits[0]
 
-    for i in range(2, len(digits)):
-        if digits[i] - digits[i - 1] != diff:
-            return False
+def input(): return sys.stdin.readline().strip()
 
-    return True
-
-def count_hansu(n):
-    count = 0
-    for i in range(1, n + 1):
-        if is_hansu(i):
-            count += 1
-    return count
 
 N = int(input())
-print(count_hansu(N))
+count = 0
+
+for num in range(1, N + 1):
+    num_str = str(num)
+    if len(num_str) <= 2:
+        count += 1
+    else:
+        is_hansu = True
+        diff = int(num_str[1]) - int(num_str[0])
+        for i in range(2, len(num_str)):
+            if int(num_str[i]) - int(num_str[i - 1]) != diff:
+                is_hansu = False
+                break
+        if is_hansu:
+            count += 1
+
+print(count)
