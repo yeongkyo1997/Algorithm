d7c382e10e28d81ce2cba810793f86a449fe9f0a
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-12 22:51:40 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 220 ms, Memory: 269388 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -1,28 +1,27 @@
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10 ** 5)
 
-def main():
-    while True:
-        N, *arr = list(map(int, input().split()))
-        if N == 0:
-            break
-        
-        stack = []
-        result = 0
-        for i in range(N):
-            while stack and arr[stack[-1]] > arr[i]:
-                h = arr[stack.pop()]
-                w = i if not stack else i - stack[-1] - 1
-                result = max(result, h * w)
-            stack.append(i)
-        
-        while stack:
+while True:
+    n, *arr = map(int, input().split())
+
+    if n == 0:
+        break
+
+    stack = []
+    result = 0
+
+    for i in range(n):
+        while stack and arr[i] < arr[stack[-1]]:
             h = arr[stack.pop()]
-            w = N if not stack else N - stack[-1] - 1
+            w = i if not stack else i - stack[-1] - 1
             result = max(result, h * w)
-        
-        print(result)
 
-if __name__ == '__main__':
-    main()
+        stack.append(i)
+
+    while stack:
+        h = arr[stack.pop()]
+        w = n if not stack else n - stack[-1] - 1
+        result = max(result, h * w)
+
+    print(result)
