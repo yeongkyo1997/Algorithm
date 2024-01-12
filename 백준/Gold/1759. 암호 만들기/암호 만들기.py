9e2773db1ecda423b66eb655d5785b4b192d350f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-12 22:27:40 +0900

    [Gold V] Title: 암호 만들기, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,38 +1,14 @@
 import sys
+from itertools import combinations
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 L, C = map(int, input().split())
 
-inputs = list(input().split())
-
-inputs.sort()
-
-
-def check(pwd):
-    j = 0
-    m = 0
-    for i in range(len(pwd)):
-        if pwd[i] in ['a', 'e', 'i', 'o', 'u']:
-            m += 1
-        else:
-            j += 1
-    if j >= 2 and m >= 1:
-        return True
-    else:
-        return False
-
-
-def combination(cnt, start):
-    if cnt == L:
-        if check(pwd):
-            print(''.join(pwd))
-        return
-    for i in range(start, C):
-        pwd[cnt] = inputs[i]
-        combination(cnt + 1, i + 1)
-
-
-pwd = [''] * L
-combination(0, 0)
+arr = input().split()
+arr.sort()
+for i in combinations(arr, L):
+    if len(list(a for a in i if a in 'aeiou')) >= 1 and len(list(a for a in i if not a in 'aeiou')) >= 2:
+        print(''.join(i))
