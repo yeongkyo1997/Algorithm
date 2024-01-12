627f4e5cfc3b7b50b5853d8350a688b39bb368a0
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-12 22:01:43 +0900

    [Silver III] Title: 조합, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -1,19 +1,10 @@
+import math
 import sys
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
 
-
-def comb(n, m):
-    if n == m or m == 0:
-        return 1
-    if dp[n][m] != 0:
-        return dp[n][m]
-    dp[n][m] = comb(n - 1, m - 1) + comb(n - 1, m)
-    return dp[n][m]
+def input(): return sys.stdin.readline().rstrip()
 
 
 n, m = map(int, input().split())
-dp = [[0] * (n + 1) for _ in range(n + 1)]
 
-print(comb(n, m))
+print(math.comb(n, m))
