107d2266d87d4bb5d93cfe3026dabc8c800308a1
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-12 20:55:10 +0900

    [Silver III] Title: 소수 구하기, Time: 736 ms, Memory: 118620 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -4,20 +4,24 @@ import sys
 def input(): return sys.stdin.readline().rstrip()
 
 
+def solutoin(M, N):
+    if N < 2:
+        return []
+
+    num = int(N**0.5) + 1
+    prime = [True] * num
+    prime[0] = prime[1] = False
+
+    for p in range(2, num):
+        if prime[p]:
+            for i in range(p * p, num, p):
+                prime[i] = False
+
+    result = [p for p in range(max(2, M), N + 1) if p % 2 != 0 and all(p %
+                                                                       i != 0 for i in range(3, int(p**0.5) + 1, 2)) or p == 2]
+
+    return result
+
+
 M, N = map(int, input().split())
-
-
-def is_prime(num):
-    if num == 1:
-        return False
-
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-
-    return True
-
-
-for i in range(M, N + 1):
-    if is_prime(i):
-        print(i)
+print(*solutoin(M, N), sep='\n')
