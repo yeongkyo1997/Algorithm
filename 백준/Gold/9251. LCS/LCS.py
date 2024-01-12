f38ed4dffbfdcafd30fcbe3faf1d32a8472420f3
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-12 22:42:17 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 118212 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,14 +1,21 @@
 import sys
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
 
 s1 = input()
 s2 = input()
-dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
 
-for i in range(1, len(s1) + 1):
-    for j in range(1, len(s2) + 1):
-        dp[i][j] = max(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1] + (s1[i - 1] == s2[j - 1]))
+len1 = len(s1)
+len2 = len(s2)
 
-print(dp[len(s1)][len(s2)])
+dp = [[0] * (len2 + 1) for _ in range(len1 + 1)]
+
+for i in range(1, len1 + 1):
+    for j in range(1, len2 + 1):
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[len1][len2])
