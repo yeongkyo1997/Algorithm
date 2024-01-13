fd08ffb8046a1c4ec7b4d2374c4919a0894bb79f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-14 08:57:26 +0900

    [Bronze V] Title: NFC West vs North, Time: 120 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/10170. NFC West vs North/NFC West vs North.java
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.java
@@ -1,31 +1,17 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
 public class Main {
-
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
-
-	public static void main(String[] args) throws IOException {
-
-		bw.write("NFC West       W   L  T\n");
-		bw.write("-----------------------\n");
-		bw.write("Seattle        13  3  0\n");
-		bw.write("San Francisco  12  4  0\n");
-		bw.write("Arizona        10  6  0\n");
-		bw.write("St. Louis      7   9  0\n\n");
-		bw.write("NFC North      W   L  T\n");
-		bw.write("-----------------------\n");
-		bw.write("Green Bay      8   7  1\n");
-		bw.write("Chicago        8   8  0\n");
-		bw.write("Detroit        7   9  0\n");
-		bw.write("Minnesota      5  10  1");
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) {
+        System.out.println("NFC West       W   L  T\n" +
+                "-----------------------\n" +
+                "Seattle        13  3  0\n" +
+                "San Francisco  12  4  0\n" +
+                "Arizona        10  6  0\n" +
+                "St. Louis      7   9  0\n" +
+                "\n" +
+                "NFC North      W   L  T\n" +
+                "-----------------------\n" +
+                "Green Bay      8   7  1\n" +
+                "Chicago        8   8  0\n" +
+                "Detroit        7   9  0\n" +
+                "Minnesota      5  10  1");
+    }
 }
