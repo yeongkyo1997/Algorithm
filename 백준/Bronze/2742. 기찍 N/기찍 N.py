f4b608ad20f5d5c346e13b4b5e3b0d4ab2a200ad
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-13 13:16:43 +0900

    [Bronze IV] Title: 기찍 N, Time: 288 ms, Memory: 302708 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.py
+++ 백준/Bronze/2742. 기찍 N/기찍 N.py
@@ -1,1 +1,20 @@
-[print(i) for i in range(int(input()), 0, -1)]
+import sys
+sys.setrecursionlimit(10 ** 5)
+
+
+def input(): return sys.stdin.readline().rstrip()
+
+
+n = int(input())
+
+
+def solution(n):
+    if n == 1:
+        print(n)
+        return 1
+
+    print(n)
+    solution(n - 1)
+
+
+solution(n)
