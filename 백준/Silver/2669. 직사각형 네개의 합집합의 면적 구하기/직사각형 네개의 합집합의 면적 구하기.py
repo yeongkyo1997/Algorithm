2da575cf427ae5842a3bfdc39881bf7bc639913e
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-13 21:12:39 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def input(): return sys.stdin.readline().rstrip()
+
+
+board = [[0] * 101 for _ in range(101)]
+
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            board[i][j] = 1
+
+print(sum(j for i in board for j in i))
