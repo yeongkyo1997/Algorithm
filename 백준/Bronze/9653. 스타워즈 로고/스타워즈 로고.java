43862261b4390f1b4ddeae859ca3d5bd5c7557da
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-14 08:58:11 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 104 ms, Memory: 13832 KB -BaekjoonHub

--- 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
@@ -1,26 +1,15 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-	public static void main(String[] args) throws IOException {
-		bw.write("    8888888888  888    88888\n");
-		bw.write("   88     88   88 88   88  88\n");
-		bw.write("    8888  88  88   88  88888\n");
-		bw.write("       88 88 888888888 88   88\n");
-		bw.write("88888888  88 88     88 88    888888\n");
-		bw.write("\n");
-		bw.write("88  88  88   888    88888    888888\n");
-		bw.write("88  88  88  88 88   88  88  88\n");
-		bw.write("88 8888 88 88   88  88888    8888\n");
-		bw.write(" 888  888 888888888 88  88      88\n");
-		bw.write("  88  88  88     88 88   88888888\n");
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) {
+        System.out.println("    8888888888  888    88888\n" +
+                "   88     88   88 88   88  88\n" +
+                "    8888  88  88   88  88888\n" +
+                "       88 88 888888888 88   88\n" +
+                "88888888  88 88     88 88    888888\n" +
+                "\n" +
+                "88  88  88   888    88888    888888\n" +
+                "88  88  88  88 88   88  88  88\n" +
+                "88 8888 88 88   88  88888    8888\n" +
+                " 888  888 888888888 88  88      88\n" +
+                "  88  88  88     88 88   88888888");
+    }
 }
