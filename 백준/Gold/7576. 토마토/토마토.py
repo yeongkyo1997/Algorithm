ce19c63e06fe7ffc70f73d032cec0b5e93b0258b
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-13 14:03:49 +0900

    [Gold V] Title: 토마토, Time: 452 ms, Memory: 156688 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,39 +1,49 @@
-import collections
 import sys
+from collections import deque
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 M, N = map(int, input().split())
-tomato = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0] * M for _ in range(N)]
-dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
-q = collections.deque()
+
+board = [list(map(int, input().split())) for _ in range(N)]
+
+
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+q = deque()
+
 for i in range(N):
     for j in range(M):
-        if tomato[i][j] == 1:
+        if board[i][j] == 1:
             q.append((i, j))
-            visited[i][j] = 1
 
-while q:
-    x, y = q.popleft()
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < N and 0 <= ny < M and tomato[nx][ny] == 0 and visited[nx][ny] == 0:
-            tomato[nx][ny] = 1
-            visited[nx][ny] = visited[x][y] + 1
-            q.append((nx, ny))
 
-flag = 0
-for i in range(N):
-    for j in range(M):
-        if tomato[i][j] == 0:
-            flag = 1
-            break
-    if flag:
-        break
+def bfs():
+    global q
 
-if flag:
-    print(-1)
-else:
-    print(max(map(max, visited)) - 1)
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in d:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
+                board[nx][ny] = board[x][y] + 1
+                q.append((nx, ny))
+    if check():
+        print(max(max(i for i in board[j]) for j in range(N)) - 1)
+    else:
+        print(-1)
+
+
+def check():
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] == 0:
+                return False
+    return True
+
+
+bfs()
