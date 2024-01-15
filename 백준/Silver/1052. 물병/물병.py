e884a253bac7d458d8ba97bfb2b27905d860cc08
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-15 12:05:13 +0900

    [Silver I] Title: 물병, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1052. 물병/물병.py
+++ 백준/Silver/1052. 물병/물병.py
@@ -1,18 +1,17 @@
 import sys
 
-sys.setrecursionlimit(10000)
-input = lambda: sys.stdin.readline().rstrip()
 
-def main():
-    N, K = map(int, input().split())
-    result = 0
-    while True:
-        if bin(N).count('1') <= K:
-            break
-        N += 1
-        result += 1
-    print(result)
+def input(): return sys.stdin.readline().rstrip()
 
 
-if __name__ == '__main__':
-    main()
+N, K = map(int, input().split())
+
+
+result = 0
+
+
+while bin(N).count('1') > K:
+    result += N & -N
+    N += N & -N
+
+print(result)
