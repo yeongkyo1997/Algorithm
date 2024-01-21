8ed26fb340d1fc804b3d9b8e316ff51bedb8cba1
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-21 12:24:13 +0900

    [Gold IV] Title: 플로이드, Time: 196 ms, Memory: 111560 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,28 +1,26 @@
-import math
 import sys
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 n = int(input())
 m = int(input())
-INF = math.inf
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
 
-for i in range(m):
+graph = [[float('inf')] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    graph[i][i] = 0
+
+
+for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a][b] = min(graph[a][b], c)
 
 for k in range(1, n + 1):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
-            if i == j:
-                continue
-            graph[i][j] = min(graph[i][k] + graph[k][j], graph[i][j])
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
 for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if graph[i][j] == INF:
-            graph[i][j] = 0
-        print(graph[i][j], end=' ')
-    print()
+    print(*[j if j != float('inf') else 0 for j in graph[i][1:]])
