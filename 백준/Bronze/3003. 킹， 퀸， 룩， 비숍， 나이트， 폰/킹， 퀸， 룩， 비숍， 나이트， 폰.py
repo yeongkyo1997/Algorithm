6cba93efed40ad9ede67a478756cf4e0d8ebcd26
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-21 13:25:56 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,6 +1,1 @@
-pieces = list(map(int, input().split()))
-correct_pieces = [1, 1, 2, 2, 2, 8]
-
-for i in range(len(pieces)):
-    diff = correct_pieces[i] - pieces[i]
-    print(diff, end=" ")
+print(*[b - a for a, b in zip(list(map(int, input().split())), [1, 1, 2, 2, 2, 8])])
