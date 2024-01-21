93d14397c2b1b1ab35c946d6c6877ae226f61d7c
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-21 16:23:09 +0900

    [Gold III] Title: 줄 세우기, Time: 252 ms, Memory: 127356 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,26 +1,32 @@
 import sys
+from collections import deque, defaultdict
 
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 N, M = map(int, input().split())
+graph = defaultdict(list)
+in_degree = defaultdict(int)
+result = []
 
-graph = [[] for _ in range(N + 1)]
-indegree = [0] * (N + 1)
-
-for i in range(M):
+for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
-    indegree[b] += 1
+    in_degree[b] += 1
 
-q = []
+q = deque()
 for i in range(1, N + 1):
-    if indegree[i] == 0:
+    if in_degree[i] == 0:
         q.append(i)
 
 while q:
-    cur = q.pop(0)
-    print(cur, end=' ')
+    cur = q.popleft()
+    result.append(cur)
+
     for i in graph[cur]:
-        indegree[i] -= 1
-        if indegree[i] == 0:
+        in_degree[i] -= 1
+        if in_degree[i] == 0:
             q.append(i)
+
+print(*result)
