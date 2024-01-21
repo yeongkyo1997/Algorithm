f9afeb67a36ede8cd882eece96af55b2d749d67d
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-21 12:58:12 +0900

    [Bronze III] Title: 공 넣기, Time: 120 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,17 +1,16 @@
 import sys
 
-sys.setrecursionlimit(1000000)
-input = lambda: sys.stdin.readline().rstrip()
 
-# BOJ 10810 - 공 바꾸기
-def main():
-    N, M = map(int, input().split())
-    arr = [0] * N
-    for _ in range(M):
-        i, j, k = map(int, input().split())
-        for l in range(i - 1, j):
-            arr[l] = k
-    print(*arr)
+def input(): return sys.stdin.readline().rstrip()
 
-if __name__ == '__main__':
-    main()
+
+N, M = map(int, input().split())
+
+arr = [0]
+arr += [0] * N
+
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    arr = arr[:a] + [c] * (b - a + 1) + arr[b + 1:]
+
+print(*arr[1:])
