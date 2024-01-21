6c3dabe0137a62695db9f7b65890f583cdff6584
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-21 12:41:07 +0900

    [Bronze IV] Title: 주사위 세개, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,10 +1,17 @@
-arr, b, c = map(int, input().split())
+import sys
 
-if arr == b == c:
-    print(10000 + arr * 1000)
-elif arr == b or arr == c:
-    print(1000 + arr * 100)
-elif b == c:
-    print(1000 + b * 100)
+
+def input(): return sys.stdin.readline().rstrip()
+
+
+arr = list(map(int, input().split()))
+s = set(arr)
+
+if len(s) == 1:
+    print(10000 + arr[0] * 1000)
+elif len(s) == 2:
+    for i in s:
+        if arr.count(i) == 2:
+            print(1000 + i * 100)
 else:
-    print(max(arr, b, c) * 100)
+    print(max(arr) * 100)
