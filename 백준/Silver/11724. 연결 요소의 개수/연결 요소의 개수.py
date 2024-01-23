9014643e5f9b7df183762490c2049c71e0fc250b
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-23 16:18:20 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 324 ms, Memory: 111484 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,31 +1,39 @@
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
 
 
-# BOJ 11724 연결 요소의 개수
-def main():
-    n, m = map(int, input().split())
-    graph = [[] for _ in range(n + 1)]
-    visited = [False] * (n + 1)
-    for _ in range(m):
-        a, b = map(int, input().split())
-        graph[a].append(b)
-        graph[b].append(a)
-
-    def dfs(v):
-        visited[v] = True
-        for i in graph[v]:
-            if not visited[i]:
-                dfs(i)
-
-    cnt = 0
-    for i in range(1, n + 1):
-        if not visited[i]:
-            dfs(i)
-            cnt += 1
-    print(cnt)
+N, M = map(int, input().split())
+parent = {i: i for i in range(1, N + 1)}
 
 
-if __name__ == '__main__':
-    main()
+def find(x):
+    if parent[x] == x:
+        return parent[x]
+
+    parent[x] = find(parent[x])
+
+    return parent[x]
+
+
+def union(a, b):
+    a = find(parent[a])
+    b = find(parent[b])
+    if a == b:
+        return
+
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    union(a, b)
+
+for i in range(1, N + 1):
+    parent[i] = find(parent[i])
+
+print(len(set(parent.values())))
