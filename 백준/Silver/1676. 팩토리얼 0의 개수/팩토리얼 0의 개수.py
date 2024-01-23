922cdff3528b8a48b9c1b93e16e5b1924e87864c
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-23 23:06:22 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,7 +1,13 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
 N = int(input())
 
-print(N // 5 + N // 25 + N // 125)
+two = 0
+five = 0
+for i in range(1, N + 1):
+    while i % 2 == 0:
+        two += 1
+        i //= 2
+    while i % 5 == 0:
+        five += 1
+        i //= 5
+
+print(min(two, five))
