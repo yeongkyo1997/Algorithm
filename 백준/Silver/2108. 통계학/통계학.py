7bf87fd789eb48ec6eba267a0d09fcf706f0076d
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-24 00:18:30 +0900

    [Silver III] Title: 통계학, Time: 400 ms, Memory: 122992 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,23 +1,28 @@
-import sys
+from collections import defaultdict
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
 
-from collections import Counter
-
-n = int(input())
-arr = [int(input()) for _ in range(n)]
+N = int(input())
+arr = [int(input()) for _ in range(N)]
 arr.sort()
+print(round(sum(arr) / len(arr)))
+print(arr[N // 2])
 
-print(round(sum(arr) / n))
+lib = defaultdict(int)
 
-print(arr[n // 2])
+for i in arr:
+    lib[i] += 1
 
-cnt = Counter(arr)
-cnt = cnt.most_common()
-if len(cnt) > 1 and cnt[0][1] == cnt[1][1]:
-    print(cnt[1][0])
+max_val = max(lib.values())
+
+max_list = []
+
+for key, val in lib.items():
+    if val == max_val:
+        max_list.append(key)
+
+if len(max_list) >= 2:
+    print(max_list[1])
 else:
-    print(cnt[0][0])
+    print(max_list[0])
 
-print(arr[-1] - arr[0])
+print(max(arr) - min(arr))
