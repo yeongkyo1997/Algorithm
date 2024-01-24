8e28e03b4843154fd85ad129ccc0ad0c8a6a7dbd
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-24 20:38:14 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 160 ms, Memory: 110560 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,23 +1,10 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
-
-def main():
-    quarter = 25
-    dime = 10
-    nickel = 5
-    penny = 1
-
-    for _ in range(int(input())):
-        n = int(input())
-        print(n // quarter, end=' ')
-        n %= quarter
-        print(n // dime, end=' ')
-        n %= dime
-        print(n // nickel, end=' ')
-        n %= nickel
-        print(n // penny)
-
-
-main()
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    print(n // 25, end=' ')
+    n %= 25
+    print(n // 10, end=' ')
+    n %= 10
+    print(n // 5, end=' ')
+    n %= 5
+    print(n)
