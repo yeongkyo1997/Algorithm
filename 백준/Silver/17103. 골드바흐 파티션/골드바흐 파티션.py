db28fffe1e6beb44724982a249494d4b019080b9
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-24 18:13:29 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 296 ms, Memory: 118228 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,24 +1,16 @@
-import sys
+MAX = 1000001
+arr = [True] * MAX
+arr[1] = False
 
-input = sys.stdin.readline
+for i in range(2, MAX):
+    if arr[i]:
+        for j in range(i * i, MAX, i):
+            arr[j] = False
 
-
-def makePrime(n):
-    p = [False] * 2 + [True] * (n + 1)
-    for i in range(2, int(n ** 0.5) + 1):
-        if p[i]:
-            for j in range(i + i, n + 1, i):
-                p[j] = False
-    return p
-
-
-prime = makePrime(1000000)
-for t in range(int(input())):
-    N = int(input())
-
+for _ in range(int(input())):
+    n = int(input())
     result = 0
-    for i in range(N // 2 + 1):
-
-        if prime[i] and prime[N - i]:
+    for i in range(2, n // 2 + 1):
+        if arr[i] and arr[n - i]:
             result += 1
     print(result)
