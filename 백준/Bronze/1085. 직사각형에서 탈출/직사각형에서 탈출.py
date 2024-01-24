75ea1ab28c51eb026a2eb905f963cdaabbc54c03
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-24 12:07:18 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -1,7 +1,2 @@
-import sys
-
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
-
 x, y, w, h = map(int, input().split())
-print(min(x, y, w - x, h - y))
+print(min(abs(x - w), abs(y - h), abs(x - 0), abs(y - 0)))
