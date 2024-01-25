a63e04caa3b6ecad8268803374b5be6a3902c230
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-25 15:14:44 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 120 ms, Memory: 237992 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,23 +1,17 @@
-import functools
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10 ** 5)
+
+n = int(input())
 
 
-def main():
-    n = int(input())
-    print(fibo(n))
-
-
-@functools.cache
 def fibo(n):
     if n == 0:
         return 0
-    if n == 1 or n == 2:
+    elif n <= 2:
         return 1
 
     return fibo(n - 1) + fibo(n - 2)
 
 
-if __name__ == '__main__':
-    main()
+print(fibo(n))
