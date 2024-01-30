2d5bbbeff16458d3d4682df39ce2fd2fe7388c21
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-30 21:37:29 +0900

    [Silver I] Title: 쿼드트리, Time: 152 ms, Memory: 111696 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,35 +1,36 @@
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 N = int(input())
 
-arr = [list(map(int, list(input()))) for _ in range(N)]
+board = [list(map(int, (input()))) for _ in range(N)]
 
 
-def check(n, x, y):
-    val = arr[x][y]
+def solution(row, col, N):
+    tmp = board[row][col]
+    check = True
+    for i in range(row, row + N):
+        for j in range(col, col + N):
+            if board[i][j] != tmp:
+                check = False
+                break
+        else:
+            continue
+        break
 
-    for i in range(x, x + n):
-        for j in range(y, y + n):
-            if val != arr[i][j]:
-                return False
-    return True
+    if not check:
+        print('(', end='')
+        N //= 2
+        solution(row, col, N)
+        solution(row, col + N, N)
+        solution(row + N, col, N)
+        solution(row + N, col + N, N)
+        print(')', end='')
+    else:
+        print(tmp, end='')
 
 
-def solve(n, x, y):
-    if check(n, x, y):
-        print(arr[x][y], end='')
-        return
-
-    mid = n // 2
-
-    print('(', end='')
-    solve(mid, x, y)
-    solve(mid, x, y + mid)
-    solve(mid, x + mid, y)
-    solve(mid, x + mid, y + mid)
-    print(')', end='')
-
-
-solve(N, 0, 0)
+solution(0, 0, N)
