ec7e80134cddd72445c0e3a3832a926a4db58e42
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-31 23:31:53 +0900

    [Gold IV] Title: 오큰수, Time: 572 ms, Memory: 256128 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -5,15 +5,16 @@ def input(): return sys.stdin.readline().rstrip()
 
 
 N = int(input())
-A = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
 stack = []
 result = [-1] * N
+
 for i in range(N):
-    if not stack:
-        stack.append(i)
-    else:
-        while stack and A[stack[-1]] < A[i]:
-            result[stack.pop()] = A[i]
-        stack.append(i)
+    while stack and arr[stack[-1]] < arr[i]:
+        result[stack.pop()] = arr[i]
+
+    stack.append(i)
+
+
 print(*result)
