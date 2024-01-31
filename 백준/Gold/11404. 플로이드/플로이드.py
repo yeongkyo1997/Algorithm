9e86c5928f6ee3efe8020087ee5042bad289eb61
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-31 16:57:45 +0900

    [Gold IV] Title: 플로이드, Time: 200 ms, Memory: 111572 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -9,10 +9,6 @@ m = int(input())
 
 graph = [[float('inf')] * (n + 1) for _ in range(n + 1)]
 
-for i in range(1, n + 1):
-    graph[i][i] = 0
-
-
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a][b] = min(graph[a][b], c)
@@ -20,7 +16,11 @@ for _ in range(m):
 for k in range(1, n + 1):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
+            if i == j:
+                graph[i][j] = 0
+                continue
             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
-for i in range(1, n + 1):
-    print(*[j if j != float('inf') else 0 for j in graph[i][1:]])
+
+for i in graph[1:]:
+    print(*[j if j != float('inf') else 0 for j in i[1:]])
