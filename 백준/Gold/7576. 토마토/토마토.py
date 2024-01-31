addaf66325cf16d78fbfae12c51d44fdb083d77e
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-31 16:47:40 +0900

    [Gold V] Title: 토마토, Time: 372 ms, Memory: 156860 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -5,45 +5,33 @@ from collections import deque
 def input(): return sys.stdin.readline().rstrip()
 
 
-M, N = map(int, input().split())
+N, M = map(int, input().split())
 
-board = [list(map(int, input().split())) for _ in range(N)]
-
-
-d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+board = [list(map(int, input().split())) for _ in range(M)]
 
 q = deque()
 
-for i in range(N):
-    for j in range(M):
+for i in range(M):
+    for j in range(N):
         if board[i][j] == 1:
             q.append((i, j))
 
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-def bfs():
-    global q
+while q:
+    x, y = q.popleft()
 
-    while q:
-        x, y = q.popleft()
+    for dx, dy in d:
+        nx, ny = x + dx, y + dy
 
-        for dx, dy in d:
-            nx, ny = x + dx, y + dy
+        if 0 <= nx < M and 0 <= ny < N and board[nx][ny] == 0:
+            board[nx][ny] = board[x][y] + 1
+            q.append((nx, ny))
 
-            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
-                board[nx][ny] = board[x][y] + 1
-                q.append((nx, ny))
-    if check():
-        print(max(max(i for i in board[j]) for j in range(N)) - 1)
-    else:
-        print(-1)
+for i in range(M):
+    for j in range(N):
+        if board[i][j] == 0:
+            print(-1)
+            exit(0)
 
-
-def check():
-    for i in range(N):
-        for j in range(M):
-            if board[i][j] == 0:
-                return False
-    return True
-
-
-bfs()
+print(max(max(i) for i in board) - 1)
