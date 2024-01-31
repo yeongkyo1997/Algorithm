d6b43dc370cdc8b03d34fc7326e85f3bc4fc3da2
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-31 17:56:56 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2516 ms, Memory: 272444 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -5,15 +5,14 @@ def input(): return sys.stdin.readline().rstrip()
 
 
 N, M = map(int, input().split())
-
 graph = [list(map(int, input().split())) for _ in range(M)]
-graph.sort(key=lambda x: x[2])
+
 
 parent = [i for i in range(N + 1)]
 
 
 def find(a):
-    if a == parent[a]:
+    if parent[a] == a:
         return a
 
     parent[a] = find(parent[a])
@@ -28,12 +27,15 @@ def union(a, b):
     if a == b:
         return False
 
-    parent[max(a, b)] = min(a, b)
+    parent[a] = b
     return True
 
 
+graph.sort(key=lambda x: x[2])
+
 result = 0
 last = 0
+
 for a, b, c in graph:
     if union(a, b):
         result += c
