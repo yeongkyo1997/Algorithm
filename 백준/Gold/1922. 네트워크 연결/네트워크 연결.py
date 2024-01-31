2b0860e394280dc1d4d5ce23ee96def3618ea988
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-31 14:48:24 +0900

    [Gold IV] Title: 네트워크 연결, Time: 316 ms, Memory: 124240 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,56 +1,43 @@
-import collections
-import math
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 N = int(input())
 M = int(input())
+
+graph = [list(map(int, input().split())) for _ in range(M)]
 parent = [i for i in range(N + 1)]
-dist = collections.defaultdict(int)
-graph = collections.defaultdict(list)
-
-
-def main():
-    for i in range(M):
-        a, b, c = map(int, input().split())
-        graph[a].append((b, c))
-        graph[b].append((a, c))
-    print(kruskal())
-
-
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a == b:
-        return False
-    if dist[a] < dist[b]:
-        a, b = b, a
-    parent[b] = a
-    if dist[a] == dist[b]:
-        dist[a] += 1
-    return True
 
 
 def find(a):
-    if parent[a] == a:
+    if a == parent[a]:
         return a
+
     parent[a] = find(parent[a])
     return parent[a]
 
 
-def kruskal():
-    edges = []
-    for i in graph:
-        for j, c in graph[i]:
-            edges.append((c, i, j))
-    edges.sort()
-    result = 0
-    for c, a, b in edges:
-        if union(a, b):
-            result += c
-    return result
+def union(a, b):
+    a = find(parent[a])
+    b = find(parent[b])
+
+    if a == b:
+        return False
+
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+    return True
 
 
-if __name__ == '__main__':
-    main()
+graph.sort(key=lambda x: x[2])
+result = 0
+for a, b, c in graph:
+    if union(a, b):
+        result += c
+
+print(result)
