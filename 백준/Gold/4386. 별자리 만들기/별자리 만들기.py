f56a337f161aa76dcb9c4a54a2116763adeb082f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-31 18:16:10 +0900

    [Gold III] Title: 별자리 만들기, Time: 136 ms, Memory: 111308 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.py
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.py
@@ -1,64 +1,49 @@
 import sys
 
-sys.setrecursionlimit(10 ** 5)
-
 
 def input(): return sys.stdin.readline().rstrip()
 
 
-n = int(input())
+def find(a):
+    if parent[a] == a:
+        return a
 
+    parent[a] = find(parent[a])
 
-graph = []
-
-
-def get_distance(a, b):
-    return ((a[0] - b[0])** 2 + (a[1] - b[1]) ** 2) ** 0.5
-
-
-point = []
-for i in range(n):
-    a, b = map(float, input().split())
-    point.append((a, b))
-
-
-def find(x):
-    global parent
-    if parent[x] == x:
-        return x
-
-    parent[x] = find(parent[x])
-
-    return parent[x]
+    return parent[a]
 
 
 def union(a, b):
-    global parent
     a = find(parent[a])
     b = find(parent[b])
 
     if a == b:
         return False
 
-    if a > b:
+    if rank[a] > rank[b]:
+        parent[b] = a
+    elif rank[a] < rank[b]:
         parent[a] = b
     else:
-        parent[b] = a
+        parent[a] = b
+        rank[b] += 1
 
     return True
 
 
-distance = []
-for i in range(n):
-    for j in range(i + 1, n):
-        distance.append((i, j, get_distance(point[i], point[j])))
+N = int(input())
+board = [list(map(float, input().split())) for _ in range(N)]
 
-distance.sort(key=lambda x: x[2])
-parent = [i for i in range(len(distance))]
+parent = [i for i in range(N + 1)]
+rank = [0] * (N + 1)
 
-result = 0
-for a, b, c in distance:
-    if union(a, b):
-        result += c
+graph = []
+for i in range(N):
+    for j in range(i + 1, N):
+        graph.append(
+            (i, j, ((board[i][0] - board[j][0]) ** 2 + (board[i][1] - board[j][1]) ** 2) ** 0.5))
 
-print(result)
+
+graph.sort(key=lambda x: x[2])
+
+print(sum(cost for a, b, cost in graph if union(a, b)))
