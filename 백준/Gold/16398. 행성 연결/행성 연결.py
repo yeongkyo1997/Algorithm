937acd85bb5be00ca8822b68dbfd3376fd40649c
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-01-31 18:08:28 +0900

    [Gold IV] Title: 행성 연결, Time: 2512 ms, Memory: 291276 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.py
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -4,26 +4,12 @@ import sys
 def input(): return sys.stdin.readline().rstrip()
 
 
-N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
-
-graph = []
-
-for i in range(N):
-    for j in range(N):
-        if i == j:
-            continue
-        graph.append((i, j, board[i][j]))
-
-parent = [i for i in range(N + 1)]
-graph = sorted(graph, key=lambda x: x[2])
-
-
 def find(a):
     if parent[a] == a:
         return a
 
     parent[a] = find(parent[a])
+
     return parent[a]
 
 
@@ -34,12 +20,34 @@ def union(a, b):
     if a == b:
         return False
 
-    parent[a] = b
+    if rank[a] > rank[b]:
+        parent[b] = a
+    elif rank[a] < rank[b]:
+        parent[a] = b
+    else:
+        parent[a] = b
+        rank[b] += 1
 
     return True
 
 
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+graph = []
+
+for i in range(N):
+    for j in range(N):
+        if i == j:
+            continue
+        graph.append((i, j, board[i][j]))
+
+parent = [i for i in range(N + 1)]
+rank = [0] * (N + 1)
+
+graph.sort(key=lambda x: x[2])
+
 result = 0
+
 for a, b, c in graph:
     if union(a, b):
         result += c
