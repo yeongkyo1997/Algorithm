27d44b3ccdee5124f7d4709c05cd384a35e564b0
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-02-01 13:23:54 +0900

    [Silver IV] Title: 차집합, Time: 704 ms, Memory: 266724 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.py
+++ 백준/Silver/1822. 차집합/차집합.py
@@ -1,24 +1,16 @@
 import sys
 
-sys.setrecursionlimit(10000)
-input = lambda: sys.stdin.readline().rstrip()
 
-def main():
-    a, b = map(int, input().split())
-    arr1 = list(map(int, input().split()))
-    arr2 = list(map(int, input().split()))
-    
-    set1 = set(arr1)
-    set2 = set(arr2)
-    
-    minus = set1 - set2
-    
-    if len(minus) == 0:
-        print(0)
-    else:
-        print(len(minus))
-        for i in sorted(minus):
-            print(i, end=' ')
+def input(): return sys.stdin.readline().rstrip()
 
-if __name__ == '__main__':
-    main()
+
+n_a, n_b = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+result = set(a) - set(b)
+
+if result:
+    print(len(result))
+    print(*sorted(result))
+else:
+    print(0)
