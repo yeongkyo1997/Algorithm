92b8882fdabb5111e2f62681488aebc34855c0a4
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-02-11 14:13:44 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 136 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,19 +1,22 @@
+import java.util.*;
 import java.io.*;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        int a, b;
         st = new StringTokenizer(br.readLine());
-        BigInteger a = new BigInteger(st.nextToken());
-        BigInteger b = new BigInteger(st.nextToken());
-
-        bw.write(a.gcd(b) + "\n");
-        bw.write((a.multiply(b).divide(a.gcd(b))) + "");
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        bw.write(String.valueOf(gcd(a, b)) + "\n");
+        bw.write(String.valueOf((a * b) / gcd(a, b)));
         bw.close();
+    }
+
+    static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
