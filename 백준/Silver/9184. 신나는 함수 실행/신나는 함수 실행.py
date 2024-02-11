78edc30c727a3f278fbf3390cadfce48f3386943
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-02-11 11:34:24 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 304 ms, Memory: 122772 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,7 +1,8 @@
 import sys
 from functools import cache
 
-input = lambda: sys.stdin.readline().rstrip()
+
+def input(): return sys.stdin.readline().rstrip()
 
 
 @cache
@@ -12,20 +13,13 @@ def w(a, b, c):
     if a > 20 or b > 20 or c > 20:
         return w(20, 20, 20)
 
-    if a < b < c:
-        return w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
-
-    return w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1)
-
-
-def main():
-    while True:
-        a, b, c = map(int, input().split())
-
-        if a == b == c == -1:
-            break
-        print(f'w({a}, {b}, {c}) = {w(a, b, c)}')
+    if a < b and b < c:
+        return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+    return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
 
 
-if __name__ == "__main__":
-    main()
+while True:
+    a, b, c = map(int, input().split())
+    if a == b == c == -1:
+        break
+    print(f'w({a}, {b}, {c}) = {w(a, b, c)}')
