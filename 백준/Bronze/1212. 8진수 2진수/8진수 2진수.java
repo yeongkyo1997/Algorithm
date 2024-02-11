fda16590123d94e1f0dec6d80ea44a813452074f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-02-11 13:18:54 +0900

    [Bronze II] Title: 8진수 2진수, Time: 336 ms, Memory: 25632 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -1,37 +1,28 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-
-    static int s = 0;
+    static String N;
+    static String[] bin = { "000", "001", "010", "011", "100", "101", "110", "111" };
 
     public static void main(String[] args) throws Exception {
-        String input = br.readLine();
-        int len = input.length();
+        N = br.readLine();
+        StringBuilder sb = new StringBuilder();
 
-        if (input.charAt(0) != '0') {
-            for (int i = 0; i < len; i++) binary(input.charAt(i) - 48, i);
-        } else bw.write("0");
-        bw.close();
-    }
-
-    static void binary(int n, int i) throws Exception {
-        if (i == 0 && n <= 3) {
-            s = 1;
-            if (n == 1) s = 0;
-        } else s = 2;
-
-        for (int j = s; j >= 0; j--) {
-            if (n >= Math.pow(2, j)) {
-                bw.write("1");
-                n -= (int) Math.pow(2, j);
-            } else bw.write("0");
+        for (int i = 0; i < N.length(); i++) {
+            sb.append(bin[N.charAt(i) - '0']);
         }
+        if (N.equals("0"))
+            bw.write('0');
+        else {
+            while (sb.charAt(0) == '0') {
+                sb = new StringBuilder(sb.substring(1));
+            }
+            bw.write(sb.toString());
+        }
+        bw.close();
     }
 }
