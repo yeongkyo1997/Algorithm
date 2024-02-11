38e29f6a6abb9d861c5a1b1d71c60d5aa6479e8d
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-02-11 12:23:26 +0900

    [Bronze V] Title: 긴자리 계산, Time: 148 ms, Memory: 15196 KB -BaekjoonHub

--- 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -1,20 +1,19 @@
+import java.util.*;
 import java.io.*;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.math.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] argrs) throws Exception {
         BigInteger A = new BigInteger(br.readLine());
         BigInteger B = new BigInteger(br.readLine());
 
-        bw.write(A.add(B) + "\n");
-        bw.write(A.subtract(B) + "\n");
-        bw.write(A.multiply(B) + "\n");
-
+        bw.write(String.valueOf(A.add(B)) + "\n");
+        bw.write(String.valueOf(A.subtract(B)) + "\n");
+        bw.write(String.valueOf(A.multiply(B)) + "\n");
         bw.close();
     }
 }
