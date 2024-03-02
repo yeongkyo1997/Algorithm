fd822b2470894726c5415e855058b2c36ecd445b
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-03-02 11:22:34 +0900

    [Gold V] Title: 합분해, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def input(): return sys.stdin.readline().rstrip()
+
+
+MOD = 1_000_000_000
+
+N, K = map(int, input().split())
+
+
+dp = [[0] * (K + 1) for _ in range(N + 1)]
+
+
+for j in range(K + 1):
+    dp[0][j] = 1
+
+
+for i in range(1, N + 1):
+    for j in range(1, K + 1):
+        dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % MOD
+
+print(dp[N][K])
