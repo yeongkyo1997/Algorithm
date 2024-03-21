173d752e09e50b9499ffec7b66fce83f7c113485
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-03-21 09:46:54 +0900

    [Bronze III] Title: 더하기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/9085. 더하기/더하기.py
+++ 백준/Bronze/9085. 더하기/더하기.py
@@ -1,12 +1,11 @@
 import sys
 
-sys.setrecursionlimit(1000000)
-input = lambda: sys.stdin.readline().rstrip()
 
-def main():
-    for _ in range(int(input())):
-        N = int(input())
-        print(sum(map(int, input().split())))
+def input(): return sys.stdin.readline().rstrip()
 
-if __name__ == '__main__':
-    main()
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    print(sum(map(int, input().split())))
