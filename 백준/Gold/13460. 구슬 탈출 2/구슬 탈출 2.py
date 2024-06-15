5831ab952460a3b8ad2680aa78a707542503344a
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-06-15 15:06:25 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 124 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -1,66 +1,69 @@
-from collections import deque
 import sys
+from collections import deque
 
 
 def input(): return sys.stdin.readline().rstrip()
 
 
-def move(x, y, dx, dy, board):
+N, M = map(int, input().split())
+
+graph = []
+visited = []
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+for i in range(N):
+    graph.append(list(input()))
+rx, ry, bx, by = [0] * 4
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 'R':
+            rx, ry = i, j
+        if graph[i][j] == 'B':
+            bx, by = i, j
+
+
+def move(x, y, dx, dy):
     cnt = 0
-    while board[x + dx][y + dy] != '#' and board[x][y] != 'O':
+    while graph[x + dx][y + dy] != '#' and graph[x][y] != 'O':
         x += dx
         y += dy
         cnt += 1
     return x, y, cnt
 
 
-def bfs(board, N, M):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+q = deque()
+q.append((rx, ry, bx, by, 1))
+visited.append((rx, ry, bx, by))
 
-    for i in range(N):
-        for j in range(M):
-            if board[i][j] == 'R':
-                rx, ry = i, j
-            if board[i][j] == 'B':
-                bx, by = i, j
+while q:
+    rx, ry, bx, by, result = q.popleft()
 
-    q = deque()
-    q.append((rx, ry, bx, by, 1))
+    if result > 10:
+        break
 
-    visited = []
-    visited.append((rx, ry, bx, by))
+    for i in range(4):
+        nrx, nry, rcnt = move(rx, ry, dx[i], dy[i])
+        nbx, nby, bcnt = move(bx, by, dx[i], dy[i])
 
-    while q:
-        rx, ry, bx, by, depth = q.popleft()
+        if graph[nbx][nby] == 'O':
+            continue
 
-        if depth > 10:
-            break
+        if graph[nrx][nry] == 'O':
+            print(result)
+            exit(0)
 
-        for i in range(4):
-            nrx, nry, r_cnt = move(rx, ry, dx[i], dy[i], board)
-            nbx, nby, b_cnt = move(bx, by, dx[i], dy[i], board)
+        if nrx == nbx and nry == nby:
+            if rcnt > bcnt:
+                nrx -= dx[i]
+                nry -= dy[i]
+            else:
+                nbx -= dx[i]
+                nby -= dy[i]
 
-            if board[nbx][nby] != 'O':
-                if board[nrx][nry] == 'O':
-                    return depth
-
-                if nrx == nbx and nry == nby:
-                    if r_cnt > b_cnt:
-                        nrx -= dx[i]
-                        nry -= dy[i]
-                    else:
-                        nbx -= dx[i]
-                        nby -= dy[i]
-
-                if (nrx, nry, nbx, nby) not in visited:
-                    visited.append((nrx, nry, nbx, nby))
-                    q.append((nrx, nry, nbx, nby, depth + 1))
-
-    return -1
-
-
-N, M = map(int, input().split())
-board = [input()for _ in range(N)]
-
-print(bfs(board, N, M))
+        if (nrx, nry, nbx, nby) not in visited:
+            visited.append((nrx, nry, nbx, nby))
+            q.append((nrx, nry, nbx, nby, result + 1))
+print(-1)
