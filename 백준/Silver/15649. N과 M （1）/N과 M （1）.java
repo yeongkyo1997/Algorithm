cdf22e994e3d50873260713c356ba3f19083155a
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-07-01 16:42:04 +0900

    [Silver III] Title: N과 M (1), Time: 408 ms, Memory: 34292 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,41 +1,45 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static int input[];
-    static int numbers[];
-    static int N, M;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static int N, M;
+	static ArrayList<int[]> result = new ArrayList<>();
+	static boolean[] visited;
 
-    public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        input = new int[N];
-        numbers = new int[M];
-        for (int i = 0; i < N; i++) {
-            input[i] = i + 1;
-        }
-        perm(0, 0);
-        bw.close();
-    }
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		visited = new boolean[N + 1];
+		
+		permutation(new int[M], 0);
+		
+		for (int[] r : result) {
+			for (int i : r) {
+				bw.write(i + " ");
+			}
+			bw.newLine();
+		}
 
-    static void perm(int cnt, int flag) throws IOException {
-        if (cnt == M) {
-            for (int number : numbers) {
-                bw.write(number + " ");
-            }
-            bw.write("\n" + "");
-            return;
-        }
+		bw.close();
+	}
 
-        for (int i = 0; i < N; i++) {
-            if ((flag & 1 << i) != 0) continue;
+	static void permutation(int[] arr, int depth) {
+		if (depth == M) {
+			result.add(arr.clone());
+			return;
+		}
 
-            numbers[cnt] = input[i];
-            perm(cnt + 1, (flag | 1 << i));
-        }
-    }
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				arr[depth] = i;
+				visited[i] = true;
+				permutation(arr, depth + 1);
+				visited[i] = false;
+			}
+		}
+	}
 }
