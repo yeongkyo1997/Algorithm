9c704a049af8429183a2c8b20cce578edf0f4db7
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-07-24 17:37:25 +0900

    [Gold III] Title: 경사로, Time: 124 ms, Memory: 110668 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.py
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -1,44 +1,51 @@
-import sys
+N, L = map(int, input().rstrip().split())
+
+board = [list(map(int, input().rstrip().split())) for _ in range(N)]
 
 
-def input(): return sys.stdin.readline().rstrip()
-
-
-N, L = map(int, input().split())
-
-board = [list(map(int, input().split())) for _ in range(N)]
-
-
-def check(road):
-    used = [False] * N
-    for i in range(1, N):
-        if abs(road[i] - road[i - 1]) > 1:
+def check(arr):
+    visited = [False] * N
+    for i in range(N - 1):
+        if abs(arr[i] - arr[i + 1]) > 1:
             return False
-        else:
-            if road[i] < road[i - 1]:
-                for j in range(L):
-                    if i + j >= N or used[i + j] or road[i + j] != road[i]:
-                        return False
-                    if road[i] == road[i + j]:
-                        used[i + j] = True
-            elif road[i] > road[i - 1]:
-                for j in range(1, L + 1):
-                    if i - j < 0 or used[i - j] or road[i - j] != road[i - 1]:
-                        return False
-                    if road[i - 1] == road[i - j]:
-                        used[i - j] = True
 
+        if arr[i] == arr[i + 1]:
+            continue
+        # 다음이 더 크다면
+        elif arr[i] < arr[i + 1]:
+            for j in range(i, i - L, -1):
+                if 0 <= j < N:
+                    if arr[i] != arr[j] or visited[j]:
+                        return False
+                    visited[j] = True
+                else:
+                    return False
+
+        # 다음이 더 작으면
+        else:
+            for j in range(i + 1, i + L + 1):
+                if 0 <= j < N:
+                    if arr[i + 1] != arr[j] or visited[j]:
+                        return False
+                    visited[j] = True
+                else:
+                    return False
     return True
 
 
 result = 0
-for road in board:
-    if check(road):
+# 행
+for i in range(N):
+    if check(board[i]):
         result += 1
 
-for i in range(N):
-    road = [board[j][i] for j in range(N)]
-    if check(road):
+# 열
+for col in range(N):
+    arr = []
+    for row in range(N):
+        arr.append(board[row][col])
+
+    if check(arr):
         result += 1
 
 print(result)
