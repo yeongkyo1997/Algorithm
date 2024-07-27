c48fc8e2bb816637a2962c7aedceddda69ad0999
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-07-27 12:40:06 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1776 ms, Memory: 317924 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -11,24 +11,23 @@ board = [list(map(int, input().rstrip())) for _ in range(N)]
 
 def bfs():
     q = collections.deque()
-    visited = collections.defaultdict(lambda: math.inf)
+    visited = [[[math.inf] * 2 for _ in range(M)] for _ in range(N)]
     q.append((0, 0, 0))
-    visited[(0, 0, 0)] = 1
-
+    visited[0][0][0] = 1
     while q:
         x, y, flag = q.popleft()
         if x == N - 1 and y == M - 1:
-            return visited[(x, y, flag)]
+            return visited[x][y][flag]
 
         for d in range(4):
             nx, ny = x + dx[d], y + dy[d]
 
             if 0 <= nx < N and 0 <= ny < M:
-                if board[nx][ny] == 0 and visited[(nx, ny, flag)] > visited[(x, y, flag)] + 1:
-                    visited[(nx, ny, flag)] = visited[(x, y, flag)] + 1
+                if board[nx][ny] == 0 and visited[nx][ny][flag] > visited[x][y][flag] + 1:
+                    visited[nx][ny][flag] = visited[x][y][flag] + 1
                     q.append((nx, ny, flag))
-                elif board[nx][ny] == 1 and flag == 0 and visited[(nx, ny, 1)] > visited[(x, y, flag)] + 1:
-                    visited[(nx, ny, 1)] = visited[(x, y, flag)] + 1
+                elif board[nx][ny] == 1 and flag == 0 and visited[nx][ny][1] > visited[x][y][flag] + 1:
+                    visited[nx][ny][1] = visited[x][y][flag] + 1
                     q.append((nx, ny, 1))
 
     return -1
