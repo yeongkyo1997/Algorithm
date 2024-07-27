229cfb6756e55d99b4de081251c2a00556e19e27
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-07-27 12:50:58 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 6496 ms, Memory: 458728 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -1,36 +1,36 @@
-from collections import deque
-import sys
+import collections
+import math
+
+N, M, K = map(int, input().rstrip().split())
+
+board = [list(map(int, input().rstrip())) for _ in range(N)]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
 
-def input(): return sys.stdin.readline().rstrip()
+def bfs():
+    q = collections.deque()
+    visited = [[[math.inf] * (K + 1) for _ in range(M)] for _ in range(N)]
+    visited[0][0][0] = 1
+    q.append((0, 0, 0))
+
+    while q:
+        x, y, w = q.popleft()
+        if x == N - 1 and y == M - 1:
+            return visited[x][y][w]
+
+        for d in range(4):
+            nx, ny = x + dx[d], y + dy[d]
+
+            if 0 <= nx < N and 0 <= ny < M:
+                if board[nx][ny] == 0 and visited[nx][ny][w] > visited[x][y][w] + 1:
+                    visited[nx][ny][w] = visited[x][y][w] + 1
+                    q.append((nx, ny, w))
+                elif board[nx][ny] == 1 and w + 1 <= K and visited[nx][ny][w + 1] > visited[x][y][w] + 1:
+                    visited[nx][ny][w + 1] = visited[x][y][w] + 1
+                    q.append((nx, ny, w + 1))
+
+    return -1
 
 
-N, M, K = map(int, input().split())
-
-visited = [[[float('inf')] * (K + 1) for _ in range(M)]for _ in range(N)]
-visited[0][0][0] = 1
-board = [list(map(int, input())) for _ in range(N)]
-
-d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-q = deque([(0, 0, 0)])
-
-while q:
-    x, y, wall = q.popleft()
-    if x == N - 1 and y == M - 1:
-        print(min(visited[x][y]))
-        break
-
-    for dx, dy in d:
-        nx, ny = x + dx, y + dy
-
-        if 0 <= nx < N and 0 <= ny < M:
-            if board[nx][ny] == 0:
-                if visited[nx][ny][wall] > visited[x][y][wall] + 1:
-                    visited[nx][ny][wall] = visited[x][y][wall] + 1
-                    q.append((nx, ny, wall))
-            else:
-                if wall < K and visited[nx][ny][wall + 1] > visited[x][y][wall] + 1:
-                    visited[nx][ny][wall + 1] = visited[x][y][wall] + 1
-                    q.append((nx, ny, wall + 1))
-else:
-    print(-1)
+print(bfs())
