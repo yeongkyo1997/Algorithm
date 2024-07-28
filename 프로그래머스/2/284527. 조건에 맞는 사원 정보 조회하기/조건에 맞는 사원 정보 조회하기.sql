c9e4dec48c253c2d9e14a1aad19d802d66110648
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-07-28 11:16:06 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,24 @@
+WITH GRADE_SUM AS (
+    SELECT 
+        EMP_NO,
+        SUM(SCORE) AS SCORE
+    FROM 
+        HR_GRADE
+    WHERE 
+        YEAR = 2022
+    GROUP BY 
+        EMP_NO
+)
+SELECT 
+    G.SCORE,
+    E.EMP_NO,
+    E.EMP_NAME,
+    E.POSITION,
+    E.EMAIL
+FROM 
+    GRADE_SUM G
+JOIN 
+    HR_EMPLOYEES E ON G.EMP_NO = E.EMP_NO
+ORDER BY 
+    G.SCORE DESC
+LIMIT 1;
