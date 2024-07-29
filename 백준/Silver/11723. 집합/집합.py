07f33d16d756eb81ffb9bbb28a77909828fa96aa
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-07-29 10:35:52 +0900

    [Silver V] Title: 집합, Time: 2196 ms, Memory: 129516 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,29 +1,25 @@
 import sys
 
+input = lambda: sys.stdin.readline().rstrip()
 
-def input(): return sys.stdin.readline().strip()
+s = set()
+M = int(input())
 
-
-s = 0
-result = []
-for _ in range(int(input())):
+for _ in range(M):
     command = input().split()
 
-    if command[0] == 'add':
-        s |= 1 << int(command[1])
-    elif command[0] == 'remove':
-        s &= ~(1 << int(command[1]))
-    elif command[0] == 'check':
-        if s & (1 << int(command[1])):
-            print(1)
+    if 'add' == command[0]:
+        s.add(int(command[1]))
+    elif 'remove' == command[0]:
+        s.discard(int(command[1]))
+    elif 'check' == command[0]:
+        print(1 if int(command[1]) in s else 0)
+    elif 'toggle' == command[0]:
+        if int(command[1]) in s:
+            s.discard(int(command[1]))
         else:
-            print(0)
-    elif command[0] == 'toggle':
-        if s & (1 << int(command[1])):
-            s &= ~(1 << int(command[1]))
-        else:
-            s |= (1 << int(command[1]))
-    elif command[0] == 'all':
-        s = (1 << 21) - 1
-    elif command[0] == 'empty':
-        s = 0
+            s.add(int(command[1]))
+    elif 'all' == command[0]:
+        s = set(range(1, 21))
+    else:
+        s = set()
