f0d87125c40a8bfbe35dab1465169ebfde85319b
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-07-31 13:22:38 +0900

    [Gold V] Title: 빗물, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,26 +1,11 @@
-def trapped_water(h, w, blocks):
-    left_max = [0] * w
-    right_max = [0] * w
-    
-    left_max[0] = blocks[0]
-    right_max[-1] = blocks[-1]
-    
-    for i in range(1, w):
-        left_max[i] = max(left_max[i-1], blocks[i])
+H, W = map(int, input().rstrip().split())
 
-    for i in range(w-2, -1, -1):
-        right_max[i] = max(right_max[i+1], blocks[i])
+board = list(map(int, input().rstrip().split()))
 
-    trapped_water_amt = 0
-    
-    for i in range(1, w-1):
-        min_height = min(left_max[i], right_max[i])
-        trapped_water_amt += max(0, min_height - blocks[i])
-    
-    return trapped_water_amt
+result = 0
+for i in range(1, W - 1):
+    left = max(board[:i])
+    right = max(board[i + 1:])
+    result += max(0, min(left, right) - board[i])
 
-h, w = map(int, input().split())
-blocks = list(map(int, input().split()))
-
-result = trapped_water(h, w, blocks)
 print(result)
