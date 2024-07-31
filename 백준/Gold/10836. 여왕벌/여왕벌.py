7a13aae6fc3be64dfde2242f72706bfc4eac4317
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-07-31 14:32:04 +0900

    [Gold IV] Title: 여왕벌, Time: 268 ms, Memory: 121800 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -0,0 +1,37 @@
+import copy
+
+M, N = map(int, input().rstrip().split())
+board = [[1] * M for _ in range(M)]
+dir = [(-1, 0), (0, -1), (-1, -1)]
+
+
+def change_board(arr):
+    x, y = M - 1, 0
+
+    for i in range(3):
+        for j in range(arr[i]):
+            board[x][y] += i
+            if x - 1 >= 0:
+                x -= 1
+            else:
+                y += 1
+
+
+def spread():
+    for i in range(1, M):
+        for j in range(1, M):
+            max_val = 0
+            for dx, dy in dir:
+                nx, ny = i + dx, j + dy
+                max_val = max(max_val, board[nx][ny] - tmp[nx][ny])
+            board[i][j] += max_val
+
+
+for _ in range(N):
+    arr = list(map(int, input().rstrip().split()))
+    tmp = copy.deepcopy(board)
+    change_board(arr)
+    spread()
+
+for b in board:
+    print(*b)
