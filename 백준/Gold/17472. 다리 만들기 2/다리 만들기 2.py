a834708f9c8c1e84947e80d9413bd6cd41546604
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-05 12:42:58 +0900

    [Gold I] Title: 다리 만들기 2, Time: 112 ms, Memory: 112008 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.py
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.py
@@ -1,14 +1,19 @@
 import collections
+import math
+
+dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 N, M = map(int, input().rstrip().split())
 board = [list(map(int, input().rstrip().split())) for _ in range(N)]
-dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+visited = [[False] * M for _ in range(N)]
 
 
-def bfs(x, y, mark):
-    q = collections.deque([(x, y)])
-    visited[x][y] = True
-    board[x][y] = mark
+# 섬에 번호 부여하기
+def bfs(x, y, num):
+    board[x][y] = num
+    q = collections.deque()
+    q.append((x, y))
 
     while q:
         x, y = q.popleft()
@@ -18,80 +23,81 @@ def bfs(x, y, mark):
 
             if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and board[nx][ny] == 1:
                 visited[nx][ny] = True
-                board[nx][ny] = mark
+                board[nx][ny] = num
                 q.append((nx, ny))
 
 
-def get_dist(x, y, num, dx, dy):
-    nx, ny = x, y
-    dist = 0
+num = 1
+for i in range(N):
+    for j in range(M):
+        if not visited[i][j] and board[i][j] == 1:
+            bfs(i, j, num)
+            num += 1
 
-    while True:
-        nx += dx
-        ny += dy
-        if nx < 0 or nx >= N or ny < 0 or ny >= M or board[nx][ny] == num:
-            break
-        if board[nx][ny] != 0 and board[nx][ny] != num:
-            return nx, ny, dist
-        dist += 1
-    return -1, -1, -1
+connected = []
 
 
-def find(x):
-    if parent[x] == x:
-        return x
-    parent[x] = find(parent[x])
-    return parent[x]
+# 섬 연결하기
+def connect_island(x, y, num):
+    for dx, dy in dir:
+        length = 0
+        nx, ny = x, y
+
+        while True:
+            nx += dx
+            ny += dy
+
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                break
+            if 0 < board[nx][ny] <= num:
+                break
+
+            if board[nx][ny] > num:
+                if length >= 2:
+                    connected.append((length, num, board[nx][ny]))
+                    break
+                else:
+                    break
+            length += 1
+
+
+for i in range(N):
+    for j in range(M):
+        if board[i][j] > 0:
+            connect_island(i, j, board[i][j])
+
+parent = [i for i in range(num)]
+rank = [0] * num
+
+
+def find(a):
+    if parent[a] == a:
+        return a
+    parent[a] = find(parent[a])
+    return parent[a]
 
 
 def union(a, b):
     a = find(a)
     b = find(b)
 
-    if a != b:
-        if rank[a] > rank[b]:
-            parent[b] = a
-        elif rank[a] > rank[b]:
-            parent[a] = b
-        else:
-            parent[b] = a
-            rank[a] += 1
+    if rank[a] > rank[b]:
+        parent[b] = a
+    elif rank[a] < rank[b]:
+        parent[a] = b
+    else:
+        parent[b] = a
+        rank[a] += 1
 
 
-mark = 1
-visited = [[False] * M for _ in range(N)]
-for i in range(N):
-    for j in range(M):
-        if board[i][j] == 1 and not visited[i][j]:
-            bfs(i, j, mark)
-            mark += 1
+result = []
+connected.sort()
+for dist, a, b in connected:
+    if find(a) != find(b):
+        union(a, b)
+        result.append(dist)
 
-edges = []
-visited = [[False] * M for _ in range(N)]
-for i in range(N):
-    for j in range(M):
-        if board[i][j] != 0:
-            for dx, dy in dir:
-                x, y, dist = get_dist(i, j, board[i][j], dx, dy)
-                if x != -1 and dist >= 2:
-                    edges.append((i, j, x, y, dist))
-
-parent = [i for i in range((mark - 1))]
-rank = [1] * (mark - 1)
-mst = 0
-edges.sort(key=lambda x: (x[-1]))
-
-result = 0
-for e in edges:
-    a, b, c, d, cost = e
-    if find(board[a][b] - 1) != find(board[c][d] - 1):
-        result += cost
-        mst += 1
-        union(board[a][b] - 1, board[c][d] - 1)
-        if mst == mark - 2:
-            break
-
-if mst != mark - 2:
-    print(-1)
+if len(result) == num - 2:
+    print(sum(result))
 else:
-    print(result)
+    print(-1)
