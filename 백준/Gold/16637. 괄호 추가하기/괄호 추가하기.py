54c6f0d2d4b27609535e55f72f3b23de502e592c
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-05 11:55:59 +0900

    [Gold III] Title: 괄호 추가하기, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
@@ -1,82 +1,34 @@
-import copy
 import math
-import re
 
 N = int(input())
-s = input()
-symbol = re.split('[0-9]', s)[1:]
-num = re.split('[\-*+]', s)
-s_list = list(s)
+s = input().rstrip()
+
+
+def calc(a, op, b):
+    a = int(a)
+    b = int(b)
+    if op == '+':
+        return a + b
+    if op == '-':
+        return a - b
+    if op == '*':
+        return a * b
+
+
+def dfs(acc, idx):
+    global result
+
+    if idx == N - 1:
+        result = max(result, acc)
+        return
+
+    # 괄호X
+    if N > idx + 2:
+        dfs(calc(acc, s[idx + 1], s[idx + 2]), idx + 2)
+    if N > idx + 4:
+        dfs(calc(acc, s[idx + 1], calc(s[idx + 2], s[idx + 3], s[idx + 4])), idx + 4)
+
+
 result = -math.inf
-
-
-def check_bracket(calc):
-    stack = []
-    for c in calc:
-        if c == '(':
-            if stack:
-                return False
-            stack.append('(')
-        elif c == ')':
-            if not stack:
-                return False
-            stack.pop()
-    return True
-
-
-def postfix(calc):
-    ret = ''
-    stack = []
-    for c in calc:
-        if c in '+-*':
-            while stack and stack[-1] != '(':
-                ret += stack.pop()
-            stack.append(c)
-        elif c == '(':
-            stack.append(c)
-        elif c == ')':
-            while stack and stack[-1] != '(':
-                ret += stack.pop()
-            stack.pop()
-        else:
-            ret += c
-
-    while stack:
-        ret += stack.pop()
-    return ret
-
-
-def calc_postfix(calc):
-    stack = []
-    for c in calc:
-        if c.isdigit():
-            stack.append(int(c))
-        else:
-            b = stack.pop()
-            a = stack.pop()
-            if c == '+':
-                stack.append(a + b)
-            elif c == '-':
-                stack.append(a - b)
-            elif c == '*':
-                stack.append(a * b)
-    return int(stack.pop())
-
-
-for i in range(1, 1 << len(num)):
-    arr = []
-    for j in range(len(num) - 1):
-        if i & (1 << j):
-            arr.append(j)
-    tmp = copy.deepcopy(num)
-    for i in arr:
-        tmp[i] = f'({num[i]}'
-        tmp[i + 1] = f'{num[i + 1]})'
-
-    zip_list = list(zip(tmp, symbol))
-    calc = ''.join(''.join(ele) for ele in zip_list).strip()
-
-    if check_bracket(calc):
-        result = max(result, calc_postfix(postfix(calc)))
-
+dfs(int(s[0]), 0)
 print(result)
