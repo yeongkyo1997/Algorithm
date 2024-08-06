a08c7309fafe6a5c5b3c7d38e411036b3b2fbe87
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-06 13:29:35 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 114076 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,21 @@
-import sys
-from collections import deque, defaultdict
+import collections
 
+dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+N, M = map(int, input().split())
 
-def input(): return sys.stdin.readline().strip()
+board = [list(map(int, input())) for _ in range(N)]
 
-
-n, m = map(int, input().split())
-arr = [list(map(int, input())) for _ in range(n)]
-visited = defaultdict(lambda: [False] * m)
-
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-
-q = deque()
+q = collections.deque()
 q.append((0, 0, 1))
 
 while q:
     x, y, depth = q.popleft()
-    if x == n - 1 and y == m - 1:
+    if x == N - 1 and y == M - 1:
         print(depth)
         break
+    for dx, dy in dir:
+        nx, ny = x + dx, y + dy
 
-    for i in range(4):
-        nx, ny, ndepth = x + dx[i], y + dy[i], depth + 1
-
-        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and arr[nx][ny] == 1:
-            q.append((nx, ny, ndepth))
-            visited[nx][ny] = True
+        if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 1:
+            board[nx][ny] = 0
+            q.append((nx, ny, depth + 1))
