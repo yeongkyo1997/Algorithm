f3c5d68a2b76d9452668e244f695864bb94f9d3a
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-06 13:57:15 +0900

    [Silver III] Title: N과 M (5), Time: 168 ms, Memory: 113584 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,11 +1,19 @@
-import sys
-from itertools import permutations
-
-
-def input(): return sys.stdin.readline().rstrip()
-
-
 N, M = map(int, input().split())
 
-for i in permutations(sorted(map(int, input().split())), M):
-    print(*i)
+arr = list(map(int, input().split()))
+arr.sort()
+
+
+def dfs(path, depth, flag):
+    if depth == M:
+        print(*path)
+        return
+
+    for i in range(N):
+        if flag & (1 << i) == 0:
+            path.append(arr[i])
+            dfs(path, depth + 1, flag | (1 << i))
+            path.pop()
+
+
+dfs([], 0, 0)
