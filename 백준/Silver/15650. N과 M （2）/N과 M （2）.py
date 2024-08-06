3310ac7a6b56c8609961a4d25bbdbee5616643ba
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-06 13:45:01 +0900

    [Silver III] Title: N과 M (2), Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,23 +1,6 @@
+import itertools
+
 N, M = map(int, input().split())
 
-
-def combination(N, M):
-    result = []
-
-    def dfs(path, start):
-        if len(path) == M:
-            result.append(path[:])
-            return
-        for i in range(start, N + 1):
-            path.append(i)
-            dfs(path, i + 1)
-            path.pop()
-
-    dfs([], 1)
-
-    return result
-
-
-result = combination(N, M)
-for i in result:
-    print(*i)
+for c in itertools.combinations(range(1, N + 1), M):
+    print(*c)
