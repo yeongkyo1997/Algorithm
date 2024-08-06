14206a2e4a3e6d9013d5af44ad8eadf58d962127
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-06 13:43:12 +0900

    [Silver I] Title: 단지번호붙이기, Time: 92 ms, Memory: 109400 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,33 +1,32 @@
-import sys
-
-sys.setrecursionlimit(10 ** 5)
-def input(): return sys.stdin.readline().rstrip()
-
-
+dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 N = int(input())
 board = [list(map(int, input())) for _ in range(N)]
 
-d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+visited = [[False] * N for _ in range(N)]
 
 
 def dfs(x, y):
-    if x < 0 or y < 0 or x >= N or y >= N or not board[x][y]:
+    if visited[x][y]:
         return 0
 
-    board[x][y] = 0
-    ret = 1
-    for dx, dy in d:
-        ret += dfs(x + dx, y + dy)
+    visited[x][y] = True
+    result = 1
 
-    return ret
+    for dx, dy in dir:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 1:
+            board[nx][ny] = 0
+            result += dfs(nx, ny)
+
+    return result
 
 
-result = []
+arr = []
+
 for i in range(N):
     for j in range(N):
         if board[i][j] == 1:
-            result.append(dfs(i, j))
+            arr.append(dfs(i, j))
 
-print(len(result))
-result.sort()
-print(*result, sep='\n')
+print(len(arr))
+print(*sorted(arr), sep='\n')
