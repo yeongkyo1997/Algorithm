86cf2af6ae5df20d264f5589660b86634457e335
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-06 12:30:53 +0900

    [Gold V] Title: 토마토, Time: 632 ms, Memory: 237208 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,37 +1,41 @@
-import sys
-from collections import deque
+import collections
 
+dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-def input(): return sys.stdin.readline().rstrip()
+M, N = map(int, input().rstrip().split())
 
+board = [list(map(int, input().rstrip().split())) for _ in range(N)]
 
-N, M = map(int, input().split())
-
-board = [list(map(int, input().split())) for _ in range(M)]
-
-q = deque()
-
-for i in range(M):
-    for j in range(N):
-        if board[i][j] == 1:
-            q.append((i, j))
-
-d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-while q:
-    x, y = q.popleft()
-
-    for dx, dy in d:
-        nx, ny = x + dx, y + dy
-
-        if 0 <= nx < M and 0 <= ny < N and board[nx][ny] == 0:
-            board[nx][ny] = board[x][y] + 1
-            q.append((nx, ny))
-
-for i in range(M):
-    for j in range(N):
+tomato = []
+zero_cnt = 0
+for i in range(N):
+    for j in range(M):
         if board[i][j] == 0:
-            print(-1)
-            exit(0)
+            zero_cnt += 1
+        elif board[i][j] == 1:
+            tomato.append((i, j, 0))
 
-print(max(max(i) for i in board) - 1)
+
+def bfs():
+    global zero_cnt
+    q = collections.deque(tomato)
+    visited = [[False] * M for _ in range(N)]
+
+    while q:
+        x, y, depth = q.popleft()
+        for dx, dy in dir:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and board[nx][ny] == 0:
+                visited[nx][ny] = True
+                q.append((nx, ny, depth + 1))
+                zero_cnt -= 1
+                if zero_cnt == 0:
+                    return depth + 1
+    return -1
+
+
+if zero_cnt == 0:
+    print(0)
+else:
+    print(bfs())
