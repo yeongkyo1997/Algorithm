5b25b1bda620f57064c6c0ea04b62cedec8bd383
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-06 11:53:55 +0900

    [Silver III] Title: N과 M (4), Time: 168 ms, Memory: 114096 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,11 +1,15 @@
-import sys
-from itertools import combinations_with_replacement
+N, M = map(int, input().rstrip().split())
 
 
-def input(): return sys.stdin.readline().rstrip()
+def dfs(path, depth, start):
+    if depth == M:
+        print(*path)
+        return
+
+    for i in range(start, N + 1):
+        path.append(i)
+        dfs(path, depth + 1, i)
+        path.pop()
 
 
-N, M = map(int, input().split())
-
-for i in combinations_with_replacement(range(1, N + 1), M):
-    print(*i)
+dfs([], 0, 1)
