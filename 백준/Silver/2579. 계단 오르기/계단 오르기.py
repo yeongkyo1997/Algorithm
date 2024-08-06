e99f6a72b596e7080848f06c84017c6ed094c6d8
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-06 13:24:18 +0900

    [Silver III] Title: 계단 오르기, Time: 112 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,24 +1,23 @@
-from collections import defaultdict
-import sys
-sys.setrecursionlimit(10 ** 5)
-def input(): return sys.stdin.readline().strip()
+import collections
 
+N = int(input())
 
-n = int(input())
-arr = [0] + [int(input()) for _ in range(n)]
-lib = defaultdict(lambda: -1)
+arr = [0]
 
+for _ in range(N):
+    arr.append(int(input()))
 
-def solve(idx):
-    if idx <= 0:
-        return 0
+dp = collections.defaultdict(int)
 
-    if lib[idx] != -1:
-        return lib[idx]
+dp[0] = 0
+if N >= 1:
+    dp[1] = arr[1]
+if N >= 2:
+    dp[2] = dp[1] + arr[2]
+if N >= 3:
+    dp[3] = max(arr[1], arr[2]) + arr[3]
 
-    lib[idx] = max(solve(idx - 2) + arr[idx],
-                   solve(idx - 3) + arr[idx - 1] + arr[idx])
-    return lib[idx]
+for i in range(4, N + 1):
+    dp[i] = max(dp[i - 2], dp[i - 1 - 2] + arr[i - 1]) + arr[i]
 
-
-print(solve(n))
+print(dp[N])
