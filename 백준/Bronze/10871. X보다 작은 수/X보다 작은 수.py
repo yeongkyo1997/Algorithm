e27403faf7dbcd1cb275d63737628305e9c1d2e6
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-08 08:46:14 +0900

    [Bronze V] Title: X보다 작은 수, Time: 104 ms, Memory: 111392 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,9 +1,3 @@
-import sys
-
-
-def input(): return sys.stdin.readline().strip()
-
-
 N, X = map(int, input().split())
-
-print(*filter(lambda x: x < X, map(int, input().split())))
+arr = list(filter(lambda a: a < X, map(int, input().split())))
+print(*arr)
