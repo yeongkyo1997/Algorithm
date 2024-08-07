939025a05d758e02a6e54ff23bb43948d0e81605
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-07 15:23:35 +0900

    [Gold IV] Title: 뱀, Time: 120 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,37 +1,56 @@
-from collections import deque
-dx, dy = (0, 0, 1, -1), (1, -1, 0, 0)
-L, D = (3, 2, 0, 1), (2, 3, 1, 0)
+import collections
 
-def solve():
-    x, y, z, d, ans = 0, 0, 0, 0, 0
-    arr[0][0] = 2
-    q = deque()
-    q.append((0, 0))
-    while True:
-        x, y = x + dx[d], y + dy[d]
-        ans += 1
-        if x < 0 or x >= N or y < 0 or y >= N or arr[x][y] == 2:
-            print(ans)
-            return
-        if not arr[x][y]:
-            nx, ny = q.popleft()
-            arr[nx][ny] = 0
-        arr[x][y] = 2
-        q.append((x, y))
-        t, c = b[z]
-        if ans == int(t):
-            if c == 'L':
-                d = L[d]
-            else:
-                d = D[d]
-            z = (z + 1) % M
-
+dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 N = int(input())
-arr = [[0] * N for _ in range(N)]
-for _ in range(int(input())):
-    u, v = map(int, input().split())
-    arr[u - 1][v - 1] = 1
-M = int(input())
-b = [list(map(str, input().split())) for _ in range(M)]
-solve()
+K = int(input())
+
+snake = collections.deque()
+snake.append((0, 0))
+board = [[0] * N for _ in range(N)]
+board[0][0] = 1
+
+for _ in range(K):
+    r, c = map(int, input().split())
+    board[r - 1][c - 1] = 2
+
+L = int(input())
+turn = {}
+for _ in range(L):
+    X, C = input().split()
+    turn[int(X)] = C
+
+
+def move_snake():
+    result = 0
+    d = 0
+    x, y = snake[0]
+    nx, ny = x, y
+    while True:
+        dx, dy = dir[d]
+        nx += dx
+        ny += dy
+        result += 1
+        if nx < 0 or nx >= N or ny < 0 or ny >= N:
+            break
+        if board[nx][ny] == 1:
+            break
+
+        if board[nx][ny] == 2:
+            board[nx][ny] = 1
+            snake.append((nx, ny))
+        if board[nx][ny] == 0:
+            snake.append((nx, ny))
+            board[nx][ny] = 1
+            rx, ry = snake.popleft()
+            board[rx][ry] = 0
+
+        if result in turn.keys():
+            if turn[result] == 'L':
+                d = (d - 1) % 4
+            else:
+                d = (d + 1) % 4
+    return result
+
+
+print(move_snake())
