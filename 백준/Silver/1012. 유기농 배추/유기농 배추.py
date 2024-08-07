93d911db164ad7286a49f4d764791785e5c0a986
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-07 11:34:34 +0900

    [Silver II] Title: 유기농 배추, Time: 124 ms, Memory: 111216 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,33 @@
-import sys
-sys.setrecursionlimit(10 ** 5)
-
-
-def input(): return sys.stdin.readline().rstrip()
-
-
-d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-T = int(input())
+dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
 def dfs(x, y):
-    if x < 0 or x >= N or y < 0 or y >= M or board[x][y] == 0:
-        return 1
+    stack = [(x, y)]
+    while stack:
+        x, y = stack.pop()
+        if x < 0 or x >= N or y < 0 or y >= M:
+            continue
+        if board[x][y] == 0:
+            continue
 
-    board[x][y] = 0
-    for dx, dy in d:
-        nx, ny = x + dx, y + dy
-        dfs(nx, ny)
+        board[x][y] = 0
+        for dx, dy in dir:
+            nx, ny = x + dx, y + dy
+            stack.append((nx, ny))
     return 1
 
 
-for _ in range(T):
+for _ in range(int(input())):
     M, N, K = map(int, input().split())
     board = [[0] * M for _ in range(N)]
-
+    graph = []
     for _ in range(K):
-        X, Y = map(int, input().split())
-        board[Y][X] = 1
-
+        y, x = map(int, input().split())
+        board[x][y] = 1
+        graph.append((x, y))
     result = 0
-
-    for i in range(N):
-        for j in range(M):
-            if board[i][j] == 1:
-                result += dfs(i, j)
+    for x, y in graph:
+        if board[x][y] == 1:
+            result += dfs(x, y)
 
     print(result)
