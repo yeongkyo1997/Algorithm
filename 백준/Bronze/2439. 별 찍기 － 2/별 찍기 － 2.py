a2fb2dbec2b61c4efe297cf0efb921b3ec8ec986
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-08 08:25:08 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -1,10 +1,4 @@
-import sys
+N = int(input())
 
-
-def input(): return sys.stdin.readline().strip()
-
-
-n = int(input())
-
-for i in range(n - 1, -1, -1):
-    print(f'{" " * i}{"*" * (n - i)}')
+for i in range(1, N + 1):
+    print(' ' * (N - i) + '*' * i)
