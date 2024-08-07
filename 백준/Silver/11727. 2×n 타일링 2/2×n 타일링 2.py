1ffe86e8cc1bc92ee3a017186a7ac897387e138e
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-07 11:26:31 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 120 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,18 +1,15 @@
-import sys
+import collections
 
-input = lambda: sys.stdin.readline().rstrip()
+n = int(input())
 
+dp = collections.defaultdict()
 
-def main():
-    n = int(input())
-    dp = [0] * (n + 1)
-    dp[1] = 1
-    if n > 1:
-        dp[2] = 3
-    for i in range(3, n + 1):
-        dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007
-    print(dp[n])
+MOD = 10_007
 
+dp[1] = 1
+dp[2] = 3
 
-if __name__ == '__main__':
-    main()
+for i in range(3, n + 1):
+    dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % MOD
+
+print(dp[n] % MOD)
