db6df0c2c5d3e080d0ed5f451657e5005eb5e920
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-08 08:56:52 +0900

    [Bronze III] Title: 알람 시계, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,18 +1,6 @@
-import sys
-
-
-def input(): return sys.stdin.readline().rstrip()
-
-
 H, M = map(int, input().split())
-
-
 M -= 45
-
 if M < 0:
     H -= 1
-    M += 60
 
-if H < 0:
-    H = 23
-print(H, M)
+print(H % 24, M % 60)
