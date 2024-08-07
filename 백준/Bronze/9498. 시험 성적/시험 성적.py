b13f40e725c4cfedf9971ef1bcbb39c07f4a55b7
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-08 08:48:13 +0900

    [Bronze V] Title: 시험 성적, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,18 +1,12 @@
-import sys
-
-
-def input(): return sys.stdin.readline().rstrip()
-
-
 score = int(input())
 
-if score < 60:
-    print('F')
-elif score < 70:
-    print('D')
-elif score < 80:
-    print('C')
-elif score < 90:
-    print('B')
-else:
+if score >= 90:
     print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+elif score >= 60:
+    print('D')
+else:
+    print('F')
