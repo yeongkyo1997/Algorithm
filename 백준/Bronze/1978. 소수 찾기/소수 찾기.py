400cbedffedfedcfbbd5d3ca6475ebbe3d87a48c
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-08 09:41:20 +0900

    [Bronze II] Title: 소수 찾기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,10 +1,11 @@
 N = int(input())
 
+arr = list(map(int, input().split()))
+
 
 def is_prime(n):
     if n == 1:
         return False
-
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
@@ -12,5 +13,4 @@ def is_prime(n):
     return True
 
 
-arr = list(map(int, input().split()))
-print(sum([1 for i in arr if is_prime(i)]))
+print(sum(1 for a in arr if is_prime(a)))
