8ed8fed934ef90a035c33d3262904c447531392f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-08 12:09:56 +0900

    [Silver IV] Title: 수 찾기, Time: 176 ms, Memory: 144656 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,21 +1,6 @@
 N = int(input())
-arr = list(map(int, input().rstrip().split()))
-arr.sort()
-
-
-def binary_search(left, right, num):
-    while left <= right:
-        mid = (left + right) // 2
-        if num > arr[mid]:
-            left = mid + 1
-        elif num < arr[mid]:
-            right = mid - 1
-        else:
-            return 1
-
-    return 0
-
+s = set(map(int, input().split()))
 
 input()
-for ele in map(int, input().rstrip().split()):
-    print(binary_search(0, len(arr) - 1, ele))
+for i in map(int, input().split()):
+    print(int(i in s))
