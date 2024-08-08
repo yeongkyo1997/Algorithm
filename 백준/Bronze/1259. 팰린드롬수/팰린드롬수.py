5d2de1340ecaeecd24532c5ea741fb8afda124cd
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-08 09:54:42 +0900

    [Bronze I] Title: 팰린드롬수, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,29 +1,8 @@
-import sys
-
-
-def input(): return sys.stdin.readline().rstrip()
-
-
-def num_reverse(n):
-    result = 0
-
-    while True:
-        result += n % 10
-        n //= 10
-
-        if n == 0:
-            break
-        result *= 10
-
-    return result
-
-
 while True:
-    n = int(input())
-    if n == 0:
+    s = input()
+    if s == '0':
         break
-
-    if n == num_reverse(n):
+    if s == s[::-1]:
         print('yes')
     else:
         print('no')
