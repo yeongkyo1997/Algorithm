48f654f92e45eb76eea5fac2278d7b8a53605593
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-08 09:19:11 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,16 +1,7 @@
-import sys
-
-
-def input(): return sys.stdin.readline().rstrip()
-
-
-s = input()
-
-
-result = []
-
+S = input()
 
 for i in range(ord('a'), ord('z') + 1):
-    result.append(s.find(chr(i)))
-
-print(*result)
+    try:
+        print(S.index(chr(i)), end=' ')
+    except:
+        print(-1, end=' ')
