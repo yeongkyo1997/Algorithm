b91fcde8ba1d7eb636c30c89a76bac193f62ab3f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-08 11:25:52 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 436 ms, Memory: 129172 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,10 +1,7 @@
-import sys
-from collections import deque
-
 N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-arr = sorted([list(map(int, input().split()))
-             for _ in range(N)], key=lambda x: (x[0], x[1]))
+arr.sort(key=lambda x: (x[0], x[1]))
 
-for a, b in arr:
-    print(a, b)
+for a in arr:
+    print(*a)
