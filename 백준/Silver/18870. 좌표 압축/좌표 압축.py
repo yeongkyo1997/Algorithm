baa4e29d83363a177bb7dbb22bd1616d51df654e
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-08 20:06:14 +0900

    [Silver II] Title: 좌표 압축, Time: 1048 ms, Memory: 274796 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,9 +1,10 @@
+import bisect
+
 N = int(input())
+
 arr = list(map(int, input().split()))
 
 sorted_arr = sorted(set(arr))
 
-result = {val: idx for idx, val in enumerate(sorted_arr)}
-
-for i in arr:
-    print(result[i], end=' ')
+for a in arr:
+    print(bisect.bisect_left(sorted_arr, a), end=' ')
