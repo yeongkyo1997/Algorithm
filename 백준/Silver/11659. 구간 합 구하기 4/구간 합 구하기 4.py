22a750589fcf160c83fdad9c4267af8ec5b3fb5f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-09 13:14:45 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2856 ms, Memory: 127168 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,18 +1,11 @@
-import sys
-
-
-def input(): return sys.stdin.readline().rstrip()
-
-
 N, M = map(int, input().split())
 
 arr = list(map(int, input().split()))
+acc = [0]
 
-sum_arr = [0] * N
-sum_arr[0] = arr[0]
-for i in range(1, N):
-    sum_arr[i] = arr[i] + sum_arr[i - 1]
+for i in range(1, N + 1):
+    acc.append(acc[i - 1] + arr[i - 1])
 
 for _ in range(M):
     a, b = map(int, input().split())
-    print(sum_arr[b - 1] - sum_arr[a - 1] + arr[a - 1])
+    print(acc[b] - acc[a - 1])
