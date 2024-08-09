93167cb0296b78971b88449863787bc5b4994fd4
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-09 12:53:01 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 2956 ms, Memory: 137888 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,21 +1,16 @@
-import sys
-
-
-def input(): return sys.stdin.readline().rstrip()
-
-
 N, M = map(int, input().split())
-pokemon_dict = {} 
-pokemon_list = []  
 
-for i in range(1, N + 1):
-    name = input().rstrip()
-    pokemon_dict[name] = i
-    pokemon_list.append(name)
+name = {}
+number = {}
+
+for i in range(N):
+    s = input()
+    name[s] = i + 1
+    number[i + 1] = s
 
 for _ in range(M):
-    question = input()
-    if question.isdigit(): 
-        print(pokemon_list[int(question) - 1])
-    else:  
-        print(pokemon_dict[question])
+    s = input()
+    if s.isdigit():
+        print(number[int(s)])
+    else:
+        print(name[s])
