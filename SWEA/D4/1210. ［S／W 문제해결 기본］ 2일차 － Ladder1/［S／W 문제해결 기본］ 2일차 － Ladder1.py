5b13ae76812a67a5a5f80b5fde6ba83fac9e4271
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-11 10:08:59 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 171 ms, Memory: 49,812 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -1,29 +1,31 @@
-for t in range(1, 11):
-    input()
+for _ in range(1, 11):
+    t = int(input())
     N = 100
-    board = [list(map(int, input().rstrip().split())) for _ in range(N)]
-    x, y = 0, 0
+    board = [list(map(int, input().split())) for _ in range(N)]
+    x, y = -1, -1
+
     for col in range(N):
         if board[N - 1][col] == 2:
             x, y = N - 1, col
+            break
+    result = 0
 
     while True:
-        # 왼쪽
-        if y - 1 >= 0 and board[x][y - 1] == 1:
-            while y - 1 >= 0 and board[x][y - 1] == 1:
-                y -= 1
-            x -= 1
-            continue
-        # 오른쪽
-        if y + 1 < N and board[x][y + 1] == 1:
-            while y + 1 < N and board[x][y + 1] == 1:
-                y += 1
-            x -= 1
-            continue
+        # 왼쪽 체크
+        while 0 <= y - 1 < N and board[x][y - 1] == 1:
+            y -= 1
+            if board[x - 1][y] == 1:
+                x -= 1
 
-        # 위로
-        if x - 1 >= 0:
-            x -= 1
+        # 오른쪽 체크
+        while 0 <= y + 1 < N and board[x][y + 1] == 1:
+            y += 1
+            if board[x - 1][y] == 1:
+                x -= 1
+
         if x == 0:
-            print(f'#{t} {y}')
+            result = y
             break
+        x -= 1
+
+    print(f'#{t} {result}')
