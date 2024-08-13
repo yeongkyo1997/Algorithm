05a6fc07c439655043bab863e4e5c5f6e0bfa27f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-13 18:32:06 +0900

    [Gold III] Title: 드래곤 커브, Time: 128 ms, Memory: 110704 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
@@ -1,21 +1,29 @@
-v, result = [0], 0
-arr = [[0] * 101 for _ in range(101)]
-dx, dy = (1, 0, -1, 0), (0, -1, 0, 1)
-
-for i in range(1, 11):
-    k = 1 << (i - 1)
-    for j in range(k):
-        v.append((v[k - j - 1] + 1) % 4)
+dir = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+N = 101
+board = [[0] * N for _ in range(N)]
+curve = []
 
 for _ in range(int(input())):
     x, y, d, g = map(int, input().split())
-    arr[x][y] = 1
-    for i in range(1 << g):
-        x, y = x + dx[(v[i] + d) % 4], y + dy[(v[i] + d) % 4]
-        arr[x][y] = 1
+    curve = [d]
+    board[x][y] = 1
 
-for i in range(100):
-    for j in range(100):
-        if arr[i][j] and arr[i + 1][j] and arr[i][j + 1] and arr[i + 1][j + 1]:
+    for _ in range(g):
+        for i in range(len(curve) - 1, -1, -1):
+            curve.append((curve[i] + 1) % 4)
+
+    for i in curve:
+        dx, dy = dir[i]
+        x_new, y_new = x + dx, y + dy
+        if 0 <= x_new < N and 0 <= y_new < N:
+            board[x_new][y_new] = 1
+            x, y = x_new, y_new
+
+result = 0
+
+for i in range(N - 1):
+    for j in range(N - 1):
+        if board[i][j] and board[i + 1][j] and board[i][j + 1] and board[i + 1][j + 1]:
             result += 1
+
 print(result)
