4f11cbdcc0e564f178a2a8c07f46fa6247ca3e32
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-16 12:16:52 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 540 ms, Memory: 256412 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -1,46 +1,42 @@
 import sys
 from bisect import bisect_left
 
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
 
 
-def lis(arr):
-    lis_arr = [arr[0]]
-    index_arr = [0]
-    index = 1
+def lis_with_sequence(arr):
+    n = len(arr)
+    lis = []
+    predecessors = [-1] * n  # 이전 원소의 인덱스를 저장
+    lis_indices = []  # lis의 인덱스를 저장
 
-    for num in arr[1:]:
-        if lis_arr[-1] < num:
-            lis_arr.append(num)
-            index_arr.append(index)
-            index += 1
+    for i, num in enumerate(arr):
+        pos = bisect_left(lis, num)
+        if pos == len(lis):
+            lis.append(num)
+            lis_indices.append(i)
         else:
-            i = bisect_left(lis_arr, num)
-            lis_arr[i] = num
-            index_arr.append(i)
+            lis[pos] = num
+            lis_indices[pos] = i
 
-    return lis_arr, index_arr
+        if pos > 0:
+            predecessors[i] = lis_indices[pos - 1]
 
+    # LIS 수열 복원
+    lis_length = len(lis)
+    lis_sequence = []
+    k = lis_indices[-1]
+    for _ in range(lis_length):
+        lis_sequence.append(arr[k])
+        k = predecessors[k]
+    lis_sequence.reverse()
 
-def get_lis(arr, index_arr):
-    ans = []
-    idx = len(arr) - 1
-    num = max(index_arr)
-
-    while idx >= 0:
-        if index_arr[idx] == num:
-            ans.append(arr[idx])
-            num -= 1
-        idx -= 1
-
-    return ans[::-1]
+    return lis_length, lis_sequence
 
 
-N = int(input())
-nums = list(map(int, input().split()))
-
-lis_arr, index_arr = lis(nums)
-ans = get_lis(nums, index_arr)
-
-print(len(ans))
-print(*ans)
+# 입력 예시
+n = int(input())
+arr = list(map(int, input().split()))
+length, sequence = lis_with_sequence(arr)
+print(length)  # 출력: 4
+print(' '.join(map(str, sequence)))  # 출력: 10 20 30 50
