af4b223181a4a64607562ad7ce7b8cb0ab64e67f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-20 21:57:36 +0900

    [Diamond IV] Title: It’s a Mod, Mod, Mod, Mod World, Time: 364 ms, Memory: 118972 KB -BaekjoonHub

---
+++ 백준/Diamond/16998. It’s a Mod， Mod， Mod， Mod World/It’s a Mod， Mod， Mod， Mod World.py
@@ -0,0 +1,31 @@
+import math
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def div_sum(p, q, n):
+    if n == 0 or p == 0:
+        return 0
+    if q == 1:
+        return p * n * (n + 1) // 2
+    if p > q:
+        return div_sum(p % q, q, n) + n * (n + 1) // 2 * (p // q)
+    return n * (n * p // q) + (n // q) - div_sum(q, p, n * p // q)
+
+
+def mod_sum(p, q, n):
+    gcd = math.gcd(p, q)
+    return p * n * (n + 1) // 2 - q * div_sum(p // gcd, q // gcd, n)
+
+
+def solve():
+    t = int(input())
+
+    for _ in range(t):
+        p, q, n = map(int, input().split())
+        print(mod_sum(p, q, n))
+
+
+if __name__ == "__main__":
+    solve()
