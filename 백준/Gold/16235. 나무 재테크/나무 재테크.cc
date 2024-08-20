7089e45aff03a0e09d726fc0515ee540f031a2de
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-20 13:47:59 +0900

    [Gold III] Title: 나무 재테크, Time: 160 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.cc
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.cc
@@ -1,82 +1,129 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int n, m, k, ans, e[11][11], remain[11][11];
+int dir[8][2] = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
+int N, M, K;
 
-deque <int> que[11][11]; 
+vector<vector<int>> board;
+vector<vector<int>> add_board;
+vector<vector<vector<int>>> trees;
 
-int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
-int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1}; 
+void spring()
+{
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            if (!trees[i][j].empty())
+            {
+                sort(trees[i][j].begin(), trees[i][j].end());
 
-int main(void) {
-	ios::sync_with_stdio(false);
-	cin >> n >> m >> k; 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			cin >> e[i][j];
-
-	for (int i = 0; i < m; i++) {
-		int a, b, c;
-
-		cin >> a >> b >> c;
-
-		que[a-1][b-1].push_back(c);
-	}
-
-	for (int i = 0; i < n; i++) 
-		for (int j = 0; j < n; j++) {
-			remain[i][j] = 5;
-			sort(que[i][j].begin(), que[i][j].end());
-		} while (k--) { // 봄 & 여름 
-
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					for (int p = 0; p < que[i][j].size(); p++) {
-						if (que[i][j][p] <= remain[i][j]) {
-							remain[i][j] -= que[i][j][p]; 
-							que[i][j][p]++; 
-						}
-						else {
-							while (p < que[i][j].size()) {
-								remain[i][j] += (que[i][j].back() / 2); 
-								que[i][j].pop_back();
-							}
-							break; 
-						} 
-					}
-				}
-			}
-
-			// 가을 
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					for (int p = 0; p < que[i][j].size(); p++) {
-						if (que[i][j][p] % 5 == 0) {
-							for (int r = 0; r < 8; r++) {
-								int nx = i + dx[r];
-								int ny = j + dy[r]; 
-								if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-									que[nx][ny].push_front(1);
-								}
-							}
-						}
-					}
-				}
-			}
-			// 겨울 
-			for (int i = 0; i < n;i++) {
-				for (int j = 0; j < n; j++) {
-					remain[i][j] += e[i][j];
-				}
-			}
-		}
-		
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				ans += que[i][j].size();
-		cout << ans << "\n";
+                vector<int> del_list;
+                for (int t = 0; t < trees[i][j].size(); ++t)
+                {
+                    if (board[i][j] >= trees[i][j][t])
+                    {
+                        board[i][j] -= trees[i][j][t];
+                        trees[i][j][t] += 1;
+                    }
+                    else
+                    {
+                        del_list.push_back(t);
+                    }
+                }
+                for (int d = del_list.size() - 1; d >= 0; --d)
+                {
+                    board[i][j] += trees[i][j][del_list[d]] / 2;
+                    trees[i][j].erase(trees[i][j].begin() + del_list[d]);
+                }
+            }
+        }
+    }
 }
 
+void fall()
+{
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            if (!trees[i][j].empty())
+            {
+                for (int t : trees[i][j])
+                {
+                    if (t % 5 == 0)
+                    {
+                        for (int d = 0; d < 8; ++d)
+                        {
+                            int nx = i + dir[d][0];
+                            int ny = j + dir[d][1];
+
+                            if (0 <= nx && nx < N && 0 <= ny && ny < N)
+                            {
+                                trees[nx][ny].push_back(1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void winter()
+{
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            board[i][j] += add_board[i][j];
+        }
+    }
+}
+
+int main()
+{
+    cin >> N >> M >> K;
+
+    board.assign(N, vector<int>(N, 5));
+    add_board.assign(N, vector<int>(N));
+    trees.assign(N, vector<vector<int>>(N));
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            cin >> add_board[i][j];
+        }
+    }
+
+    for (int i = 0; i < M; ++i)
+    {
+        int x, y, year;
+        cin >> x >> y >> year;
+        trees[x - 1][y - 1].push_back(year);
+    }
+
+    for (int i = 0; i < K; ++i)
+    {
+        spring();
+        fall();
+        winter();
+    }
+
+    int result = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            result += trees[i][j].size();
+        }
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
