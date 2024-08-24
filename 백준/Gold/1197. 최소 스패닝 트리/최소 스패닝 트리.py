ba0455c33e1ae573d277d9c156ddc0f06c0f826e
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-08-24 14:26:32 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 368 ms, Memory: 129716 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,37 +1,49 @@
+import collections
 import sys
 
-
-def input(): return sys.stdin.readline().rstrip()
-
+input = lambda: sys.stdin.readline().rstrip()
 
 V, E = map(int, input().split())
 
-graph = [list(map(int, input().split())) for _ in range(E)]
+graph = []
 
+for _ in range(E):
+    s, e, d = map(int, input().split())
+    graph.append((s, e, d))
 
 parent = [i for i in range(V + 1)]
+rank = collections.defaultdict(int)
 
 
 def find(a):
-    if parent[a] == a:
-        return a
-
-    parent[a] = find(parent[a])
-
+    if a != parent[a]:
+        parent[a] = find(parent[a])
     return parent[a]
 
 
 def union(a, b):
-    a = find(parent[a])
-    b = find(parent[b])
+    a = find(a)
+    b = find(b)
 
     if a == b:
         return False
 
-    parent[max(a, b)] = min(a, b)
+    if rank[a] > rank[b]:
+        parent[b] = a
+    elif rank[a] < rank[a]:
+        parent[a] = b
+    else:
+        parent[b] = a
+        rank[a] += 1
+
     return True
 
 
 graph.sort(key=lambda x: x[2])
 
-print(sum(c for a, b, c in graph if union(a, b)))
+result = 0
+for s, e, d in graph:
+    if union(s, e):
+        result += d
+
+print(result)
