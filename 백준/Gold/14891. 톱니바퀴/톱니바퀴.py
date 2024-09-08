cbcbd34bad5d85bf3e45b9b0f217d53a2b794ae8
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-08 21:15:30 +0900

    [Gold V] Title: 톱니바퀴, Time: 124 ms, Memory: 113736 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,49 +1,43 @@
 import collections
 import copy
 
-board = [[]]
-for _ in range(4):
-    arr = collections.deque(list(map(int, input())))
-    board.append(arr)
-K = int(input())
-
 
 def rotate(n, d):
-    global board
-    tmp = copy.deepcopy(board)
-    tmp[n].rotate(d)
+    tmp = [copy.copy(b) for b in board]
+    board[n].rotate(d)
 
-    # 왼쪽
-    cur_d = d
-    cur = n
-    while cur + 1 <= 4:
-        cur_d *= -1
-        if board[cur][2] == board[cur + 1][-2]:
-            break
-        else:
-            tmp[cur + 1].rotate(cur_d)
-        cur += 1
-
+    nn, nd = n, d
     # 오른쪽
-    cur_d = d
-    cur = n
-    while cur - 1 >= 1:
-        cur_d *= -1
-        if board[cur][-2] == board[cur - 1][2]:
+    while nn + 1 <= 4:
+        if tmp[nn][2] == tmp[nn + 1][-2]:
             break
         else:
-            tmp[cur - 1].rotate(cur_d)
-        cur -= 1
+            nd *= -1
+            board[nn + 1].rotate(nd)
+        nn += 1
 
-    board = tmp
+    nn, nd = n, d
+    # 왼쪽
+    while nn - 1 >= 1:
+        if tmp[nn][-2] == tmp[nn - 1][2]:
+            break
+        else:
+            nd *= -1
+            board[nn - 1].rotate(nd)
+        nn -= 1
 
 
-for _ in range(K):
-    n, d = map(int, input().split())
-    rotate(n, d)
+if __name__ == '__main__':
+    board = [[]]
+    for _ in range(4):
+        board.append(collections.deque(list(map(int, input()))))
 
-result = 0
-for i, b in enumerate(board[1:]):
-    result += b[0] << i
+    k = int(input())
+    for _ in range(k):
+        rotate(*map(int, input().split()))
 
-print(result)
+    result = 0
+    for i, b in enumerate(board[1:]):
+        result += b[0] << i
+
+    print(result)
