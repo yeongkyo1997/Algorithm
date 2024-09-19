5e070546447753102b14df3f02b2133a11031b2d
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-19 20:54:04 +0900

    [Gold V] Title: 상어 초등학교, Time: 260 ms, Memory: 116276 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,55 +1,68 @@
+import collections
 import sys
 
+def find_position(num):
+    # 좋아하는 학생 수 구하기
+    def get_favorite_student(x, y):
+        ret = 0
+        for dx, dy in dir:
+            nx, ny = x + dx, y + dy
 
-def input(): return sys.stdin.readline().rstrip()
+            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] in students[num]:
+                ret += 1
 
+        return ret
 
-def seat(N, preferences, student):
-    a, b = -1, -1
-    ret = None
+    # 빈자리 수 구하기
+    def get_empty(x, y):
+        ret = 0
+        for dx, dy in dir:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0:
+                ret += 1
+
+        return ret
+
+    candi = []
     for i in range(N):
         for j in range(N):
-            if room[i][j] == 0:
-                like, empty = 0, 0
-                for dx, dy in d:
-                    nx, ny = i + dx, j + dy
-                    if 0 <= nx < N and 0 <= ny < N:
-                        if room[nx][ny] in preferences[student]:
-                            like += 1
-                        elif room[nx][ny] == 0:
-                            empty += 1
+            if board[i][j] == 0:
+                # 좋아하는 학생 수 구하기
+                like = get_favorite_student(i, j)
+                empty = get_empty(i, j)
+                candi.append((-like, -empty, i, j, (i, j, like)))
 
-                if (like, empty) > (a, b):
-                    a, b = like, empty
-                    ret = (i, j)
+    candi.sort()
 
-    return ret
+    row, col, like = candi[0][-1]
+
+    board[row][col] = num
 
 
-def solution(N, prefer):
-    ret = 0
-    for i in range(N):
-        for j in range(N):
-            student = room[i][j]
-            like = sum(1 for dx, dy in d if 0 <= i + dx < N and 0 <=
-                       j + dy < N and room[i + dx][j + dy] in prefer[student])
-            if like:
-                ret += 10 ** (like - 1)
-    return ret
+if __name__ == '__main__':
+    dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    N = int(input())
+    board = [[0] * N for _ in range(N)]
 
+    students = collections.defaultdict(set)
 
-d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    result = 0
+    for _ in range(N ** 2):
+        data = list(map(int, input().split()))
+        num = data[0]
+        students[num] = set(data[1:])
+        find_position(num)
 
-N = int(input())
-preferences = {}
-for _ in range(N ** 2):
-    student, *like = map(int, input().split())
-    preferences[student] = like
+    for x in range(N):
+        for y in range(N):
+            like = 0
+            for dx, dy in dir:
+                nx, ny = x + dx, y + dy
 
-room = [[0] * N for _ in range(N)]
+                if 0 <= nx < N and 0 <= ny < N and board[nx][ny] in students[board[x][y]]:
+                    like += 1
+            if like > 0:
+                result += 10 ** (like - 1)
 
-for student in preferences:
-    x, r = seat(N, preferences, student)
-    room[x][r] = student
-
-print(solution(N, preferences))
+    print(result)
