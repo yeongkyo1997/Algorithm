1d2ce0e7ba8b348b04ad15dbeab906097055b4cc
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-19 21:50:41 +0900

    [Bronze V] Title: 문자열, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.py
+++ 백준/Bronze/9086. 문자열/문자열.py
@@ -1,3 +1,6 @@
-for _ in range(int(input())):
-    s = input()
-    print(s[0] + s[-1])
+if __name__ == '__main__':
+    T = int(input())
+
+    for _ in range(T):
+        s = input()
+        print(s[0], s[-1], sep='')
