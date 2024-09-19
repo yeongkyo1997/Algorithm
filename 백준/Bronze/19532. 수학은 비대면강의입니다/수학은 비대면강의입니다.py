603873616eefa8e2f4e65d545bfa4dd21baebb88
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-19 22:05:19 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,6 +1,11 @@
-a, b, c, d, e, f = map(int, input().split())
+if __name__ == '__main__':
+    a, b, c, d, e, f = map(int, input().split())
 
-for x in range(-999, 1000):
-    for y in range(-999, 1000):
-        if a * x + b * y == c and d * x + e * y == f:
-            print(x, y)
+    for x in range(-999, 1000):
+        for y in range(-999, 1000):
+            if a * x + b * y == c and d * x + e * y == f:
+                print(x, y)
+                break
+        else:
+            continue
+        break
