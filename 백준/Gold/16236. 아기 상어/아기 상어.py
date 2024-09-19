883de75f5557ef04f6cf161d5824fa4183554226
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-19 16:23:04 +0900

    [Gold III] Title: 아기 상어, Time: 148 ms, Memory: 114156 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,14 +1,15 @@
 import collections
 import sys
 
-# 먹을 물고기 찾기
-def bfs(x, y):
-    global sx, sy, eat, shark_size
-    dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+# 상어 움직이기
+def move(x, y):
+    global sx, sy, eat, size
+    candi = []
     q = collections.deque()
+    dist = float('inf')
     q.append((x, y, 0))
     visited = [[False] * N for _ in range(N)]
-    candi = []
+    visited[x][y] = True
 
     while q:
         x, y, depth = q.popleft()
@@ -16,56 +17,55 @@ def bfs(x, y):
         for dx, dy in dir:
             nx, ny = x + dx, y + dy
 
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and board[nx][ny] <= size:
                 visited[nx][ny] = True
-                if board[nx][ny] > shark_size:
-                    continue
 
-                q.append((nx, ny, depth + 1))
-                if 0 < board[nx][ny] < shark_size:
-                    candi.append((depth + 1, nx, ny))
-    if not candi:
-        return -1
+                # 크기가 같은 물고기이거나 0이라면
+                if board[nx][ny] == size or board[nx][ny] == 0:
+                    # 먹을 수 없음, 이동만 가능
+                    q.append((nx, ny, depth + 1))
+                else:
+                    # 먹을 수 있는 후보에 추가 후 더 이상 움직이지 않아도 됨
+                    candi.append((nx, ny, depth + 1))
 
-    candi.sort()
-    time, sx, sy = candi[0]
-    board[sx][sy] = 0
-    eat += 1
-    if eat == shark_size:
-        shark_size += 1
-        eat = 0
+    candi.sort(key=lambda x: (x[2], x[0], x[1]))
 
-    return time
+    # 움직일 수 있다면
+    if candi:
+        # 움직이기
+        x, y, depth = candi[0]
+        sx, sy = x, y
+        eat += 1
+        board[x][y] = 0
+        if eat == size:
+            eat = 0
+            size += 1
+        return depth
+    else:
+        return 0
 
 
 if __name__ == '__main__':
+    dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     N = int(input())
     board = [list(map(int, input().split())) for _ in range(N)]
 
-    # 현재 상어의 크기
-    shark_size = 2
-
-    # 현재 먹은 물고기
+    sx, sy = -1, -1
+    size = 2
     eat = 0
 
-    # 현재 상어의 위치
-    sx, sy = -1, -1
     for i in range(N):
         for j in range(N):
             if board[i][j] == 9:
                 sx, sy = i, j
                 board[i][j] = 0
-                break
-        else:
-            continue
-        break
 
     result = 0
-    # 먹을 수 있는 상어 찾기
     while True:
-        time = bfs(sx, sy)
-        if time == -1:
+        val = move(sx, sy)
+        if val == 0:
             break
         else:
-            result += time
+            result += val
+
     print(result)
