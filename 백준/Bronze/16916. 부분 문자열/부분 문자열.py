441bb60dfea7ff9bd8a8e4052cb77faf28b77575
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-26 11:22:04 +0900

    [Bronze II] Title: 부분 문자열, Time: 40 ms, Memory: 34380 KB -BaekjoonHub

--- 백준/Bronze/16916. 부분 문자열/부분 문자열.py
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.py
@@ -1,44 +1,7 @@
-def KMP(S, P):
-    def LPS(pattern):
-        lps = [0] * len(pattern)
-        length = 0
-        i = 1
-
-        while i < len(pattern):
-            if pattern[i] == pattern[length]:
-                length += 1
-                lps[i] = length
-                i += 1
-            else:
-                if length != 0:
-                    length = lps[length - 1]
-                else:
-                    lps[i] = 0
-                    i += 1
-        return lps
-
-    n = len(S)
-    m = len(P)
-    lps = LPS(P)
-
-    i = 0  
-    j = 0  
-
-    while i < n:
-        if S[i] == P[j]:
-            i += 1
-            j += 1
-
-        if j == m:
-            return 1
-        elif i < n and S[i] != P[j]:
-            if j != 0:
-                j = lps[j - 1]
-            else:
-                i += 1
-    return 0
-
-
-S = input()
-P = input()
-print(KMP(S, P))
+if __name__ == '__main__':
+    S = input()
+    P = input()
+    if P in S:
+        print(1)
+    else:
+        print(0)
