b94763aa8ef0ef96bc66420c60a005c2ad297069
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-09-30 11:32:59 +0900

    [Silver IV] Title: 스택, Time: 124 ms, Memory: 111944 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,20 +1,61 @@
-stack = []
-for _ in range(int(input())):
-    command = input().split()
+import sys
 
-    if command[0] == 'push':
-        stack.append(int(command[1]))
-    elif command[0] == 'pop':
-        if stack:
+input = lambda: sys.stdin.readline().rstrip()
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.header = None
+        self.size = 0
+
+    def is_empty(self):
+        return 1 if self.header is None else 0
+
+    def push(self, x):
+        node = Node(x)
+        self.size += 1
+
+        node.next = self.header
+        self.header = node
+
+    def pop(self):
+
+        node = self.header
+        if node is None:
+            return -1
+        self.size -= 1
+        self.header = node.next
+
+        return node.val
+
+    def top(self):
+        if self.is_empty():
+            return -1
+        return self.header.val
+
+
+if __name__ == '__main__':
+    stack = Stack()
+
+    N = int(input())
+
+    for _ in range(N):
+        query, *data = input().split()
+
+        if query == 'push':
+            x = data[0]
+            stack.push(x)
+        elif query == 'pop':
             print(stack.pop())
-        else:
-            print(-1)
-    elif command[0] == 'size':
-        print(len(stack))
-    elif command[0] == 'empty':
-        print(int(stack == []))
-    elif command[0] == 'top':
-        if stack:
-            print(stack[-1])
-        else:
-            print(-1)
+        elif query == 'size':
+            print(stack.size)
+        elif query == 'empty':
+            print(stack.is_empty())
+        elif query == 'top':
+            print(stack.top())
