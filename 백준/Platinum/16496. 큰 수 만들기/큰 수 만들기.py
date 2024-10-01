df1c186b6ee97f7039af13e3f0229bb1c412c4c2
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-01 11:15:55 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 128 ms, Memory: 113728 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -1,24 +1,35 @@
-n = int(input())
-numbers = list(map(str, input().split()))
+import sys
+from functools import cmp_to_key
+
+input = lambda: sys.stdin.readline().rstrip()
 
 
-def compare(a, b):
-    if a + b > b + a:
-        return True
+def largest_number():
+    # 입력 받기
+    N = int(input())
+
+    # 모든 숫자를 문자열로 변환
+    numbers = input().split()
+
+    # 비교 함수 정의
+    def compare(a, b):
+        if a + b > b + a:
+            return -1  # a가 b보다 먼저 오도록
+        elif a + b < b + a:
+            return 1  # b가 a보다 먼저 오도록
+        else:
+            return 0
+
+    # 사용자 정의 정렬
+    numbers.sort(key=cmp_to_key(compare))
+
+    # 모든 숫자가 '0'인 경우 '0' 하나만 출력
+    if numbers[0] == '0':
+        print('0')
     else:
-        return False
+        print(''.join(numbers))
 
 
-for i in range(1, n):
-    key = numbers[i]
-    j = i - 1
-    while j >= 0 and compare(key, numbers[j]):
-        numbers[j + 1] = numbers[j]
-        j -= 1
-    numbers[j + 1] = key
-
-answer = ''.join(numbers)
-if answer[0] == '0':
-    print('0')
-else:
-    print(answer)
+# 함수 호출
+if __name__ == "__main__":
+    largest_number()
