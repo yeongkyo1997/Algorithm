e8d604179e12a6bb6e38e75a7171a6e59a3bbe59
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-01 11:25:35 +0900

    [Diamond V] Title: 제곱 ㄴㄴ, Time: 160 ms, Memory: 125868 KB -BaekjoonHub

--- 백준/Diamond/1557. 제곱 ㄴㄴ/제곱 ㄴㄴ.py
+++ 백준/Diamond/1557. 제곱 ㄴㄴ/제곱 ㄴㄴ.py
@@ -1,20 +1,51 @@
-def squareNoNo(n):
-    p = 0
-    for i in range(1, int(n ** 0.5) + 1, 1):
-        p += mobius[i] * (n // (i * i))
-    return p
-l, r = 0, 2000000000
-mobius = [0] * 1000001
-k = int(input())
-mobius[1] = 1
-for i in range(1, 1000001):
-    if mobius[i]:
-        for j in range(i * 2, 1000001, i):
-            mobius[j] -= mobius[i]
-while l < r - 1:
-    mid = (l + r) // 2
-    if squareNoNo(mid) < k:
-        l = mid
-    else:
-        r = mid
-print(r)
+import sys
+import math
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def calc_mobius(n):
+    mobius = [1] * (n + 1)
+    is_prime = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if is_prime[p]:
+            for multiple in range(p, n + 1, p):
+                is_prime[multiple] = False
+                mobius[multiple] *= -1
+            p_sq = p * p
+            for multiple in range(p_sq, n + 1, p_sq):
+                mobius[multiple] = 0
+    return mobius
+
+
+def cnt_no_square(n, mobius):
+    limit = int(math.isqrt(n))
+    ret = 0
+    for d in range(1, limit + 1):
+        ret += mobius[d] * (n // (d * d))
+    return ret
+
+
+def find(k):
+    low = 1
+    high = 2 * k
+
+    while low < high:
+        mid = (low + high) // 2
+        mobius = calc_mobius(int(math.isqrt(mid)) + 1)
+        cnt = cnt_no_square(mid, mobius)
+        if cnt < k:
+            low = mid + 1
+        else:
+            high = mid
+    return low
+
+
+def main():
+    K = int(input())
+    result = find(K)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
