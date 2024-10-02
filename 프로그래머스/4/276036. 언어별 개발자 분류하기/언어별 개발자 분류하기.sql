4f9e16b3759535ed74c763c2bdbb7e2eeff20959
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-02 10:09:11 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,36 +1,34 @@
-WITH FrontEndCode AS (
-    SELECT SUM(CODE) AS TotalCode
-    FROM SKILLCODES
-    WHERE CATEGORY = 'Front End'
+WITH SkillCodes AS (
+    SELECT 
+        MAX(CASE WHEN NAME = 'JavaScript' THEN CODE END) AS JavaScript,
+        MAX(CASE WHEN NAME = 'React' THEN CODE END) AS React,
+        MAX(CASE WHEN NAME = 'Vue' THEN CODE END) AS Vue,
+        MAX(CASE WHEN NAME = 'Python' THEN CODE END) AS Python,
+        MAX(CASE WHEN NAME = 'C#' THEN CODE END) AS CSharp
+    FROM 
+        SKILLCODES
 ),
-PythonCode AS (
-    SELECT CODE
-    FROM SKILLCODES
-    WHERE NAME = 'Python'
-),
-CSharpCode AS (
-    SELECT CODE
-    FROM SKILLCODES
-    WHERE NAME = 'C#'
+DeveloperGrades AS (
+    SELECT 
+        d.ID,
+        d.EMAIL,
+        CASE
+            WHEN (d.SKILL_CODE & sc.JavaScript > 0 OR d.SKILL_CODE & sc.React > 0 OR d.SKILL_CODE & sc.Vue > 0)
+                 AND d.SKILL_CODE & sc.Python > 0 THEN 'A'
+            WHEN d.SKILL_CODE & sc.CSharp > 0 THEN 'B'
+            WHEN (d.SKILL_CODE & sc.JavaScript > 0 OR d.SKILL_CODE & sc.React > 0 OR d.SKILL_CODE & sc.Vue > 0) THEN 'C'
+            ELSE NULL
+        END AS GRADE
+    FROM 
+        DEVELOPERS d
+    CROSS JOIN 
+        SkillCodes sc
 )
 SELECT 
-    CASE 
-        WHEN (d.SKILL_CODE & f.TotalCode) > 0 AND (d.SKILL_CODE & p.CODE) > 0 THEN 'A'
-        WHEN (d.SKILL_CODE & c.CODE) > 0 THEN 'B'
-        WHEN (d.SKILL_CODE & f.TotalCode) > 0 THEN 'C'
-    END AS GRADE,
-    d.ID,
-    d.EMAIL
+    GRADE, ID, EMAIL
 FROM 
-    DEVELOPERS d
-CROSS JOIN FrontEndCode f
-CROSS JOIN PythonCode p
-CROSS JOIN CSharpCode c
+    DeveloperGrades
 WHERE 
-    (d.SKILL_CODE & f.TotalCode) > 0 
-    OR (d.SKILL_CODE & p.CODE) > 0
-    OR (d.SKILL_CODE & c.CODE) > 0
-HAVING 
     GRADE IS NOT NULL
 ORDER BY 
     GRADE, ID;
