5f3f414bd5891a09dc8e338c8937ba3cbfa34ded
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-09 15:44:52 +0900

    [Gold III] Title: 치즈, Time: 196 ms, Memory: 117304 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,46 +1,51 @@
 import collections
+import sys
 
-dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-N, M = map(int, input().split())
-
-board = [list(map(int, input().split())) for _ in range(N)]
-one_cnt = 0
-for b in board:
-    one_cnt += b.count(1)
+input = lambda: sys.stdin.readline().rstrip()
 
 
-def bfs(x, y):
-    global one_cnt
+def melting():
+    melt_cnt = [[0] * M for _ in range(N)]
     q = collections.deque()
-    q.append((x, y))
-    visited = [[0] * M for _ in range(N)]
-    visited[x][y] = 1
+    q.append((0, 0))
+    visited = [[False] * M for _ in range(N)]
+    visited[0][0] = True
 
     while q:
         x, y = q.popleft()
+
         for dx, dy in dir:
             nx, ny = x + dx, y + dy
 
-            if 0 <= nx < N and 0 <= ny < M:
-                if board[nx][ny] == 0 and visited[nx][ny] == 0:
-                    visited[nx][ny] = 1
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+                if board[nx][ny] == 1:
+                    melt_cnt[nx][ny] += 1
+                else:
+                    visited[nx][ny] = True
                     q.append((nx, ny))
-                elif board[nx][ny] == 1:
-                    visited[nx][ny] += 1
-                    if visited[nx][ny] >= 2:
-                        board[nx][ny] = 0
-                        one_cnt -= 1
+
+    for i in range(N):
+        for j in range(M):
+            if melt_cnt[i][j] >= 2:
+                board[i][j] = 0
+                global cheese_cnt
+                cheese_cnt -= 1
 
 
-result = 0
+if __name__ == '__main__':
+    dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    N, M = map(int, input().split())
+    cheese_cnt = 0
+    board = [list(map(int, input().split())) for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] == 1:
+                cheese_cnt += 1
 
-while True:
-    bfs(0, 0)
-    result += 1
-    if one_cnt == 0:
-        print(result)
-        break
-    if result >= N * M * 2:
-        print(-1)
-        break
+    result = 0
+    while True:
+        if cheese_cnt == 0:
+            break
+        melting()
+        result += 1
+    print(result)
