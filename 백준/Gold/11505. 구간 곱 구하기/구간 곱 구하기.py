0f832263b1df3f1fbf800445b9212b798d8bdabe
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-09 15:20:33 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 540 ms, Memory: 156696 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
@@ -1,21 +1,25 @@
-import math
 import sys
+
+
+MOD = 1_000_000_007
 
 input = lambda: sys.stdin.readline().rstrip()
 
 
 def build(node, start, end):
     if start == end:
-        tree[node] = arr[start]
+        tree[node] = arr[start] % MOD
         return
 
     mid = start + end >> 1
+
     build(node * 2, start, mid)
     build(node * 2 + 1, mid + 1, end)
+
     tree[node] = (tree[node * 2] * tree[node * 2 + 1]) % MOD
 
 
-def update(idx, val, node, start, end):
+def update(val, idx, node, start, end):
     if idx < start or end < idx:
         return
 
@@ -24,8 +28,9 @@ def update(idx, val, node, start, end):
         return
 
     mid = start + end >> 1
-    update(idx, val, node * 2, start, mid)
-    update(idx, val, node * 2 + 1, mid + 1, end)
+
+    update(val, idx, node * 2, start, mid)
+    update(val, idx, node * 2 + 1, mid + 1, end)
     tree[node] = (tree[node * 2] * tree[node * 2 + 1]) % MOD
 
 
@@ -37,18 +42,23 @@ def query(left, right, node, start, end):
         return tree[node]
 
     mid = start + end >> 1
+
     return (query(left, right, node * 2, start, mid) * query(left, right, node * 2 + 1, mid + 1, end)) % MOD
 
 
 if __name__ == '__main__':
-    MOD = 1_000_000_007
     N, M, K = map(int, input().split())
+
     arr = [int(input()) for _ in range(N)]
-    tree = [0] * (N * 4)
+    tree = [1] * (4 * N)
     build(1, 0, N - 1)
+
     for _ in range(M + K):
-        a, b, c = map(int, input().split())
-        if a == 1:
-            update(b - 1, c, 1, 0, N - 1)
-        else:
-            print(query(b - 1, c - 1, 1, 0, N - 1))
+        q, *data = map(int, input().split())
+
+        if q == 1:
+            b, c = data
+            update(c, b - 1, 1, 0, N - 1)
+        elif q == 2:
+            b, c = data
+            print(query(b - 1, c - 1, 1, 0, N - 1) % MOD)
