13ff74275ea48c6d197d9b5cf4c3cead355622d7
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-09 15:34:30 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 696 ms, Memory: 186164 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.py
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.py
@@ -1,5 +1,6 @@
 import sys
 
+
 input = lambda: sys.stdin.readline().rstrip()
 
 
@@ -9,6 +10,7 @@ def build(node, start, end):
         return
 
     mid = start + end >> 1
+
     build(node * 2, start, mid)
     build(node * 2 + 1, mid + 1, end)
     tree[node] = tree[node * 2] + tree[node * 2 + 1]
@@ -17,15 +19,16 @@ def build(node, start, end):
 def propagation(node, start, end):
     if lazy[node] != 0:
         tree[node] += (end - start + 1) * lazy[node]
+
         if start != end:
             lazy[node * 2] += lazy[node]
             lazy[node * 2 + 1] += lazy[node]
+
         lazy[node] = 0
 
 
 def update(left, right, val, node, start, end):
     propagation(node, start, end)
-
     if right < start or end < left:
         return
 
@@ -38,6 +41,7 @@ def update(left, right, val, node, start, end):
         return
 
     mid = start + end >> 1
+
     update(left, right, val, node * 2, start, mid)
     update(left, right, val, node * 2 + 1, mid + 1, end)
     tree[node] = tree[node * 2] + tree[node * 2 + 1]
@@ -45,7 +49,6 @@ def update(left, right, val, node, start, end):
 
 def query(left, right, node, start, end):
     propagation(node, start, end)
-
     if right < start or end < left:
         return 0
 
@@ -53,22 +56,23 @@ def query(left, right, node, start, end):
         return tree[node]
 
     mid = start + end >> 1
-
     return query(left, right, node * 2, start, mid) + query(left, right, node * 2 + 1, mid + 1, end)
 
 
 if __name__ == '__main__':
     N, M, K = map(int, input().split())
-    arr = [int(input()) for _ in range(N)]
-    tree = [0] * (N * 4)
-    lazy = [0] * (N * 4)
-    build(1, 0, N - 1)
-    for _ in range(M + K):
-        a, *data = map(int, input().split())
 
-        if a == 1:
+    arr = [int(input()) for _ in range(N)]
+    tree = [0] * (4 * N)
+    lazy = [0] * (4 * N)
+    build(1, 0, N - 1)
+
+    for _ in range(M + K):
+        q, *data = map(int, input().split())
+
+        if q == 1:
             b, c, d = data
             update(b - 1, c - 1, d, 1, 0, N - 1)
-        else:
+        elif q == 2:
             b, c = data
             print(query(b - 1, c - 1, 1, 0, N - 1))
