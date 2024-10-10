82d150e26ce22f29c0c87b350417aa55a6ad3b59
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-10 20:23:57 +0900

    [Gold IV] Title: 스도쿠, Time: 488 ms, Memory: 21428 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,76 +1,66 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-    static final int BOARD_SIZE = 9;
-    static int[][] board = new int[BOARD_SIZE][BOARD_SIZE];
-    static ArrayList<Pair> list = new ArrayList<>();
-
-    static class Pair {
-        int x;
-        int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < BOARD_SIZE; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-                if (board[i][j] == 0)
-                    list.add(new Pair(i, j));
-            }
-        }
-        sol(0);
-    }
+    static int[][] board = new int[9][9];
+    static List<int[]> zero = new ArrayList<>();
 
     static boolean check(int x, int y, int num) {
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            if (board[x][i] == num || board[i][y] == num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[x][i] == num)
                 return false;
-            }
+            if (board[i][y] == num)
+                return false;
         }
+
         for (int i = (x / 3) * 3; i < (x / 3) * 3 + 3; i++) {
             for (int j = (y / 3) * 3; j < (y / 3) * 3 + 3; j++) {
-                if (board[i][j] == num) {
+                if (board[i][j] == num)
                     return false;
-                }
             }
         }
         return true;
     }
 
-    static void sol(int depth) throws IOException {
-        if (depth == list.size()) {
-            for (int i = 0; i < BOARD_SIZE; i++) {
-                for (int j = 0; j < BOARD_SIZE; j++) {
+    static void dfs(int depth) throws IOException {
+        if (depth == zero.size()) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
                     bw.write(board[i][j] + " ");
                 }
-                bw.write("\n" + "");
+                bw.newLine();
             }
-            bw.flush();
             bw.close();
             System.exit(0);
+            return;
         }
-        for (int i = 1; i < 10; i++) {
-            Pair pair = list.get(depth);
-            if (check(pair.x, pair.y, i)) {
-                board[pair.x][pair.y] = i;
-                sol(depth + 1);
-                board[pair.x][pair.y] = 0;
+
+        for (int i = 1; i <= 9; i++) {
+            int[] node = zero.get(depth);
+            int x = node[0];
+            int y = node[1];
+
+            if (check(x, y, i)) {
+                board[x][y] = i;
+                dfs(depth + 1);
+                board[x][y] = 0;
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if (board[i][j] == 0)
+                    zero.add(new int[] { i, j });
+            }
+        }
+
+        dfs(0);
     }
 }
