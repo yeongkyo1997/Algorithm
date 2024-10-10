11faa40b58ed9067aaa3505ee3d427fe4e75dff8
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-10 19:30:16 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 18268 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,37 +1,37 @@
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
-
-    static boolean[][] map;
-    private static int N;
+    static int N;
+    static int[][] board;
 
     public static void main(String[] args) throws IOException {
-        map = new boolean[100][100];
         N = Integer.parseInt(br.readLine());
+        board = new int[1000][1000];
+        int result = 0;
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            for (int j = x; j < x + 10; j++) {
-                for (int k = y; k < y + 10; k++) {
-                    map[j][k] = true;
+
+            for (int row = x; row < x + 10; row++) {
+                for (int col = y; col < y + 10; col++) {
+                    board[row][col] = 1;
                 }
             }
         }
-        int cnt = 0;
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (map[i][j]) cnt++;
+
+        for (int i = 1; i < 101; i++) {
+            for (int j = 1; j < 101; j++) {
+                if (board[i][j] == 1)
+                    result++;
             }
         }
-        bw.write(cnt + "");
-        bw.flush();
+        bw.write(result + "");
         bw.close();
-
     }
-
 }
