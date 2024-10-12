7f81b3a0283da640df8d1c2453c0731f5cac05cc
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-12 15:01:37 +0900

    [Gold IV] Title: 공유기 설치, Time: 140 ms, Memory: 112092 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,33 +1,35 @@
 import sys
 
+input = lambda: sys.stdin.readline().rstrip()
 
-def input(): return sys.stdin.readline().rstrip()
+if __name__ == '__main__':
+    N, C = map(int, input().split())
 
+    arr = [int(input()) for _ in range(N)]
+    arr.sort()
+    start = 1
+    end = arr[-1] - arr[0]
 
-N, C = map(int, input().split())
+    result = 0
 
-arr = [int(input()) for _ in range(N)]
+    while (start <= end):
+        # 가장 가까운 공유기 사이의 최대 거리(최대 거리이기 때문에 lower bound를 사용해야한다.
+        mid = start + end >> 1
+        # 현재 가장 가까운 공유기
+        cur = arr[0]
+        # 선택된 공유기의 개수
+        cnt = 1
 
+        for i in range(1, N):
+            # 가장 가까운 공유기의 거리와 같거나 크다면 공유기 개수 증가
+            if arr[i] - cur >= mid:
+                cnt += 1
+                cur = arr[i]
 
-arr.sort()
+        if cnt >= C:
+            start = mid + 1
+            result = mid
+        elif cnt < C:
+            end = mid - 1
 
-start = 1
-end = 2 ** 30
-
-while start <= end:
-    mid = (start + end) // 2
-    cur = arr[0]
-    cnt = 1
-
-    for i in range(1, len(arr)):
-        if arr[i] >= cur + mid:
-            cnt += 1
-            cur = arr[i]
-
-    if cnt >= C:
-        start = mid + 1
-        result = mid
-    else:
-        end = mid - 1
-
-print(result)
+    print(result)
