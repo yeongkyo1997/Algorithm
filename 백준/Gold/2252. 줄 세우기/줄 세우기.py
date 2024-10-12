1c7085df8444c4b8ba24530fffc9071b34097afc
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-12 22:54:07 +0900

    [Gold III] Title: 줄 세우기, Time: 184 ms, Memory: 121004 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,32 +1,34 @@
+import collections
 import sys
-from collections import deque, defaultdict
 
+input = lambda: sys.stdin.readline().rstrip()
+if __name__ == '__main__':
+    N, M = map(int, input().split())
 
-def input(): return sys.stdin.readline().rstrip()
+    # 진입 차수를 저장
+    in_degree = [0] * (N + 1)
+    # 그래프 저장
+    graph = [[] for _ in range(N + 1)]
 
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        in_degree[b] += 1
 
-N, M = map(int, input().split())
-graph = defaultdict(list)
-in_degree = defaultdict(int)
-result = []
-
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    in_degree[b] += 1
-
-q = deque()
-for i in range(1, N + 1):
-    if in_degree[i] == 0:
-        q.append(i)
-
-while q:
-    cur = q.popleft()
-    result.append(cur)
-
-    for i in graph[cur]:
-        in_degree[i] -= 1
+    q = collections.deque()
+    for i in range(1, N + 1):
         if in_degree[i] == 0:
             q.append(i)
 
-print(*result)
+    result = []
+
+    while q:
+        node = q.popleft()
+        result.append(node)
+
+        for g in graph[node]:
+            in_degree[g] -= 1
+            if in_degree[g] == 0:
+                q.append(g)
+
+    print(*result)
