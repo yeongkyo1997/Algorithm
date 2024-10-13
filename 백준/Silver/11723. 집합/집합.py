954c3c964bc6ee3d7b5ae4db27cd5d892d4d49b1
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-14 00:28:45 +0900

    [Silver V] Title: 집합, Time: 1116 ms, Memory: 134956 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -2,23 +2,26 @@ import sys
 
 input = lambda: sys.stdin.readline().rstrip()
 
-s = set()
-for _ in range(int(input())):
-    command = input().rstrip().split()
-
-    if command[0] == 'add':
-        s.add(int(command[1]))
-    elif command[0] == 'remove':
-        s.discard(int(command[1]))
-    elif command[0] == 'check':
-        print(int(int(command[1]) in s))
-    elif command[0] == 'toggle':
-        val = int(command[1])
-        if val in s:
-            s.discard(val)
-        else:
-            s.add(val)
-    elif command[0] == 'all':
-        s = set(range(1, 21))
-    else:
-        s = set()
+if __name__ == '__main__':
+    S = 0
+    for _ in range(int(input())):
+        q, *data = input().split()
+        if q == 'add':
+            x = int(data[0])
+            S |= 1 << x
+        elif q == 'remove':
+            x = int(data[0])
+            S &= ~(1 << x)
+        elif q == 'check':
+            x = int(data[0])
+            print(int(S & (1 << x) != 0))
+        elif q == 'toggle':
+            x = int(data[0])
+            if S & (1 << x):
+                S &= ~ (1 << x)
+            else:
+                S |= (1 << x)
+        elif q == 'all':
+            S = (1 << 21) - 1
+        elif q == 'empty':
+            S = 0
