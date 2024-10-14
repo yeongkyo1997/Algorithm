f143216a794426e07a030c64cccec514551ccb82
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-14 10:14:03 +0900

    [Gold I] Title: 외판원 순회, Time: 324 ms, Memory: 126944 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -1,36 +1,32 @@
-from functools import cache
 import sys
 
 
-def input(): return sys.stdin.readline().rstrip()
+def tsp(N, board):
+    INF = float('inf')
+    dp = [[INF] * (1 << N) for _ in range(N)]
+    dp[0][1] = 0
 
+    for visited in range(1 << N):
+        for last in range(N):
+            if not (visited & (1 << last)):
+                continue
+            for next in range(N):
+                if visited & (1 << next):
+                    continue
+                if board[last][next] == 0:
+                    continue
+                next_visited = visited | (1 << next)
+                dp[next][next_visited] = min(dp[next][next_visited], dp[last][visited] + board[last][next])
 
-INF = int(1e9)
-N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
-dp = [[INF] * (1 << N) for _ in range(N)]
-
-
-@cache
-def dfs(cur, visited):
-    if visited == (1 << N) - 1:
-        if board[cur][0]:
-            return board[cur][0]
-        else:
-            return INF
-
-    if dp[cur][visited] != INF:
-        return dp[cur][visited]
-
+    res = INF
     for i in range(1, N):
-        if visited & (1 << i):
-            continue
-        if not board[cur][i]:
-            continue
-
-        dp[cur][visited] = min(dp[cur][visited], dfs(
-            i, visited | (1 << i)) + board[cur][i])
-    return dp[cur][visited]
+        if board[i][0] != 0:
+            res = min(res, dp[i][(1 << N) - 1] + board[i][0])
+    return res if res != INF else -1
 
 
-print(dfs(0, 1))
+if __name__ == '__main__':
+    N = int(input())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    result = tsp(N, board)
+    print(result)
