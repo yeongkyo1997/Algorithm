c73506907e803e3859d31e0a39dfed1aa3073962
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-14 09:40:46 +0900

    [Silver II] Title: 외판원 순회 2, Time: 1088 ms, Memory: 110432 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,27 +1,26 @@
+import itertools
+import math
 import sys
 
-sys.setrecursionlimit(10 ** 6)
-input = lambda: sys.stdin.readline().rstrip()
 
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-dp = [[-1] * (1 << n) for _ in range(n)]
+if __name__ == '__main__':
+    N = int(input())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    result = math.inf
 
+    for perm in itertools.permutations(range(N), N):
+        perm = list(perm)
+        perm.append(perm[0])
 
-def tsp(cur, visited):
-    if visited == (1 << n) - 1:
-        if arr[cur][0] != 0:
-            return arr[cur][0]
-        return 1e9
-    
-    if dp[cur][visited] != -1:
-        return dp[cur][visited]
-    
-    dp[cur][visited] = 1e9
-    for i in range(n):
-        if arr[cur][i] != 0 and visited & (1 << i) == 0:
-            dp[cur][visited] = min(dp[cur][visited], tsp(i, visited | (1 << i)) + arr[cur][i])
-    return dp[cur][visited]
+        start = perm[0]
 
+        total = 0
+        for nxt in perm[1:]:
+            if board[start][nxt] == 0:
+                break
+            total += board[start][nxt]
+            start = nxt
+        else:
+            result = min(result, total)
 
-print(tsp(0, 1))
+    print(result)
