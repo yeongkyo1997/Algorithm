a9f1ca5798696da763b1cef9d78cb773ade9d7b6
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-15 15:44:25 +0900

    [Silver I] Title: 용돈 관리, Time: 152 ms, Memory: 112344 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.py
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.py
@@ -1,32 +1,31 @@
 import sys
 
 
-def input(): return sys.stdin.readline().rstrip()
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    arr = [int(input()) for _ in range(N)]
+    max_val = max(arr)
+    left = 1
+    right = int(1e10)
+    result = 0
 
+    while left <= right:
+        # 목표 인출 금액(조정해야할 값임)
+        mid = left + right >> 1
+        # 현재 가지고 있는돈
+        cur = mid
+        cnt = 1
 
-N, M = map(int, input().split())
+        for a in arr:
+            if cur < a:
+                cur = mid
+                cnt += 1
+            cur -= a
 
-arr = [int(input()) for _ in range(N)]
+        if cnt > M or mid < max_val:
+            left = mid + 1
+        else:
+            right = mid - 1
+            result = mid
 
-
-start = min(arr)
-end = sum(arr)
-
-while start <= end:
-    mid = (start + end) // 2
-    cur = mid
-    cnt = 1
-
-    for i in arr:
-        if cur < i:
-            cur = mid
-            cnt += 1
-        cur -= i
-
-    if mid < max(arr) or cnt > M:
-        start = mid + 1
-    else:
-        end = mid - 1
-        result = mid
-
-print(result)
+    print(result)
