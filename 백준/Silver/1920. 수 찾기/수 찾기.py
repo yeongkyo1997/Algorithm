b5e26d6f9684089e0e088c51c222041c0adf5287
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-16 12:01:23 +0900

    [Silver IV] Title: 수 찾기, Time: 204 ms, Memory: 136000 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,6 +1,24 @@
-N = int(input())
-s = set(map(int, input().split()))
+def binary_search(x):
+    start = 0
+    end = N - 1
 
-input()
-for i in map(int, input().split()):
-    print(int(i in s))
+    while start <= end:
+        mid = start + end >> 1
+
+        if arr[mid] == x:
+            return 1
+        elif arr[mid] > x:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return 0
+
+
+if __name__ == '__main__':
+    N = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    M = int(input())
+
+    for a in list(map(int, input().split())):
+        print(binary_search(a))
