823ac07131b306ea2faf658ffaef8f8be82c39c3
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-16 12:13:29 +0900

    [Silver II] Title: 과자 나눠주기, Time: 840 ms, Memory: 254052 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -1,27 +1,27 @@
 import sys
 
 
-def input(): return sys.stdin.readline().rstrip()
+if __name__ == '__main__':
+    M, N = map(int, input().split())
+    arr = list(map(int, input().split()))
+    arr.sort()
 
+    start = 1
+    end = int(1e9)
 
-M, N = map(int, input().split())
+    result = 0
+    while start <= end:
+        # 길이
+        mid = start + end >> 1
 
-arr = list(map(int, input().split()))
+        cnt = 0
+        for a in arr:
+            cnt += a // mid
 
-start = 1
-end = max(arr)
+        if cnt >= M:
+            start = mid + 1
+            result = mid
+        else:
+            end = mid - 1
 
-while start <= end:
-    mid = (start + end) // 2
-    cnt = 0
-
-    for i in arr:
-        if i >= mid:
-            cnt += i // mid
-
-    if cnt >= M:
-        start = mid + 1
-    else:
-        end = mid - 1
-
-print(end)
+    print(result)
