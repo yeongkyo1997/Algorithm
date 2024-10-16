e28b17c21f3969592f5f61b69f5d3e4b94a82b8e
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-16 14:08:51 +0900

    [Gold IV] Title: 공유기 설치, Time: 176 ms, Memory: 113124 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,35 +1,26 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
 if __name__ == '__main__':
-    N, C = map(int, input().split())
-
+    N, M = map(int, input().split())
     arr = [int(input()) for _ in range(N)]
     arr.sort()
-    start = 1
-    end = arr[-1] - arr[0]
+    left = 1
+    right = int(1e9)
 
     result = 0
+    while left <= right:
+        mid = (left + right) // 2
 
-    while (start <= end):
-        # 가장 가까운 공유기 사이의 최대 거리(최대 거리이기 때문에 lower bound를 사용해야한다.
-        mid = start + end >> 1
-        # 현재 가장 가까운 공유기
-        cur = arr[0]
-        # 선택된 공유기의 개수
         cnt = 1
+        cur = arr[0]
 
-        for i in range(1, N):
-            # 가장 가까운 공유기의 거리와 같거나 크다면 공유기 개수 증가
-            if arr[i] - cur >= mid:
+        for a in arr:
+            if a - cur >= mid:
                 cnt += 1
-                cur = arr[i]
+                cur = a
 
-        if cnt >= C:
-            start = mid + 1
+        if cnt >= M:
+            left = mid + 1
             result = mid
-        elif cnt < C:
-            end = mid - 1
+        else:
+            right = mid - 1
 
     print(result)
