95eb3e700abce7940bd186a4af7b8a33d9155526
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-16 13:17:48 +0900

    [Silver IV] Title: 제곱근, Time: 100 ms, Memory: 110428 KB -BaekjoonHub

--- 백준/Silver/13706. 제곱근/제곱근.py
+++ 백준/Silver/13706. 제곱근/제곱근.py
@@ -1,21 +1,18 @@
-import sys
+if __name__ == '__main__':
+    N = int(input())
 
+    left = 0
+    right = N
+    result = 0
+    while left <= right:
+        mid = (left + right) // 2
 
-def input(): return sys.stdin.readline().rstrip()
+        if mid ** 2 == N:
+            result = mid
+            break
+        elif mid ** 2 > N:
+            right = mid - 1
+        else:
+            left = mid + 1
 
-
-N = int(input())
-
-start = 1
-end = N
-
-while start <= end:
-    mid = (start + end) // 2
-
-    if mid ** 2 == N:
-        print(mid)
-        break
-    elif mid ** 2 < N:
-        start = mid + 1
-    else:
-        end = mid - 1
+    print(result)
