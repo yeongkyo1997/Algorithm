bc1301807f11c0d894c7984521e5063f6f81ff3d
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-18 17:21:14 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1944 ms, Memory: 272304 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,4 +1,25 @@
-N = int(input())
-arr = [int(input()) for _ in range(N)]
+import random
+import sys
 
-print(*sorted(arr), sep='\n')
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot_idx = random.randint(0, len(arr) - 1)
+    pivot = arr[pivot_idx]
+
+    left = [x for x in arr if pivot > x]
+    mid = [x for x in arr if pivot == x]
+    right = [x for x in arr if pivot < x]
+
+    return quick_sort(left) + mid + quick_sort(right)
+
+
+if __name__ == '__main__':
+    N = int(input())
+    arr = [int(input()) for _ in range(N)]
+
+    arr = quick_sort(arr)
+    print(*arr)
