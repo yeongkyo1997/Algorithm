bac3576bbbf1536ca18d7a5eb8eba19f000d3fa6
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-24 12:23:00 +0900

    [Gold III] Title: 줄 세우기, Time: 556 ms, Memory: 47696 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,46 +1,57 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        List<List<Integer>> list = new ArrayList<>();
-        int[] edgeCount = new int[N + 1];
+        int[] inDegree = new int[N + 10];
+        Arrays.fill(inDegree, 0);
+        Queue<Integer> q = new ArrayDeque<>();
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
-        for (int i = 0; i < N + 1; i++) {
-            list.add(new ArrayList<>());
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            list.get(a).add(b);
-            edgeCount[b]++;
+            graph.get(a).add(b);
+            inDegree[b]++;
         }
 
-        Queue<Integer> queue = new ArrayDeque<>();
-        for (int i = 1; i < N + 1; i++) {
-            if (edgeCount[i] == 0) queue.offer(i);
+        for (int i = 1; i <= N; i++) {
+            if (inDegree[i] == 0)
+                q.offer(i);
         }
 
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            bw.write(cur + " ");
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            bw.write(x + " ");
 
-            List<Integer> tmp = list.get(cur);
+            for (int g : graph.get(x)) {
+                inDegree[g]--;
 
-            for (Integer ele : tmp) {
-                if (--edgeCount[ele] == 0) queue.offer(ele);
+                if (inDegree[g] == 0) {
+                    q.add(g);
+                }
             }
         }
-
         bw.close();
     }
 }
