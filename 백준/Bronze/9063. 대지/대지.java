73b45cf0cd203ebfcf1dd2f87c319ef50a6ea611
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-25 13:46:22 +0900

    [Bronze III] Title: 대지, Time: 264 ms, Memory: 27368 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        int N = Integer.parseInt(br.readLine());
+        int min_x = Integer.MAX_VALUE;
+        int max_x = -Integer.MIN_VALUE;
+        int min_y = Integer.MAX_VALUE;
+        int max_y = -Integer.MIN_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            min_x = Math.min(x, min_x);
+            max_x = Math.max(max_x, x);
+            min_y = Math.min(min_y, y);
+            max_y = Math.max(max_y, y);
+        }
+
+        bw.write((max_x - min_x) * (max_y - min_y) + "");
+
+        bw.close();
+    }
+}
