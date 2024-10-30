b6d688a54619541fa11681a697d6e91454bc8f78
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-10-30 16:44:21 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int N;
+    cin >> N;
+    vector<int> arr;
+    while (N != 0)
+    {
+        arr.push_back(N % 10);
+        N /= 10;
+    }
+
+    sort(arr.begin(), arr.end(), greater<int>());
+
+    for (auto a : arr)
+    {
+        cout << a;
+    }
+}
