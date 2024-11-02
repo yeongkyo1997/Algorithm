6f320f55d265fbbaa128a2b3c0c1d501ce8b86dc
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-11-02 13:26:14 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int year;
+    cin >> year;
+
+    cout << year - 1946;
+}
