01861bf73d8989713f88b455c6381ffff8d161ab
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-11-02 13:58:35 +0900

    [Bronze V] Title: A+B - 9, Time: 212 ms, Memory: 17412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -1,17 +1,19 @@
+import java.util.*;
 import java.io.*;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.math.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        BigInteger a, b;
         st = new StringTokenizer(br.readLine());
-        BigInteger A = new BigInteger(String.valueOf(Integer.parseInt(st.nextToken())));
-        BigInteger B = new BigInteger(String.valueOf(Integer.parseInt(st.nextToken())));
-        bw.write(A.add(B) + "\n");
+        a = new BigInteger(st.nextToken());
+        b = new BigInteger(st.nextToken());
+        bw.write(a.add(b) + "");
+
         bw.close();
     }
 }
