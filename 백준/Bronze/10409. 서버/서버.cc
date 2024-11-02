500763ad8e0d4bbeb02d92db05474aacdd360eaf
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-11-02 19:08:31 +0900

    [Bronze III] Title: 서버, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10409. 서버/서버.cc
+++ 백준/Bronze/10409. 서버/서버.cc
@@ -1,16 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
-int main(void)
+
+int main()
 {
-	int n, t, arr[50], sum = 0, cnt = 0;
-	cin >> n >> t;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	for (int i = 0; i < n; i++)
-	{
-		if (sum + arr[i] > t)
-			break;
-		sum += arr[i], cnt++;
-	}
-	cout << cnt;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, T;
+    cin >> n >> T;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    int result = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        sum += arr[i];
+        if (sum > T)
+            break;
+        result += 1;
+    }
+    cout << result;
 }
