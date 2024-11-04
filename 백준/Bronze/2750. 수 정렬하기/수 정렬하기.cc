3e1cc1947d054d093d9304e3180f6ba0b0d33eda
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-11-04 15:16:30 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,53 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
-
-void quickSort(vector<int> &arr, int start, int end)
-{
-    if (start >= end)
-        return;
-
-    if (start < end)
-    {
-        int i = start - 1;
-        int j = end + 1;
-        int pivot = arr[(start + end) / 2];
-
-        while (i < j)
-        {
-            while (arr[++i] < pivot)
-            {
-            }
-            while (arr[--j] > pivot)
-            {
-            }
-
-            if (i >= j)
-                break;
-
-            swap(arr[i], arr[j]);
-        }
-        quickSort(arr, start, i - 1);
-        quickSort(arr, j + 1, end);
-    }
-}
 
 int main()
 {
     int N;
     cin >> N;
-    vector<int> arr(N);
 
-    for (int i = 0; i < N; i++)
+    vector<int> arr;
+
+    while (N--)
     {
-        cin >> arr[i];
+        int a;
+        cin >> a;
+        arr.push_back(a);
     }
 
-    quickSort(arr, 0, N - 1);
-
+    sort(arr.begin(), arr.end());
     for (auto a : arr)
     {
-        cout << a << "\n";
+        cout << a << '\n';
     }
 }
