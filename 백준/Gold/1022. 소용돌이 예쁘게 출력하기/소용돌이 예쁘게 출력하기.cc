0cd3f949448c8706e59b84d2949fec13d946b50f
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-11-04 10:59:08 +0900

    [Gold III] Title: 소용돌이 예쁘게 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.cc
+++ 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.cc
@@ -1,53 +1,77 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <iomanip>
-#include <cmath>
-#include <algorithm>
 using namespace std;
 
-int calculate(int row, int column) {
-    int border = max(abs(row), abs(column));
-    int min_val = pow(2 * border - 1, 2) + 1;
-
-    if (row == border) {
-        return min_val + 7 * border - 1 + column;
-    }
-
-    if (column == -border) {
-        return min_val + 5 * border - 1 + row;
-    }
-
-    if (row == -border) {
-        return min_val + 3 * border - 1 - column;
-    }
-
-    return min_val + border - 1 - row;
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
+int main()
+{
+    ios::sync_with_stdio(false);
     cin.tie(0);
-
-    int r1, c1, r2, c2;
+    long long r1, c1, r2, c2;
     cin >> r1 >> c1 >> r2 >> c2;
 
-    int vortex[r2 - r1 + 1][c2 - c1 + 1];
-    int max_val = 0;
-    for (int i = r1; i <= r2; i++) {
-        for (int j = c1; j <= c2; j++) {
-            vortex[i - r1][j - c1] = calculate(i, j);
-            max_val = max(max_val, vortex[i - r1][j - c1]);
+    int n_rows = r2 - r1 + 1;
+    int n_cols = c2 - c1 + 1;
+
+    vector<vector<long long>> grid(n_rows, vector<long long>(n_cols, 0));
+    long long max_num_in_grid = 1;
+
+    for (int row = 0; row < n_rows; ++row)
+    {
+        long long r = r1 + row;
+        for (int col = 0; col < n_cols; ++col)
+        {
+            long long c = c1 + col;
+            long long k = max(abs(r), abs(c));
+            long long num;
+            if (k == 0)
+            {
+                num = 1;
+            }
+            else
+            {
+                long long max_num = (2 * k + 1) * (2 * k + 1);
+                if (r == k)
+                {
+                    num = max_num - (k - c);
+                }
+                else if (c == -k)
+                {
+                    num = max_num - 2 * k - (k - r);
+                }
+                else if (r == -k)
+                {
+                    num = max_num - 4 * k - (k + c);
+                }
+                else if (c == k)
+                {
+                    num = max_num - 6 * k - (k + r);
+                }
+                else
+                {
+                    // This should not happen as k = max(abs(r), abs(c))
+                    num = 1;
+                }
+            }
+            grid[row][col] = num;
+            if (num > max_num_in_grid)
+            {
+                max_num_in_grid = num;
+            }
         }
     }
 
-    int max_len = to_string(max_val).length();
+    int len = to_string(max_num_in_grid).size();
 
-    for (int i = 0; i <= r2 - r1; i++) {
-        for (int j = 0; j <= c2 - c1; j++) {
-            cout << setw(max_len) << vortex[i][j];
-            if (j != c2 - c1) cout << ' ';
+    for (int row = 0; row < n_rows; ++row)
+    {
+        for (int col = 0; col < n_cols; ++col)
+        {
+            if (col != 0)
+            {
+                cout << ' ';
+            }
+            cout << right << setw(len) << grid[row][col];
         }
         cout << '\n';
     }
-
-    return 0;
 }
