9fe57cbed0040f6094ab0f56c28fb6fc17a0ceb3
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-11-19 16:56:32 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
@@ -1,94 +1,147 @@
-#include <iostream>
-#include <algorithm>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct balls {
-	int depth;
-	int rx, ry, bx, by;
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+struct State
+{
+    int rx, ry;
+    int bx, by;
+    int moves;
 };
-int irx, iry, ibx, iby, hx, hy;
 
-int n, m, ans = -1;
-int map[10][10];
-int dir[4][2] = { {1,0},{0,1},{-1,0},{0,-1} };
-bool visit[10][10][10][10];
-char str[11];
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    vector<string> board(N);
+    for (auto &s : board)
+        cin >> s;
 
-void move(int& x, int& y, int d) {
-	while (1) {
-		x += dir[d][0]; y += dir[d][1];
-		if (map[x][y] == 1) {
-			x -= dir[d][0]; y -= dir[d][1];
-			break;
-		}
-		else if (map[x][y] == 2)
-			break;
-	}
-}
+    int rx, ry, bx, by, hole_x, hole_y;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (board[i][j] == 'R')
+            {
+                rx = i;
+                ry = j;
+                board[i][j] = '.';
+            }
+            if (board[i][j] == 'B')
+            {
+                bx = i;
+                by = j;
+                board[i][j] = '.';
+            }
+            if (board[i][j] == 'O')
+            {
+                hole_x = i;
+                hole_y = j;
+            }
+        }
+    }
 
-int main() {
-	cin >> n >> m;
-	for (int i = 0; i < n; ++i) {
-		cin >> str;
-		for (int j = 0; j < m; ++j) {
-			switch (str[j]) {
-			case '.':
-				map[i][j] = 0; break;
-			case '#':
-				map[i][j] = 1; break;
-			case 'O':
-				map[i][j] = 2; hx = i, hy = j; break;
-			case 'R':
-				irx = i, iry = j; break;
-			case 'B':
-				ibx = i, iby = j; break;
-			}
-		}
-	}
+    bool visited[10][10][10][10];
+    memset(visited, false, sizeof(visited));
+    queue<State> q;
+    q.push(State{rx, ry, bx, by, 0});
+    visited[rx][ry][bx][by] = true;
+    int result = -1;
 
-	queue<balls> q;
-	q.push({ 0,irx,iry,ibx,iby });
-	visit[irx][iry][ibx][iby] = true;
+    while (!q.empty())
+    {
+        State current = q.front();
+        q.pop();
+        if (current.moves >= 10)
+            continue;
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int nrx = current.rx, nry = current.ry;
+            int nbx = current.bx, nby = current.by;
+            bool red_in = false, blue_in = false;
 
-	while (!q.empty()) {
-		balls cur = q.front(); q.pop();
+            auto move_ball = [&](int &x, int &y) -> bool
+            {
+                while (true)
+                {
+                    if (board[x + dx[dir]][y + dy[dir]] == '#')
+                        break;
+                    x += dx[dir];
+                    y += dy[dir];
+                    if (x == hole_x && y == hole_y)
+                        return true;
+                }
+                return false;
+            };
 
-		if (cur.depth > 10)
-			break;
-		if (cur.rx == hx && cur.ry == hy) {
-			ans = cur.depth;
-			break;
-		}
+            bool first_red = false;
+            if (dir == 0)
+            {
+                if (current.rx < current.bx)
+                    first_red = true;
+            }
+            else if (dir == 1)
+            {
+                if (current.rx > current.bx)
+                    first_red = true;
+            }
+            else if (dir == 2)
+            {
+                if (current.ry < current.by)
+                    first_red = true;
+            }
+            else if (dir == 3)
+            {
+                if (current.ry > current.by)
+                    first_red = true;
+            }
 
-		for (int i = 0; i < 4; ++i) {
-			int rx = cur.rx, ry = cur.ry;
-			int bx = cur.bx, by = cur.by;
-			move(rx, ry, i), move(bx, by, i);
+            if (first_red)
+            {
+                red_in = move_ball(nrx, nry);
+                blue_in = move_ball(nbx, nby);
+            }
+            else
+            {
+                blue_in = move_ball(nbx, nby);
+                red_in = move_ball(nrx, nry);
+            }
 
-			if (bx == hx && by == hy)
-				continue;
+            if (blue_in)
+                continue;
+            if (red_in)
+            {
+                result = current.moves + 1;
+                cout << result;
+                return 0;
+            }
 
-			if (rx == bx && ry == by) {
-				switch (i) {
-				case 0:
-					cur.rx < cur.bx ? rx-- : bx--; break;
-				case 2:
-					cur.rx < cur.bx ? bx++ : rx++; break;
-				case 1:
-					cur.ry < cur.by ? ry-- : by--; break;
-				case 3:
-					cur.ry < cur.by ? by++ : ry++; break;
-				}
-			}
+            if (nrx == nbx && nry == nby)
+            {
+                int red_dist = abs(nrx - current.rx) + abs(nry - current.ry);
+                int blue_dist = abs(nbx - current.bx) + abs(nby - current.by);
+                if (red_dist > blue_dist)
+                {
+                    nrx -= dx[dir];
+                    nry -= dy[dir];
+                }
+                else
+                {
+                    nbx -= dx[dir];
+                    nby -= dy[dir];
+                }
+            }
 
-			if (!visit[rx][ry][bx][by]) {
-				balls next = { cur.depth + 1,rx,ry,bx,by };
-				q.push(next);
-				visit[rx][ry][bx][by] = true;
-			}
-		}
-	}
+            if (!visited[nrx][nry][nbx][nby])
+            {
+                visited[nrx][nry][nbx][nby] = true;
+                q.push(State{nrx, nry, nbx, nby, current.moves + 1});
+            }
+        }
+    }
 
-	cout << ans;
+    cout << result;
 }
