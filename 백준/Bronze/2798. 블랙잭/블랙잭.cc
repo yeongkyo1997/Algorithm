cf73582b9cd3e7d6a77cc29efb9036847e4d6462
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-11-20 20:03:35 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,45 +1,38 @@
 #include <iostream>
+#include <stack>
+#include <vector>
 
 using namespace std;
 
-int arr[100];
-int selection[3]; // 고른 카드
-int MAX = 0; // 골른 카드중 가장 큰 값
-int size; // 카드의 갯수
-int n; // 카드의 최대 크기
-int sum = 0;
-int count = 0; // selection의 인덱스
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
-int val(int a[]) {
-	int s = 0;
-	for (int i = 0; i < 3; i++)
-		s += a[i];
+    int N, M;
+    cin >> N >> M;
+    int result = 0;
+    vector<int> arr(N);
 
-	return s;
-}
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
 
-void result(int num) {
-	if (count < 3) {
-		for (int i = num; i < size; i++) {
-			selection[count] = arr[i];
-			count++;
-			result(i + 1);
-			count--;
-		}
-	}
-	else {
-		sum = val(selection);
-		if (MAX < sum && sum <= n) {
-			MAX = sum;
-		}
-	}	
-}
-int main() {
-	cin >> size >> n;
-
-	for (int i = 0; i < size; i++)
-		cin >> arr[i];
-
-	result(0);
-	cout << MAX;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            for (int k = j + 1; k < N; k++)
+            {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum <= M)
+                {
+                    result = max(result, sum);
+                }
+            }
+        }
+    }
+    cout << result;
 }
