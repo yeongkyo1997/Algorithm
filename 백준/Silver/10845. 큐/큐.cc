afdbee9ff89e070013e3d9eb1998ec292f5f254b
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-11-21 19:53:00 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,61 +1,74 @@
-#include<iostream>
-#include<queue>
-#include<string>
- 
+#include <bits/stdc++.h>
+
 using namespace std;
- 
-int main(void){
- 
+
+int main()
+{
+    ios::sync_with_stdio(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int N;
     cin >> N;
- 
     queue<int> q;
-    int num;
- 
-    for(int i=0; i<N; i++){
-        string str;
-        cin >> str;
- 
-        if(str == "push"){
-            int data;
-            cin >> data;
-            q.push(data);
- 
-        }else if(str == "pop"){
- 
-            if(q.size() != 0){
-                num = q.front();
-                q.pop();
-            }else{
-                num = -1;
-            }
-            cout << num << endl;
- 
-        }else if(str == "size"){
-            cout << q.size() << endl;
- 
-        }else if(str == "empty"){
-            if(q.size() == 0) num =1;
-            else num =0;
-            cout << num << endl;
- 
-        }else if(str == "front"){
-            if(q.size() == 0){
-                num = -1;
-            }else{
-                num = q.front();
-            }
-            cout << num << endl;
- 
-        }else if(str == "back"){
-            if(q.size() == 0){
-                num = -1;
-            }else{
-                num = q.back();
-            }
-            cout << num << endl;
+    while (N--)
+    {
+        string command;
+        cin >> command;
+
+        if (command == "push")
+        {
+            int num;
+            cin >> num;
+            q.push(num);
         }
- 
+        else if (command == "pop")
+        {
+            if (q.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if (command == "size")
+        {
+            cout << q.size() << '\n';
+        }
+        else if (command == "empty")
+        {
+            if (q.empty())
+            {
+                cout << 1 << '\n';
+            }
+            else
+            {
+                cout << 0 << '\n';
+            }
+        }
+        else if (command == "front")
+        {
+            if (q.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << q.front() << '\n';
+            }
+        }
+        else if (command == "back")
+        {
+            if (q.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << q.back() << '\n';
+            }
+        }
     }
-    return 0;
 }
