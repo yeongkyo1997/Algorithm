ad2dc4110e7786462158cfa3f73f697f71ded90b
yeongkyo1997 <yeongkyo1997@gmail.com>
2024-11-22 16:34:13 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,77 +1,64 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
-vector<int> aparts(25 * 25);
-int n;
-int apartNum = 0;
-vector<vector<bool>> visited(25, vector<bool>(25));
-vector<vector<int>> map(25, vector<int>(25));
+vector<int> dx = {0, 0, -1, 1};
+vector<int> dy = {-1, 1, 0, 0};
+vector<vector<int>> arr;
+int N;
 
-void bfs(int x, int y) {
-    queue<pair<int, int>> que;
-    que.push({x, y});
-    visited[x][y] = true;
-    aparts[apartNum]++;
+int dfs(int x, int y)
+{
+    if (arr[x][y] == 0)
+        return 0;
 
-    while (!que.empty()) {
-        int curX = que.front().first;
-        int curY = que.front().second;
-        que.pop();
+    arr[x][y] = 0;
 
-        for (int i = 0; i < 4; i++) {
-            int nx = curX + dx[i];
-            int ny = curY + dy[i];
+    int result = 1;
+    for (int d = 0; d < 4; d++)
+    {
+        int nx = x + dx[d];
+        int ny = y + dy[d];
 
-            if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
-                if (map[nx][ny] == 1 && !visited[nx][ny]) {
-                    que.push({nx, ny});
-                    visited[nx][ny] = true;
-                    aparts[apartNum]++;
-                }
-            }
+        if (0 <= nx && nx < N && 0 <= ny && ny < N)
+        {
+            result += dfs(nx, ny);
         }
     }
+
+    return result;
 }
 
-int main() {
-    cin >> n;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    arr.resize(N, vector<int>(N));
 
-    map = vector<vector<int>>(n, vector<int>(n));
-    visited = vector<vector<bool>>(n, vector<bool>(n));
-
-    // 전체 사각형 입력 받기
-    for (int i = 0; i < n; i++) {
-        string input;
-        cin >> input;
-        for (int j = 0; j < n; j++) {
-            map[i][j] = input[j] - '0';
+    for (int i = 0; i < N; i++)
+    {
+        string s;
+        cin >> s;
+        for (int j = 0; j < N; j++)
+        {
+            arr[i][j] = s[j] - '0';
+        }
+    }
+    vector<int> result;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (arr[i][j] == 1)
+                result.push_back(dfs(i, j));
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (map[i][j] == 1 && !visited[i][j]) {
-                apartNum++;
-                bfs(i, j);
-            }
-        }
-    }
+    sort(result.begin(), result.end());
 
-    sort(aparts.begin(), aparts.end());
-    cout << apartNum << endl;
-
-    for (int i = 0; i < aparts.size(); i++) {
-        if (aparts[i] != 0) {
-            cout << aparts[i] << endl;
-        }
-    }
-
-    return 0;
+    cout << result.size() << '\n';
+    for (auto r : result)
+        cout << r << '\n';
 }
