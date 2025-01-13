3c2804192f1e08690fd1d0c72add7bef8259dba7
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-13 12:52:35 +0900

    [Platinum III] Title: 소수 쌍, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Platinum/1017. 소수 쌍/소수 쌍.cc
+++ 백준/Platinum/1017. 소수 쌍/소수 쌍.cc
@@ -1,181 +1,151 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <cstring>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// 에라토스테네스의 체 배열 (소수 판별용)
-vector<bool> IS_NOT_PRIME;
+static const int MAX_SUM = 2000;
 
-vector<int> left_side;
-vector<int> right_side;
-bool hasNode[51][51];
-bool visited[51];
-int matched[51];
-int selected;
+bool isPrimeArr[MAX_SUM + 1];
 
-void eratosthenes()
+void sievePrime()
 {
-    IS_NOT_PRIME.resize(2000, false);
-    
-    IS_NOT_PRIME[0] = true;
-    IS_NOT_PRIME[1] = true;
-    
-    int maxPrime = (int)ceil(sqrt(2000));
-    
-    for (int i = 2; i < maxPrime; i++)
+    fill(isPrimeArr, isPrimeArr + MAX_SUM + 1, true);
+    isPrimeArr[0] = false;
+    isPrimeArr[1] = false;
+    for (int i = 2; i * i <= MAX_SUM; i++)
     {
-        if (!IS_NOT_PRIME[i])
+        if (isPrimeArr[i])
         {
-            for (int j = i + i; j < IS_NOT_PRIME.size(); j += i)
+            for (int j = i * i; j <= MAX_SUM; j += i)
             {
-                if (!IS_NOT_PRIME[j])
-                {
-                    IS_NOT_PRIME[j] = true;
-                }
+                isPrimeArr[j] = false;
             }
         }
     }
 }
 
-bool dfs(int num)
+bool dfs(int u, const vector<vector<int>> &adj,
+         vector<int> &matched, vector<bool> &visited)
 {
-    if (!visited[num])
+    for (int v : adj[u])
     {
-        visited[num] = true;
-        for (int i = 0; i < right_side.size(); i++)
+        if (visited[v])
+            continue;
+        visited[v] = true;
+
+        if (matched[v] == -1 || dfs(matched[v], adj, matched, visited))
         {
-            if (hasNode[num][i] && i != selected && !IS_NOT_PRIME[left_side[num] + right_side[i]])
-            {
-                if (matched[i] == -1 || dfs(matched[i]))
-                {
-                    matched[i] = num;
-                    return true;
-                }
-            }
+            matched[v] = u;
+            return true;
         }
     }
     return false;
 }
 
-int bipartite()
-{
-    int size = 1;
-    memset(matched, -1, sizeof(matched));
-    
-    for (int i = 1; i < left_side.size(); i++)
-    {
-        memset(visited, false, sizeof(visited));
-        if (dfs(i))
-        {
-            size++;
-        }
-    }
-    return size;
-}
-
 int main()
 {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    eratosthenes();
+    sievePrime();
 
     int N;
     cin >> N;
 
-    vector<int> numbers(N);
+    vector<int> arr(N);
     for (int i = 0; i < N; i++)
     {
-        cin >> numbers[i];
+        cin >> arr[i];
     }
 
-    if (numbers[0] % 2 != 0)
+    int firstNum = arr[0];
+
+    vector<int> others(arr.begin() + 1, arr.end());
+
+    vector<int> candidates;
+    for (int x : others)
     {
-        for (int value : numbers)
+        if (isPrimeArr[firstNum + x])
         {
-            if (value % 2 != 0)
-            {
-                left_side.push_back(value);
-            }
+            candidates.push_back(x);
+        }
+    }
+
+    vector<int> answer;
+
+    sort(candidates.begin(), candidates.end());
+
+    for (int c : candidates)
+    {
+
+        vector<int> remain;
+        remain.reserve(N - 2);
+        for (int x : others)
+        {
+            if (x == c)
+                continue;
+            remain.push_back(x);
+        }
+
+        vector<int> odd, even;
+        for (int x : remain)
+        {
+            if (x % 2)
+                odd.push_back(x);
             else
+                even.push_back(x);
+        }
+
+        if (odd.size() != even.size())
+        {
+            continue;
+        }
+
+        int sizeLeft = (int)odd.size();
+        int sizeRight = (int)even.size();
+
+        vector<vector<int>> adj(sizeLeft);
+
+        for (int i = 0; i < sizeLeft; i++)
+        {
+            for (int j = 0; j < sizeRight; j++)
             {
-                right_side.push_back(value);
+                if (isPrimeArr[odd[i] + even[j]])
+                {
+                    adj[i].push_back(j);
+                }
             }
         }
+
+        vector<int> matched(sizeRight, -1);
+        int matchCount = 0;
+        for (int i = 0; i < sizeLeft; i++)
+        {
+            vector<bool> visited(sizeRight, false);
+            if (dfs(i, adj, matched, visited))
+            {
+                matchCount++;
+            }
+        }
+
+        if (matchCount == sizeLeft)
+        {
+            answer.push_back(c);
+        }
+    }
+
+    if (answer.empty())
+    {
+        cout << -1 << "\n";
     }
     else
     {
-        for (int value : numbers)
+        for (int i = 0; i < (int)answer.size(); i++)
         {
-            if (value % 2 == 0)
-            {
-                left_side.push_back(value);
-            }
-            else
-            {
-                right_side.push_back(value);
-            }
+            cout << answer[i];
+            if (i + 1 < (int)answer.size())
+                cout << " ";
         }
-    }
-    
-    if (left_side.size() == right_side.size())
-    {
-        for (int i = 1; i < left_side.size(); i++)
-        {
-            for (int j = 0; j < right_side.size(); j++)
-            {
-                int ref = left_side[i] + right_side[j];
-
-                if (!IS_NOT_PRIME[ref])
-                {
-                    hasNode[i][j] = true;
-                }
-            }
-        }
-
-        vector<int> list;
-
-        for (int i = 0; i < N / 2; i++)
-        {
-            if (!IS_NOT_PRIME[left_side[0] + right_side[i]])
-            {
-                selected = i;
-
-                int size = bipartite();
-
-                if (size == N / 2)
-                {
-                    list.push_back(right_side[selected]);
-                }
-            }
-        }
-
-        if (list.size() == 0)
-        {
-            cout << "-1";
-        }
-        else
-        {
-            sort(list.begin(), list.end());
-
-            for (int i = 0; i < list.size(); i++)
-            {
-                if (i > 0)
-                {
-                    cout << " ";
-                }
-                cout << list[i];
-            }
-        }
-    }
-    else
-    {
-        cout << "-1";
+        cout << "\n";
     }
 
-    cout << '\n';
     return 0;
 }
