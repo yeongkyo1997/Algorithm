b457709a93835b1a98a22a367bc5f3a78356617f
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-14 09:31:08 +0900

    [Bronze V] Title: 所持金 (Money On Me), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33169. 所持金 （Money On Me）/所持金 （Money On Me）.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int A, B;
+    cin >> A >> B;
+    
+    long long total = 1000LL * A + 10000LL * B;
+    
+    cout << total;
+    
+    return 0;
+}
