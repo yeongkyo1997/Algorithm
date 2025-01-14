2f1a03e6e37783726309e513ce593e591707f05e
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-14 12:21:56 +0900

    [Silver I] Title: IOIOI, Time: 20 ms, Memory: 9000 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -1,29 +1,67 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main() {
-    int N, M, answer = 0, i = 0, count = 0;
-    std::string S;
+// Function to build the prefix function for KMP
+vector<int> buildPrefix(const string &pattern) {
+    int m = pattern.size();
+    vector<int> prefix(m, 0);
+    for(int i =1; i < m; i++) {
+        int j = prefix[i-1];
+        while(j >0 && pattern[i] != pattern[j]) {
+            j = prefix[j-1];
+        }
+        if(pattern[i] == pattern[j]) {
+            j++;
+        }
+        prefix[i] = j;
+    }
+    return prefix;
+}
 
-    std::cin >> N;
-    std::cin >> M;
-    std::cin >> S;
-
-    while (i < (M - 1)) {
-        if (S.substr(i, 3) == "IOI") {
-            i += 2;
-            count += 1;
-            if (count == N) {
-                answer += 1;
-                count -= 1;
-            }
-        } else {
-            i += 1;
-            count = 0;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    // Read inputs
+    int N;
+    cin >> N;
+    int M;
+    cin >> M;
+    string S;
+    cin >> S;
+    
+    // Generate pattern PN: 'I' followed by 'OI' N times
+    string PN = "I";
+    for(int i=0; i < N; i++) {
+        PN += "OI";
+    }
+    
+    // Edge case: if PN length > M, no matches
+    if(PN.size() > S.size()) {
+        cout << "0\n";
+        return 0;
+    }
+    
+    // Build prefix function for PN
+    vector<int> prefix = buildPrefix(PN);
+    
+    // KMP search
+    int count =0;
+    int j =0; // index for PN
+    for(int i=0; i < S.size(); i++) {
+        while(j >0 && S[i] != PN[j]) {
+            j = prefix[j-1];
+        }
+        if(S[i] == PN[j]) {
+            j++;
+        }
+        if(j == PN.size()) {
+            count++;
+            j = prefix[j-1];
         }
     }
-
-    std::cout << answer << std::endl;
-
+    
+    cout << count << "\n";
+    
     return 0;
 }
