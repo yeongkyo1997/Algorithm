2217312dc5d89a2954add0f6b6877d3e635dc1bc
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-14 09:35:26 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long long L;
+    cin >> L;
+    
+    long long t = (L + 4) / 5;
+    
+    cout << t;
+}
