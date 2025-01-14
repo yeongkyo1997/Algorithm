59a068f37941f1e623028d2108eff5f1788db673
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-14 17:32:02 +0900

    [Gold IV] Title: 단어 수학, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.cc
+++ 백준/Gold/1339. 단어 수학/단어 수학.cc
@@ -1,58 +1,55 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <math.h>
-#include <utility>
-#include <functional>
-#include <iomanip>
-#include <string.h>
-#include <string>
-#include <tuple>
-#include <string.h>
-#include <math.h>
-#include <map>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, arr[26], result;
-string a;
-vector<string> word;
-void func(int cur, int pos, int cur_sum, int sum, int visit) {
-	if (cur == word.size()) {
-		result = max(result, sum);
-		return;
-	}
-	if (pos == word[cur].size()) {
-		func(cur + 1, 0, 0, sum + cur_sum, visit);
-		return;
-	}
-
-	if (arr[word[cur][pos] - 'A'] == -1) {
-		for (int i = 0; i <= 9; i++) {
-			if (visit & (1 << i)) continue;
-			arr[word[cur][pos] - 'A'] = i;
-			func(cur, pos + 1, cur_sum * 10 + arr[word[cur][pos] - 'A'], sum, visit | (1 << i));
-			arr[word[cur][pos] - 'A'] = -1;
-		}
-	}
-	else {
-		func(cur, pos + 1, cur_sum * 10 + arr[word[cur][pos] - 'A'], sum, visit);
-	}
-
-}
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	cin >> n;
-	memset(arr, -1, sizeof(arr));
+    int N;
+    cin >> N;
+    
+    // 알파벳별 기여도를 저장할 배열(0으로 초기화)
+    // A=0, B=1, ... Z=25
+    long long alphaWeight[26] = {0};
 
-	while (n--) {
-		cin >> a;
-		word.push_back(a);
-	}
-	func(0, 0, 0, 0, 0);
-	cout << result;
+    vector<string> words(N);
+    for(int i = 0; i < N; i++){
+        cin >> words[i];
+    }
 
-	return 0;
+    // 각 단어에서, 알파벳의 자리수 기여도를 계산
+    for(const auto &word : words){
+        int len = word.size();
+        long long placeValue = 1;
+        
+        // 끝자리(일의 자리)부터 시작하여 왼쪽으로 이동
+        for(int i = len - 1; i >= 0; i--){
+            int index = word[i] - 'A';
+            alphaWeight[index] += placeValue;
+            placeValue *= 10; 
+        }
+    }
+
+    // alphaWeight가 큰 순으로 정렬하기 위해 벡터로 복사
+    vector<long long> weights;
+    for(int i = 0; i < 26; i++){
+        if(alphaWeight[i] > 0){
+            weights.push_back(alphaWeight[i]);
+        }
+    }
+
+    // 내림차순 정렬
+    sort(weights.begin(), weights.end(), greater<long long>());
+
+    // 가장 큰 가중치에 9, 그 다음 8, … 순으로 할당
+    long long answer = 0;
+    long long digit = 9;
+    for(auto w : weights){
+        answer += w * digit;
+        digit--;
+    }
+
+    cout << answer << "\n";
+    
+    return 0;
 }
