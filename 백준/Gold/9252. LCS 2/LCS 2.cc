91b5adb2cc97c75ec2a526d6e190d132075b3c53
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-14 12:32:49 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,50 +1,58 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-string a, b;
-vector<vector<int>> dp;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-void input() {
-    cin >> a >> b;
-}
+    string A, B;
+    cin >> A >> B;
 
-void pro() {
-    int alen = a.length();
-    int blen = b.length();
+    int N = A.size();
+    int M = B.size();
 
-    dp = vector<vector<int>>(alen + 1, vector<int>(blen + 1, 0));
+    // DP 배열 준비
+    vector<vector<int>> dp(N + 1, vector<int>(M + 1, 0));
 
-    for (int i = 1; i <= alen; i++) {
-        for (int j = 1; j <= blen; j++) {
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            if (a[i - 1] == b[j - 1]) {
-                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
+    // DP로 LCS 길이 계산
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            if (A[i - 1] == B[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
     }
 
-    string answer = "";
-    int i = alen;
-    int j = blen;
+    // LCS 길이
+    int lcs_length = dp[N][M];
+    cout << lcs_length << "\n";
 
-    while (i != 0 && j != 0) {
-        if (dp[i][j] == dp[i - 1][j]) {
+    // 역추적을 통해 실제 LCS 문자열 구성
+    string lcs = "";
+    int i = N, j = M;
+    while (i > 0 && j > 0) {
+        if (A[i - 1] == B[j - 1]) {
+            lcs += A[i - 1];  // 공통 문자를 추가
             i--;
-        } else if (dp[i][j] == dp[i][j - 1]) {
             j--;
         } else {
-            answer = a[i - 1] + answer;
-            i--;
-            j--;
+            if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
         }
     }
-    cout << dp[alen][blen] << endl;
-    cout << answer << endl;
-}
 
-int main() {
-    input();
-    pro();
+    // 뒤집으면 정방향 LCS가 됨
+    reverse(lcs.begin(), lcs.end());
+
+    // LCS 길이가 0이 아니라면 출력
+    if (lcs_length > 0) {
+        cout << lcs << "\n";
+    }
+
     return 0;
 }
