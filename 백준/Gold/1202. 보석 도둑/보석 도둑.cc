e1d59254a0c958c8b3b02eace40491879edf9ff9
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-14 12:30:40 +0900

    [Gold II] Title: 보석 도둑, Time: 152 ms, Memory: 8740 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,64 +1,59 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Jewelry {
-    int mass;
-    int value;
+typedef long long ll;
 
-    Jewelry() {}
-
-    Jewelry(int m, int v) {
-        mass = m;
-        value = v;
-    }
-
-    bool operator<(const Jewelry& other) const {
-        if (mass == other.mass)
-            return value < other.value;
-        return mass < other.mass;
-    }
-};
-
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     int N, K;
     cin >> N >> K;
-
-    vector<Jewelry> jewelries(N);
-    for (int i = 0; i < N; i++) {
-        int m, v;
-        cin >> m >> v;
-        jewelries[i] = Jewelry(m, v);
+    
+    // 보석 정보를 저장할 벡터: (무게, 가격)
+    vector<pair<int, int>> gems(N);
+    for(int i=0; i<N; i++){
+        cin >> gems[i].first >> gems[i].second;
     }
-
-    sort(jewelries.begin(), jewelries.end());
-
+    
+    // 보석을 무게 오름차순으로 정렬
+    sort(gems.begin(), gems.end(), [&](const pair<int,int> &a, const pair<int,int> &b) -> bool {
+        if(a.first == b.first){
+            return a.second > b.second; // 무게가 같으면 가격이 높은 순
+        }
+        return a.first < b.first;
+    });
+    
+    // 가방 용량을 저장할 벡터
     vector<int> bags(K);
-    for (int i = 0; i < K; i++) {
+    for(int i=0; i<K; i++){
         cin >> bags[i];
     }
+    
+    // 가방을 용량 오름차순으로 정렬
     sort(bags.begin(), bags.end());
-
+    
+    // 최대 힙을 사용하여 현재 가방까지 넣을 수 있는 보석의 가격을 관리
     priority_queue<int> pq;
-    long long ans = 0;
-    for (int i = 0, j = 0; i < K; i++) {
-        while (j < N && jewelries[j].mass <= bags[i]) {
-            pq.push(jewelries[j++].value);
+    
+    ll total = 0;
+    int gem_idx = 0;
+    
+    for(int i=0; i<K; i++){
+        int Ci = bags[i];
+        // 현재 가방에 넣을 수 있는 보석들을 힙에 추가
+        while(gem_idx < N && gems[gem_idx].first <= Ci){
+            pq.push(gems[gem_idx].second);
+            gem_idx++;
         }
-
-        if (!pq.empty()) {
-            ans += pq.top();
+        // 힙에서 가장 높은 가격의 보석을 선택
+        if(!pq.empty()){
+            total += pq.top();
             pq.pop();
         }
     }
-
-    cout << ans << "\n";
-
+    
+    cout << total;
+    
     return 0;
 }
