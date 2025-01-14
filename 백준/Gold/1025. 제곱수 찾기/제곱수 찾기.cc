5edcc8566379775ce1a946ba0015e16ad839f1a5
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-14 09:27:33 +0900

    [Gold V] Title: 제곱수 찾기, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.cc
+++ 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.cc
@@ -1,59 +1,62 @@
-#include <cmath>
-#include <iostream>
-#include <algorithm>
-
-#define endl "\n"
-
+#include <bits/stdc++.h>
 using namespace std;
 
-char map[10][10];
-int N, M;
-
-bool isSquare(int x) {
-    int t = (int)sqrt(x);
-
-    for (int i = t - 2; i <= t + 2; i++)
-        if (i * i == x)
-            return true;
-
-    return false;
+// 완전 제곱수 판별 함수
+bool isPerfectSquare(long long x) {
+    if (x < 0) return false;
+    long long r = (long long)floor(sqrt((long double)x));
+    return (r * r == x);
 }
 
-bool isOutOfBounds(int x, int y) {
-    return x < 0 || x >= N || y < 0 || y >= M;
-}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-int main(void) {
+    int N, M;
     cin >> N >> M;
 
-    for (int i = 0; i < N; i++)
-        cin >> map[i];
+    // 표 입력 받기 (각 행을 문자열로 저장)
+    vector<string> board(N);
+    for(int i = 0; i < N; i++){
+        cin >> board[i];
+    }
 
-    int ans = -1;
+    long long answer = -1; // 만들 수 있는 가장 큰 완전 제곱수, 초기값 -1
 
-    for (int x = 0; x < N; x++) {
-        for (int y = 0; y < M; y++) {
-            for (int dx = -N; dx < N; dx++) {
-                for (int dy = -M; dy < M; dy++) {
-                    int val = 0;
-
-                    if (dx == 0 && dy == 0)
+    // 모든 시작 위치 (r, c)
+    for(int r = 0; r < N; r++){
+        for(int c = 0; c < M; c++){
+            // 모든 (dr, dc) 공차에 대해
+            for(int dr = -N + 1; dr < N; dr++){
+                for(int dc = -M + 1; dc < M; dc++){
+                    // (dr, dc)가 (0, 0)이면 오직 현재 칸만 선택 가능
+                    if(dr == 0 && dc == 0){
+                        long long val = board[r][c] - '0';
+                        if(isPerfectSquare(val)) {
+                            answer = max(answer, val);
+                        }
                         continue;
+                    }
 
-                    int curX = x;
-                    int curY = y;
-                    while (!isOutOfBounds(curX, curY)) {
-                        val = 10 * val + (map[curX][curY] - '0');
+                    long long cur = 0; 
+                    int rr = r, cc = c;
+                    for(int step = 0; step < 9; step++){
+                        if(rr < 0 || rr >= N || cc < 0 || cc >= M) break;
+                        
+                        cur = cur * 10 + (board[rr][cc] - '0');
+                        
+                        if(isPerfectSquare(cur)) {
+                            answer = max(answer, cur);
+                        }
 
-                        if (isSquare(val))
-                            ans = max(ans, val);
-
-                        curX += dx;
-                        curY += dy;
+                        rr += dr;
+                        cc += dc;
                     }
                 }
             }
         }
     }
-    cout << ans;
+
+    cout << answer << "\n";
+    return 0;
 }
