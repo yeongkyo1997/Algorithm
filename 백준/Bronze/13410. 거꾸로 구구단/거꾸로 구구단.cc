bec097ed84ffbe36867af1f9ac01d8adfd0d9392
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-14 10:11:30 +0900

    [Bronze II] Title: 거꾸로 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/13410. 거꾸로 구구단/거꾸로 구구단.cc
+++ 백준/Bronze/13410. 거꾸로 구구단/거꾸로 구구단.cc
@@ -1,29 +1,33 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int MAX = 0;
-int n;
-int k;
-
-int reverse(int num) {
-	int re = 0;
-	while (1) {
-		re += num % 10;
-		num /= 10;
-		if (num == 0)
-			break;
-		re *= 10;
-	}
-	return re;
+int reverse_num(int num){
+    int reversed = 0;
+    while(num > 0){
+        reversed = reversed * 10 + (num % 10);
+        num /= 10;
+    }
+    return reversed;
 }
 
-int main() {
-	cin >> n >> k;
-
-	for (int i = 1; i <= k; i++) {
-		MAX = max(MAX, reverse(i * n));
-	}
-
-	cout << MAX;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N, K;
+    cin >> N >> K;
+    
+    int max_reversed = 0;
+    
+    for(int i = 1; i <= K; ++i){
+        int product = N * i;
+        int reversed = reverse_num(product);
+        if(reversed > max_reversed){
+            max_reversed = reversed;
+        }
+    }
+    
+    cout << max_reversed;
+    
+    return 0;
 }
