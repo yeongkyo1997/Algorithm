18fe311dbe5e9f9bed037eb1f557af4150652129
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-14 09:39:02 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string abb;
+    cin >> abb;
+    
+    if (abb == "NLCS")
+        cout << "North London Collegiate School";
+    else if (abb == "BHA")
+        cout << "Branksome Hall Asia";
+    else if (abb == "KIS")
+        cout << "Korea International School";
+    else if (abb == "SJA")
+        cout << "St. Johnsbury Academy";
+}
