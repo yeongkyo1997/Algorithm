a35ea37f5d38be8b88d82671adbbfb83f4724609
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-18 11:04:43 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 68 ms, Memory: 10420 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -1,40 +1,51 @@
-#include <bits/stdc++.h>
-
-class Pair {
-public:
-    int height;
-    int cnt;
-
-    Pair(int height, int cnt) : height(height), cnt(cnt) {}
-};
+#include <iostream>
+#include <stack>
+using namespace std;
 
 int main() {
-    int n;
-    std::cin >> n;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    std::stack<Pair> stack;
-    long long cnt = 0;
+    int N;
+    cin >> N;
 
-    for (int i = 0; i < n; ++i) {
-        int height;
-        std::cin >> height;
-        Pair pair(height, 1);
+    // (키, 해당 키의 연속 개수) 쌍을 저장할 스택
+    stack<pair<long long, long long>> st;
 
-        while (!stack.empty() && stack.top().height <= height) {
-            Pair pop = stack.top();
-            stack.pop();
-            cnt += pop.cnt;
-            if (pop.height == height)
-                pair.cnt += pop.cnt;
+    long long answer = 0;
+
+    while (N--) {
+        long long height;
+        cin >> height;
+
+        // 1. 스택 top의 키가 새 키보다 작으면 pop
+        long long count = 1;  // 새로 들어오는 height의 연속 개수(일단 자기 자신 1명)
+        while (!st.empty() && st.top().first < height) {
+            answer += st.top().second; // pop된 애들은 현재 사람과 모두 볼 수 있음
+            st.pop();
         }
 
-        if (!stack.empty())
-            cnt++;
+        // 2. 만약 스택 top이 새 키와 같다면 (중복 키)
+        if (!st.empty() && st.top().first == height) {
+            long long sameCount = st.top().second;
+            st.pop();
+            answer += sameCount; // 같은 키들끼리는 서로 다 볼 수 있음
 
-        stack.push(pair);
+            // 스택이 아직 비어있지 않다면, 현재 사람은 스택의 top과도 볼 수 있음
+            if (!st.empty()) answer++;
+
+            // 새로 push할 때, 연속 개수를 1 더해서 push
+            count = sameCount + 1;
+        }
+        else {
+            // 3. 스택이 비어있지 않고, top의 키가 더 크다면 서로 볼 수 있음
+            if (!st.empty()) answer++;
+        }
+
+        // (height, count)를 push
+        st.push({height, count});
     }
 
-    std::cout << cnt << std::endl;
-
+    cout << answer << "\n";
     return 0;
 }
