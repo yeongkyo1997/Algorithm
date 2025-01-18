bb31f1c852e672b370bf282365e9a0630856368e
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-18 10:43:31 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> dot) {
+    int x = dot[0];
+    int y = dot[1];
+    int answer = 0;
+    
+    if (x > 0 && y > 0) {
+        answer = 1;
+    }
+    else if (x < 0 && y > 0) {
+        answer = 2;
+    }
+    else if (x < 0 && y < 0) {
+        answer = 3;
+    }
+    else if (x > 0 && y < 0) {
+        answer = 4;
+    }
+    
+    return answer;
+}
