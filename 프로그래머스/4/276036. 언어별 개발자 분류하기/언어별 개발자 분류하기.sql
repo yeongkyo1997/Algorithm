704a5435f9b62ecf12658b47d2cfac1831c8686c
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-01-18 14:50:30 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,34 +1,38 @@
-WITH SkillCodes AS (
-    SELECT 
-        MAX(CASE WHEN NAME = 'JavaScript' THEN CODE END) AS JavaScript,
-        MAX(CASE WHEN NAME = 'React' THEN CODE END) AS React,
-        MAX(CASE WHEN NAME = 'Vue' THEN CODE END) AS Vue,
-        MAX(CASE WHEN NAME = 'Python' THEN CODE END) AS Python,
-        MAX(CASE WHEN NAME = 'C#' THEN CODE END) AS CSharp
-    FROM 
-        SKILLCODES
-),
-DeveloperGrades AS (
-    SELECT 
-        d.ID,
-        d.EMAIL,
-        CASE
-            WHEN (d.SKILL_CODE & sc.JavaScript > 0 OR d.SKILL_CODE & sc.React > 0 OR d.SKILL_CODE & sc.Vue > 0)
-                 AND d.SKILL_CODE & sc.Python > 0 THEN 'A'
-            WHEN d.SKILL_CODE & sc.CSharp > 0 THEN 'B'
-            WHEN (d.SKILL_CODE & sc.JavaScript > 0 OR d.SKILL_CODE & sc.React > 0 OR d.SKILL_CODE & sc.Vue > 0) THEN 'C'
-            ELSE NULL
-        END AS GRADE
-    FROM 
-        DEVELOPERS d
-    CROSS JOIN 
-        SkillCodes sc
-)
-SELECT 
-    GRADE, ID, EMAIL
-FROM 
-    DeveloperGrades
-WHERE 
-    GRADE IS NOT NULL
-ORDER BY 
-    GRADE, ID;
+-- 코드를 작성해주세요
+
+SELECT
+    CASE
+        WHEN (d.SKILL_CODE & fe.sum_code) > 0
+             AND (d.SKILL_CODE & py.code) > 0 THEN 'A'
+        WHEN (d.SKILL_CODE & cs.code) > 0 THEN 'B'
+        WHEN (d.SKILL_CODE & fe.sum_code) > 0 THEN 'C'
+    END AS GRADE,
+    d.ID,
+    d.EMAIL
+FROM
+    DEVELOPERS d
+    CROSS JOIN (
+        SELECT SUM(CODE) AS sum_code
+        FROM SKILLCODES
+        WHERE CATEGORY = 'Front End'
+    ) fe
+    CROSS JOIN (
+        SELECT CODE
+        FROM SKILLCODES
+        WHERE NAME = 'Python'
+    ) py
+    CROSS JOIN (
+        SELECT CODE
+        FROM SKILLCODES
+        WHERE NAME = 'C#'
+    ) cs
+WHERE
+    (
+        (d.SKILL_CODE & fe.sum_code) > 0
+        AND (d.SKILL_CODE & py.code) > 0
+    )
+    OR (d.SKILL_CODE & cs.code) > 0
+    OR (d.SKILL_CODE & fe.sum_code) > 0
+ORDER BY
+    GRADE ASC,
+    ID ASC;
