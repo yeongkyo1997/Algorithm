70143ae4d3c0bfa8e52e0b6a8a092130b5fb2b2a
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-02-03 17:49:54 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 116 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -1,35 +1,34 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-
-    static long N;
-    static long result = 1;
-
     public static void main(String[] args) throws Exception {
-        N = Long.parseLong(br.readLine());
 
-        for (long i = 2; i <= N / i; i++) {
-            if (N % i == 0) {
-                result *= i - 1;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-                while (N % i == 0) {
-                    result *= i;
-                    N /= i;
+        long n = Long.parseLong(br.readLine());
+        long result = n;
+        long temp = n;
+
+        for (long i = 2; i * i <= temp; i++) {
+            if (temp % i == 0) {
+
+                while (temp % i == 0) {
+                    temp /= i;
                 }
-                result /= i;
+
+                result -= result / i;
             }
         }
 
-        if (N != 1) result = result * (N - 1);
+        if (temp > 1) {
+            result -= result / temp;
+        }
 
         bw.write(String.valueOf(result));
+        bw.flush();
+
+        br.close();
         bw.close();
     }
 }
