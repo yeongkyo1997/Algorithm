1a9f79e9d65efc6bfbfda7e7fb54604d6bacde7f
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-02-10 23:19:26 +0900

    [Platinum V] Title: 쿨한 물건 구매, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.cc
+++ 백준/Platinum/1214. 쿨한 물건 구매/쿨한 물건 구매.cc
@@ -1,49 +1,48 @@
 #include <iostream>
-#include <unordered_set>
-
+#include <algorithm>
+#include <climits>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
+int main()
+{
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int d, p, q;
-    cin >> d >> p >> q;
+    long long D, P, Q;
+    cin >> D >> P >> Q;
 
-    int a = max(p, q);
-    int b = min(p, q);
+    if (P > Q)
+        swap(P, Q);
 
-    if (b == 1 || (b == 2 && d % 2 == 0) || d % a == 0 || d % b == 0) {
-        cout << d << endl;
+    if (P == 1)
+    {
+        cout << D << "\n";
         return 0;
     }
 
-    int limit = d / a + 1;
-    int answer = a - 1;
-    unordered_set<int> hs;
+    long long B_upper = (D + Q - 1) / Q;
+    if (B_upper > P - 1)
+        B_upper = P - 1;
 
-    for (int i = 0; i <= limit; i++) {
-        int remain = d - a * i;
-
-        if (remain > 0 && remain % b == 0) {
-            cout << d << endl;
-            return 0;
+    long long ans = LLONG_MAX;
+    for (long long b = 0; b <= B_upper; b++)
+    {
+        long long sumQ = b * Q;
+        long long candidate;
+        if (sumQ >= D)
+        {
+            candidate = sumQ;
         }
+        else
+        {
+            long long rem = D - sumQ;
 
-        if (remain < 0)
-            remain += b;
-
-        int tmp = b - remain % b;
-
-        if (hs.count(tmp) > 0)
-            break;
-
-        hs.insert(tmp);
-
-        if (answer > tmp)
-            answer = tmp;
+            long long a = (rem + P - 1) / P;
+            candidate = sumQ + a * P;
+        }
+        ans = min(ans, candidate);
     }
-    cout << d + answer << endl;
 
+    cout << ans << "\n";
     return 0;
 }
