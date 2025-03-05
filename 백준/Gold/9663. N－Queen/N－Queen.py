3b61adb8d856ceb44cc211a215870035dd02f855
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-03-05 14:58:40 +0900

    [Gold IV] Title: N-Queen, Time: 2204 ms, Memory: 127340 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,33 +1,27 @@
-import sys
+def solve_n_queen(n):
+    def dfs(row, col, diag, anti_diag):
+        nonlocal count
+        # 모든 행에 퀸을 배치한 경우
+        if row == n:
+            count += 1
+            return
+        
+        # 해당 행에서 퀸을 놓을 수 있는 위치들 (비트마스크)
+        avail = ((1 << n) - 1) & ~(col | diag | anti_diag)
+        while avail:
+            # avail에서 가장 오른쪽에 있는 1 추출
+            pos = avail & -avail
+            avail -= pos  # 해당 비트를 제거
+            
+            # 다음 행에서의 재귀 호출:
+            # col에 pos를 추가, 대각선 이동에 맞게 shift
+            dfs(row + 1, col | pos, (diag | pos) << 1, (anti_diag | pos) >> 1)
+    
+    count = 0
+    dfs(0, 0, 0, 0)
+    return count
 
-
-def input(): return sys.stdin.readline().rstrip()
-
-
-N = int(input())
-
-board = [0] * N
-
-
-def check(row):
-    for i in range(row):
-        if board[row] == board[i] or abs(row - i) == abs(board[i] - board[row]):
-            return False
-
-    return True
-
-
-def dfs(depth):
-    if N == depth:
-        return 1
-
-    result = 0
-    for i in range(N):
-        board[depth] = i
-        if check(depth):
-            result += dfs(depth + 1)
-
-    return result
-
-
-print(dfs(0))
+if __name__ == "__main__":
+    import sys
+    n = int(sys.stdin.readline().strip())
+    print(solve_n_queen(n))
