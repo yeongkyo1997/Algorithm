4be9024bd9cfbb1db5eaf209cd790c1e5da7a411
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-03-05 15:01:24 +0900

    [Gold V] Title: 용액, Time: 16 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -1,51 +1,53 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <cstdlib>  // llabs 함수 사용을 위해
 using namespace std;
 
-int N;
-vector<int> nums;
-
-int abs(int num) {
-    return num >= 0 ? num : -num;
-}
-
-pair<int, int> twoPointerSearch(int target) {
-    int lp = 0, rp = N - 1;
-    int lr = 0, rr = N - 1, tmp = abs(nums[lr] + nums[rr]);
-
-    while (lp < rp) {
-        int val = nums[lp] + nums[rp];
-        int abs_val = abs(val);
-        if (tmp > abs_val) {
-            tmp = abs_val;
-            lr = lp;
-            rr = rp;
-        }
-
-        if (val < target) {
-            lp++;
-        } else if (val > target) {
-            rp--;
-        } else {
-            return make_pair(nums[lp], nums[rp]);
-        }
-    }
-
-    return make_pair(nums[lr], nums[rr]);
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
     cin >> N;
-    nums.resize(N);
-    for (int i = 0; i < N; i++) {
-        cin >> nums[i];
+    vector<long long> solutions(N);
+    
+    // 용액의 특성값 입력 (오름차순으로 주어짐)
+    for(int i = 0; i < N; i++){
+        cin >> solutions[i];
     }
-    sort(nums.begin(), nums.end());
-    pair<int, int> result = twoPointerSearch(0);
-    cout << result.first << " " << result.second;
-
+    
+    // 투 포인터 설정
+    int left = 0, right = N - 1;
+    long long bestSum = 2000000000LL * 2LL;  // 충분히 큰 값으로 초기화
+    long long answerLeft = solutions[left], answerRight = solutions[right];
+    
+    // left와 right가 만날 때까지 반복
+    while(left < right){
+        long long currentSum = solutions[left] + solutions[right];
+        
+        // 지금까지 찾은 합의 절대값보다 더 작은 경우 갱신
+        if(llabs(currentSum) < bestSum){
+            bestSum = llabs(currentSum);
+            answerLeft = solutions[left];
+            answerRight = solutions[right];
+        }
+        
+        // 합이 0이면 바로 종료 (더 좋은 해는 없음)
+        if(currentSum == 0){
+            break;
+        }
+        
+        // 합이 음수이면 left 포인터를 오른쪽으로 이동하여 합을 증가시킴
+        if(currentSum < 0){
+            left++;
+        }
+        // 합이 양수이면 right 포인터를 왼쪽으로 이동하여 합을 감소시킴
+        else {
+            right--;
+        }
+    }
+    
+    // 출력은 오름차순이어야 하므로, 이미 solutions 배열은 오름차순 정렬되어 있음.
+    cout << answerLeft << " " << answerRight << "\n";
     return 0;
 }
