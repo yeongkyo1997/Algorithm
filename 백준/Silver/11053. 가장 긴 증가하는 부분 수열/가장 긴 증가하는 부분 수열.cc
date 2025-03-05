173ae9933cb958e61b04c387a5b7f8e37fefcd41
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-03-05 14:49:59 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,35 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 int main() {
-    int n;
-    scanf("%d", &n);
-
-    int *arr = (int*) malloc(sizeof(int) * n);
-    int *dp = (int*) malloc(sizeof(int) * n);
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> arr[i];
     }
-
-    int max = 0;
-
-    for (int i = 0; i < n; i++) {
-        dp[i] = 1;
-        for (int j = 0; j < i; j++) {
-            if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
-                dp[i] = dp[j] + 1;
+    
+    vector<int> dp(N, 1);
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (arr[i] > arr[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
             }
         }
-        if (dp[i] > max) {
-            max = dp[i];
-        }
     }
-
-    printf("%d\n", max);
-
-    free(arr);
-    free(dp);
-
+    
+    cout << *max_element(dp.begin(), dp.end());
     return 0;
 }
