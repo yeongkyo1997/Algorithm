beb823c80887804ea3eb16ed78c22ee17a357f1f
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-03-06 11:11:24 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,19 +1,31 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
+void primeFactorization(int n) {
+    // N이 1인 경우 아무것도 출력하지 않음
+    if (n == 1) return;
+    
+    // 2부터 시작하여 n의 제곱근까지 확인
+    for (int i = 2; i <= sqrt(n); i++) {
+        // i로 나누어 떨어지는 동안 계속 나누기
+        while (n % i == 0) {
+            cout << i << endl;
+            n /= i;
+        }
+    }
+    
+    // 마지막 남은 수가 1보다 크면 그 수는 소수
+    if (n > 1) {
+        cout << n << endl;
+    }
+}
+
 int main() {
-	int n;
-
-	cin >> n;
-
-	for (int i = 2; i * i <= n; i++) {
-		while (n % i == 0) {
-			cout << i << endl;
-			n /= i;
-		}
-	}
-
-	if (n != 1) 
-		cout << n;
+    int n;
+    cin >> n;
+    
+    primeFactorization(n);
+    
+    return 0;
 }
