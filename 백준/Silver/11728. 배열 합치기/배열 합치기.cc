7791848954e300312dfcc00d657d675f9219c9cb
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-03-11 11:26:15 +0900

    [Silver V] Title: 배열 합치기, Time: 432 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -1,24 +1,36 @@
-#include <stdio.h>
-int N, M;
-int A[1000001];
-int B[1000001];
-int idx1, idx2;
-int main(void) {
-	scanf("%d %d", &N, &M);
-	for (int i = 0; i < N; i++)
-		scanf("%d", &A[i]);
-	for (int i = 0; i < M; i++)
-		scanf("%d", &B[i]);
-	while (idx1 + idx2 < N + M) {
-		if (idx1 == N)
-			printf("%d ", B[idx2++]);
-		else if (idx2 == M)
-			printf("%d ", A[idx1++]);
-		else {
-			if (A[idx1] > B[idx2])
-				printf("%d ", B[idx2++]);
-			else
-				printf("%d ", A[idx1++]);
-		}
-	}
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N, M;
+    cin >> N >> M;
+    
+    vector<int> A(N), B(M);
+    for(int i=0; i<N; ++i) cin >> A[i];
+    for(int i=0; i<M; ++i) cin >> B[i];
+    
+    vector<int> merged;
+    merged.reserve(N+M);
+    
+    int i=0, j=0;
+    while(i<N && j<M) {
+        if(A[i] < B[j]) merged.push_back(A[i++]);
+        else merged.push_back(B[j++]);
+    }
+    while(i<N) merged.push_back(A[i++]);
+    while(j<M) merged.push_back(B[j++]);
+    
+    for(int k=0; k<merged.size(); ++k) {
+        cout << merged[k] << ' ';
+    }
+    
+    return 0;
 }
