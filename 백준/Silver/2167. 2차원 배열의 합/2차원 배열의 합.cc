ad7137e26cdd992f73cd58e32fb1cc9dd903a890
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-03-11 10:49:42 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 12 ms, Memory: 2816 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
@@ -1,28 +1,42 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
 #include <iostream>
-
-int dp[301][301];
-
-int Dp(int i, int j, int x, int y) {
-	return dp[x][y] - dp[x][j - 1] - dp[i - 1][y] + dp[i - 1][j - 1];
-}
+#include <vector>
+using namespace std;
 
 int main() {
-	int N, M, K;
-
-	std::cin >> N >> M;
-
-	for (int i = 1; i <= N; i++)
-		for (int j = 1; j <= M; j++) {
-			std::cin >> dp[i][j];
-			dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + dp[i][j];
-		}
-
-	std::cin >> K;
-
-	for (int l = 0; l < K; l++) {
-		int i, j, x, y;
-
-		std::cin >> i >> j >> x >> y;
-		std::cout << Dp(i, j, x, y) << std::endl;
-	}
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int N, M;
+    cin >> N >> M;
+    
+    vector<vector<int>> matrix(N+1, vector<int>(M+1, 0));
+    vector<vector<int>> prefix(N+1, vector<int>(M+1, 0));
+    
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= M; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+    
+    // Compute 2D prefix sums
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= M; ++j) {
+            prefix[i][j] = matrix[i][j] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
+        }
+    }
+    
+    int K;
+    cin >> K;
+    
+    while (K--) {
+        int i, j, x, y;
+        cin >> i >> j >> x >> y;
+        int sum = prefix[x][y] - prefix[i-1][y] - prefix[x][j-1] + prefix[i-1][j-1];
+        cout << sum << '\n';
+    }
+    
+    return 0;
 }
