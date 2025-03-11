0891ad60fd024dbca9cb50b2cab1067a716a9020
yeongkyo1997 <yeongkyo1997@gmail.com>
2025-03-11 12:22:26 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.cc
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.cc
@@ -1,14 +1,34 @@
-#include<cstdio>
-int x, a, b;
-int main() {
-    for (int i = 0; i < 2; i++) {
-        scanf("%d", &x);
-        for (int j = 1e6; j; j /= 10) {
-            int t = x / j % 10;
-            a += t == 6 ? 5 * j : t*j;
-            b += t == 5 ? 6 * j : t*j;
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("O3")
+#pragma GCC target("avx2")
+
+pair<int, int> get_min_max(const string& s) {
+    string min_s, max_s;
+    for (char c : s) {
+        if (c == '5' || c == '6') {
+            min_s += '5';
+            max_s += '6';
+        } else {
+            min_s += c;
+            max_s += c;
         }
     }
-    printf("%d %d", a, b);
+    return {stoi(min_s), stoi(max_s)};
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int A, B;
+    cin >> A >> B;
+    
+    auto a = get_min_max(to_string(A));
+    auto b = get_min_max(to_string(B));
+    
+    cout << a.first + b.first << ' ' << a.second + b.second << '\n';
+    
     return 0;
 }
