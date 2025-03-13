d20a60f3e46fe2e029dccb6233535741323dade4
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-13 15:08:09 +0900

    [Bronze V] Title: The World Responds, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.cc
@@ -0,0 +1,12 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << "The world says hello!\n";
+    return 0;
+}
