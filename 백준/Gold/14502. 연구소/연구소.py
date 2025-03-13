28f2609d1f38ee24f087ab3023db354816fbc930
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-13 16:12:35 +0900

    [Gold IV] Title: 연구소, Time: 528 ms, Memory: 118628 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,59 +1,58 @@
-import collections
+import sys
+from collections import deque
 import copy
-import math
 
-dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+input = lambda: sys.stdin.readline().rstrip()
+
+d = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+
+
+def dfs(path, start, depth):
+    global result
+    if depth == 3:
+        q = deque(virus)
+        tmp = copy.deepcopy(board)
+        for px, py in path:
+            tmp[px][py] = 1
+
+        while q:
+            x, y = q.popleft()
+
+            for dx, dy in d:
+                nx, ny = x + dx, y + dy
+
+                if 0 <= nx < N and 0 <= ny < M and tmp[nx][ny] == 0:
+                    tmp[nx][ny] = 2
+                    q.append((nx, ny))
+
+        cnt = 0
+        for i in range(N):
+            for j in range(M):
+                if tmp[i][j] == 0:
+                    cnt += 1
+
+        result = max(result, cnt)
+        return
+
+    for i in range(start, len(zero)):
+        path.append(zero[i])
+        dfs(path, i + 1, depth + 1)
+        path.pop()
+
+
 N, M = map(int, input().split())
-
 board = [list(map(int, input().split())) for _ in range(N)]
 
 virus = []
-zero_cnt = -3
+zero = []
+
 for i in range(N):
     for j in range(M):
         if board[i][j] == 2:
             virus.append((i, j))
-        if board[i][j] == 0:
-            zero_cnt += 1
+        elif board[i][j] == 0:
+            zero.append((i, j))
 
-
-def bfs(board, zero_cnt):
-    global result
-    q = collections.deque(virus)
-    while q:
-        x, y = q.popleft()
-        for dx, dy in dir:
-            nx, ny = x + dx, y + dy
-
-            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
-                board[nx][ny] = 2
-                q.append((nx, ny))
-                zero_cnt -= 1
-    result = max(result, zero_cnt)
-
-
-def dfs(path, depth, sx, sy):
-    if depth == 3:
-        tmp = copy.deepcopy(board)
-        for x, y in path:
-            tmp[x][y] = 1
-        bfs(tmp, zero_cnt)
-        return
-
-    for i in range(sx, N):
-        for j in range(sy, M):
-            if board[i][j] != 0:
-                continue
-            path.append((i, j))
-            if j == M - 1:
-                dfs(path, depth + 1, i + 1, 0)
-            else:
-                dfs(path, depth + 1, i, j + 1)
-
-            path.pop()
-        sy = 0
-
-
-result = -math.inf
-dfs([], 0, 0, 0)
+result = 0
+dfs([], 0, 0)
 print(result)
