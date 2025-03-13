563d85eb331132ac9029d909134f4eae9d128ed4
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-13 13:28:47 +0900

    [Bronze V] Title: A + B, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/32260. A ＋ B/A ＋ B.cc
@@ -0,0 +1,12 @@
+#include "aplusb.h"
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <iostream>
+using namespace std;
+
+int sum(int A, int B) {
+    return A + B;
+}
