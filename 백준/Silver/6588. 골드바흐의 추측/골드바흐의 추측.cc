2012b8f02f9b3c5b612d6eed19a65e275ebae26e
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-13 13:18:46 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 56 ms, Memory: 10856 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -1,29 +1,49 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-const int MAX = 1000000;
-bool prime[MAX + 1];
+const int MAX = 1000001;
+
+vector<bool> sieve(MAX, true);
+vector<int> primes;
+vector<pair<int, int>> ans(MAX);
+
+void precompute() {
+    sieve[0] = sieve[1] = false;
+    for (int i = 2; i < MAX; i++) {
+        if (sieve[i]) {
+            primes.push_back(i);
+            for (long long j = (long long)i * i; j < MAX; j += i)
+                sieve[j] = false;
+        }
+    }
+
+    for (int n = 6; n < MAX; n += 2) {
+        bool found = false;
+        for (int i = 1; i < primes.size(); i++) {
+            int a = primes[i];
+            if (a > n / 2) break;
+            if (sieve[n - a]) {
+                ans[n] = {a, n - a};
+                found = true;
+                break;
+            }
+        }
+        if (!found) ans[n] = {-1, -1};
+    }
+}
 
 int main() {
-    for (int i = 2; i * i <= MAX; i++) {
-        if (!prime[i]) {
-            for (int j = (i * i) % MAX; j <= MAX; j += i) {
-                prime[j] = true;
-            }
-        }
-    }
-    while (true) {
-        int n;
-        scanf("%d", &n);
-        if (n == 0) break;
-        for (int i = 2; i <= MAX; i++) {
-            if (!prime[i]) {
-                int j = n - i;
-                if (!prime[j]) {
-                    printf("%d = %d + %d\n", n, i, j);
-                    break;
-                }
-            }
-        }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    precompute();
+
+    int n;
+    while (cin >> n && n != 0) {
+        cout << n << " = " << ans[n].first << " + " << ans[n].second << '\n';
     }
     return 0;
+    
 }
