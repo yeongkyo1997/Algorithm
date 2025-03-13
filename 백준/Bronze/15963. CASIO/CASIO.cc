5f1a521b0cf7708b21b3479f410690a11a536525
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-13 15:05:58 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15963. CASIO/CASIO.cc
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -1,15 +1,12 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    long long int N, M;
-    cin >> N >> M;
-
-    cout << ((N == M) ? 1 : 0);
+    cin.tie(nullptr);
+    
+    string n, m;
+    cin >> n >> m;
+    cout << (n == m) << '\n';
 }
