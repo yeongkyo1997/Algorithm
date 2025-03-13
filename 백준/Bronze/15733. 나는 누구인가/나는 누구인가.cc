5ce3871d66c44be6741d6d1c6b7bac86591f96d3
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-13 13:22:30 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,12 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << "I'm Sexy\n";
+    return 0;
+}
