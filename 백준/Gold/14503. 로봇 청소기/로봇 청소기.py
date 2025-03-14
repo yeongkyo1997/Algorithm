7b152ec07d3593c3d6504591bc1584948a5fa058
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-14 10:39:54 +0900

    [Gold V] Title: 로봇 청소기, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,37 +1,51 @@
-dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+import sys
 
-N, M = map(int, input().split())
+input = lambda: sys.stdin.readline().rstrip()
 
-x, y, d = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
+dxy = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 
 def move(x, y, d):
-    result = 0
-    while True:
-        if board[x][y] == 0:
-            result += 1
-            board[x][y] = -1
+    ret = 0
+    clean = [[0] * M for _ in range(N)]
 
+    while True:
+        # 현재 청소가 되지 않은 경우
+        if clean[x][y] == 0:
+            ret += 1
+            clean[x][y] = 1
+
+        # 청소가 되지 않은 빈칸 찾기
         for i in range(4):
             d = (d - 1) % 4
-            dx, dy = dir[d]
+            dx, dy = dxy[d]
             nx, ny = x + dx, y + dy
 
-            if 0 <= nx < N and 0 <= ny < M:
-                if board[nx][ny] == 0:
-                    x, y = nx, ny
-                    break
+            if (
+                0 <= nx < N
+                and 0 <= ny < M
+                and board[nx][ny] == 0
+                and clean[nx][ny] == 0
+            ):
+                x, y = nx, ny
+                break
+        # 빈칸이 없다면
         else:
-            dx, dy = dir[d]
-            x -= dx
-            y -= dy
-            if x < 0 or x >= N or y < 0 or y >= M:
+            dx, dy = dxy[d]
+            nx, ny = x - dx, y - dy
+
+            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
+                x, y = nx, ny
+            else:
                 break
-            if board[x][y] == 1:
-                break
 
-    return result
+    return ret
 
 
-print(move(x, y, d))
+N, M = map(int, input().split())
+
+r, c, d = map(int, input().split())
+
+board = [list(map(int, input().split())) for _ in range(N)]
+result = move(r, c, d)
+print(result)
