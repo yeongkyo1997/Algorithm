43d2b554d7b017e57ef3e445bb5354210d650cd0
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-15 09:48:15 +0900

    [Gold V] Title: 트리와 쿼리, Time: 56 ms, Memory: 9640 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -1,58 +1,57 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <tuple>
-
+#include <bits/stdc++.h>
 using namespace std;
 
+const int MAX = 100001;
+vector<int> adj[MAX];
+int subtree_size[MAX];
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
     int N, R, Q;
     cin >> N >> R >> Q;
     
-    vector<vector<int>> adj(N + 1);
-    for (int i = 0; i < N - 1; ++i) {
-        int U, V;
-        cin >> U >> V;
-        adj[U].push_back(V);
-        adj[V].push_back(U);
+    for(int i = 0; i < N-1; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    
-    vector<int> subtree_size(N + 1, 0);
+
     stack<tuple<int, int, bool>> st;
     st.push(make_tuple(R, -1, false));
     
-    while (!st.empty()) {
-        auto current = st.top();
+    while(!st.empty()) {
+        auto tpl = st.top();
         st.pop();
-        int u = get<0>(current);
-        int parent = get<1>(current);
-        bool visited = get<2>(current);
         
-        if (!visited) {
+        int u = get<0>(tpl);
+        int parent = get<1>(tpl);
+        bool visited = get<2>(tpl);
+        
+        if(!visited) {
             st.push(make_tuple(u, parent, true));
-            for (int v : adj[u]) {
-                if (v != parent) {
+            for(int v : adj[u]) {
+                if(v != parent) {
                     st.push(make_tuple(v, u, false));
                 }
             }
-        } else {
+        }
+        else {
             subtree_size[u] = 1;
-            for (int v : adj[u]) {
-                if (v != parent) {
+            for(int v : adj[u]) {
+                if(v != parent) {
                     subtree_size[u] += subtree_size[v];
                 }
             }
         }
     }
-    
-    for (int i = 0; i < Q; ++i) {
+
+    while(Q--) {
         int U;
         cin >> U;
         cout << subtree_size[U] << '\n';
     }
-    
-    return 0;
 }
