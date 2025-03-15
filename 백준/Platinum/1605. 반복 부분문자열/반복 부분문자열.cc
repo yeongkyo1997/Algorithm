47d65e060ad3cf4362683e8ffe6986748bdfbc29
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-15 11:26:56 +0900

    [Platinum III] Title: 반복 부분문자열, Time: 600 ms, Memory: 20412 KB -BaekjoonHub

--- 백준/Platinum/1605. 반복 부분문자열/반복 부분문자열.cc
+++ 백준/Platinum/1605. 반복 부분문자열/반복 부분문자열.cc
@@ -1,97 +1,84 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
+#include <unordered_set>
+
 using namespace std;
 
-struct State
-{
-    int len, link;
-    int next[26];
-    int occ;
-    State() : len(0), link(-1), occ(0)
-    {
-        for (int i = 0; i < 26; i++)
-            next[i] = -1;
+const int base1 = 911, mod1 = 1e9 + 7;
+const int base2 = 3571, mod2 = 1e9 + 9;
+
+struct HashPair {
+    long long h1, h2;
+    HashPair(long long a, long long b) : h1(a), h2(b) {}
+    bool operator==(const HashPair& other) const {
+        return h1 == other.h1 && h2 == other.h2;
     }
 };
 
-int main()
-{
+namespace std {
+    template<> struct hash<HashPair> {
+        size_t operator()(const HashPair& p) const {
+            return (p.h1 * 1234567891 + p.h2) % (1LL << 32);
+        }
+    };
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    
     int L;
-    cin >> L;
     string s;
-    cin >> s;
-    int n = s.size();
-
-    vector<State> st;
-    st.reserve(2 * n);
-    st.push_back(State());
-    int last = 0;
-
-    for (char c : s)
-    {
-        int cur = st.size();
-        st.push_back(State());
-        st[cur].len = st[last].len + 1;
-        st[cur].occ = 1;
-        int letter = c - 'a';
-        int p = last;
-        for (; p != -1 && st[p].next[letter] == -1; p = st[p].link)
-            st[p].next[letter] = cur;
-        if (p == -1)
-        {
-            st[cur].link = 0;
-        }
-        else
-        {
-            int q = st[p].next[letter];
-            if (st[p].len + 1 == st[q].len)
-            {
-                st[cur].link = q;
+    cin >> L >> s;
+    
+    if (L < 2) {
+        cout << 0 << '\n';
+        return 0;
+    }
+    
+    vector<long long> prefix1(L + 1, 0), prefix2(L + 1, 0);
+    vector<long long> base_pows1(L + 1, 1), base_pows2(L + 1, 1);
+    
+    for (int i = 1; i <= L; ++i) {
+        base_pows1[i] = (base_pows1[i-1] * base1) % mod1;
+        base_pows2[i] = (base_pows2[i-1] * base2) % mod2;
+    }
+    
+    for (int i = 0; i < L; ++i) {
+        prefix1[i+1] = (prefix1[i] * base1 + (s[i] - 'a' + 1)) % mod1;
+        prefix2[i+1] = (prefix2[i] * base2 + (s[i] - 'a' + 1)) % mod2;
+    }
+    
+    int answer = 0;
+    int low = 1, high = L-1;
+    
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        bool found = false;
+        unordered_set<HashPair> seen;
+        
+        for (int i = 0; i <= L - mid; ++i) {
+            long long h1 = (prefix1[i + mid] - (prefix1[i] * base_pows1[mid]) % mod1 + mod1) % mod1;
+            long long h2 = (prefix2[i + mid] - (prefix2[i] * base_pows2[mid]) % mod2 + mod2) % mod2;
+            HashPair current(h1, h2);
+            
+            if (seen.count(current)) {
+                found = true;
+                break;
             }
-            else
-            {
-                int clone = st.size();
-                st.push_back(State());
-                st[clone].len = st[p].len + 1;
-                st[clone].link = st[q].link;
-
-                for (int j = 0; j < 26; j++)
-                {
-                    st[clone].next[j] = st[q].next[j];
-                }
-                st[clone].occ = 0;
-                for (; p != -1 && st[p].next[letter] == q; p = st[p].link)
-                    st[p].next[letter] = clone;
-                st[q].link = st[cur].link = clone;
-            }
+            seen.insert(current);
         }
-        last = cur;
+        
+        if (found) {
+            answer = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
     }
-
-    vector<int> order(st.size());
-    for (int i = 0; i < st.size(); i++)
-        order[i] = i;
-    sort(order.begin(), order.end(), [&st](int a, int b)
-         { return st[a].len > st[b].len; });
-
-    for (int idx : order)
-    {
-        if (st[idx].link != -1)
-            st[st[idx].link].occ += st[idx].occ;
-    }
-
-    int ans = 0;
-    for (int i = 0; i < st.size(); i++)
-    {
-        if (st[i].occ >= 2 && st[i].len > ans)
-            ans = st[i].len;
-    }
-
-    cout << ans << "\n";
+    
+    cout << answer << '\n';
+    
     return 0;
 }
