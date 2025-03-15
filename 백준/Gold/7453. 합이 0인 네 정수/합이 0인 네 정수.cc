e29ca489504958bd540b819cef7799e9b5b9ef52
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-15 10:20:26 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 6592 ms, Memory: 64664 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -1,82 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
+int main() {
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
-
+    
     int n;
     cin >> n;
-
+    
     vector<int> A(n), B(n), C(n), D(n);
-
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; ++i)
         cin >> A[i] >> B[i] >> C[i] >> D[i];
-    }
-
-    vector<int> AB;
-    AB.reserve((long long)n * n);
-
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            AB.push_back(A[i] + B[j]);
+    
+    vector<int> AB_sum;
+    AB_sum.reserve(n * n);
+    for (int a : A)
+        for (int b : B)
+            AB_sum.push_back(a + b);
+    
+    sort(AB_sum.begin(), AB_sum.end());
+    
+    long long cnt = 0;
+    for (int c : C) {
+        for (int d : D) {
+            int target = -(c + d);
+            auto range = equal_range(AB_sum.begin(), AB_sum.end(), target);
+            cnt += range.second - range.first;
         }
     }
-
-    vector<int> CD;
-    CD.reserve((long long)n * n);
-
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            CD.push_back(C[i] + D[j]);
-        }
-    }
-
-    sort(AB.begin(), AB.end());
-    sort(CD.begin(), CD.end());
-
-    long long result = 0;
-    int i = 0;
-    int j = CD.size() - 1;
-
-    while (i < AB.size() && j >= 0)
-    {
-        long long cur_sum = (long long)AB[i] + (long long)CD[j];
-
-        if (cur_sum == 0)
-        {
-            long long cnt1 = 1;
-            int cur_a = AB[i];
-            i++;
-            while (i < AB.size() && AB[i] == cur_a)
-            {
-                cnt1++;
-                i++;
-            }
-
-            long long cnt2 = 1;
-            int cur_b = CD[j];
-            j--;
-            while (j >= 0 && CD[j] == cur_b)
-            {
-                cnt2++;
-                j--;
-            }
-            result += cnt1 * cnt2;
-        }
-        else if (cur_sum > 0)
-            j--;
-        else
-            i++;
-    }
-
-    cout << result;
+    
+    cout << cnt;
     return 0;
 }
