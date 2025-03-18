8af994cb9705c66d1c2670907fe54796de183e1c
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-19 07:42:00 +0900

    [Silver IV] Title: 설탕 배달, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,21 +1,21 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
 N = int(input())
 
 result = 0
-
 if N % 5 == 0:
-    print(N // 5)
-
+    result = N // 5
 else:
     while N > 0:
         N -= 3
         result += 1
+
         if N % 5 == 0:
             result += N // 5
-            print(result)
             break
-        elif N == 1 or N == 2:
-            print(-1)
-            break
-        elif N == 0:
-            print(result)
-            break
+        if N < 3:
+            result = -1
+
+print(result)
