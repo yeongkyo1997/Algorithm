4f3476b48ce8e74ce5dc7f93aa777cebb3cd2c88
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-19 08:34:40 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 238072 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,21 +1,36 @@
-N = int(input())
+import sys, collections
 
-for _ in range(N):
-    stack = []
+sys.setrecursionlimit(100000)
 
-    for i in input():
-        if i == '(':
-            stack.append('(')
-        else:
-            if not stack:
-                print('NO')
-                break
+
+input = lambda: sys.stdin.readline().rstrip()
+
+T = int(input())
+result = []
+
+for _ in range(T):
+    data = list(input())
+    cnt = 0
+    for d in data:
+        if cnt == 0:
+            if d == "(":
+                cnt += 1
             else:
-                if stack.pop() != '(':
-                    print('NO')
-                    break
-    else:
-        if stack:
-            print('NO')
+                result.append("NO")
+                break
         else:
-            print('YES')
+            if d == "(":
+                cnt += 1
+            else:
+                if cnt == 0:
+                    result.append("NO")
+                    break
+                else:
+                    cnt -= 1
+    else:
+        if cnt == 0:
+            result.append("YES")
+        else:
+            result.append("NO")
+
+print("\n".join(result))
