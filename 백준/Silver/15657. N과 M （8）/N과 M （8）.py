7f3e0e295180e3c2707df8b9d781b8fd204f3470
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-18 14:45:21 +0900

    [Silver III] Title: N과 M (8), Time: 112 ms, Memory: 110920 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.py
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -1,10 +1,10 @@
-import itertools
-import sys
+import sys, itertools
 
-sys.setrecursionlimit(10 ** 6)
+
 input = lambda: sys.stdin.readline().rstrip()
 
 N, M = map(int, input().split())
-
-for i in itertools.combinations_with_replacement(sorted(map(int, input().split())), M):
-    print(*i)
+arr = list(map(int, input().split()))
+arr.sort()
+for c in itertools.combinations_with_replacement(arr, M):
+    print(*c)
