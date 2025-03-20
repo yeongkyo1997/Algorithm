e84e73f56b3dc6577a2111338b28429e49fc136d
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-20 15:34:22 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 110712 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,20 +1,31 @@
+import sys
+import collections, functools
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+@functools.lru_cache(maxsize=32)
+def solve(total):
+    if total > N:
+        return 0
+    if total == N:
+        return 1
+
+    ret = 0
+    for i in range(1, 4):
+        total += i
+        ret += solve(total)
+        total -= i
+
+    return ret
+
+
 T = int(input())
 
-
-def dfs(total, n):
-    if total >= n:
-        if total == n:
-            return 1
-        else:
-            return 0
-
-    result = 0
-
-    for i in range(1, 4):
-        result += dfs(total + i, n)
-
-    return result
-
-
+result = []
 for _ in range(T):
-    print(dfs(0, int(input())))
+    N = int(input())
+    result.append(solve(0))
+    solve.cache_clear()
+
+print("\n".join(map(str, result)))
