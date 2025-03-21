1a56d6e51037e96253df280a667d47cbb72bf741
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-21 14:29:36 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 6004 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,58 +1,51 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    string A, B;
-    cin >> A >> B;
-
-    int N = A.size();
-    int M = B.size();
-
-    // DP 배열 준비
-    vector<vector<int>> dp(N + 1, vector<int>(M + 1, 0));
-
-    // DP로 LCS 길이 계산
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= M; j++) {
-            if (A[i - 1] == B[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
+    
+    string s1, s2;
+    cin >> s1 >> s2;
+    
+    int n = s1.size(), m = s2.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+    
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            if (s1[i-1] == s2[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
         }
     }
-
-    // LCS 길이
-    int lcs_length = dp[N][M];
-    cout << lcs_length << "\n";
-
-    // 역추적을 통해 실제 LCS 문자열 구성
-    string lcs = "";
-    int i = N, j = M;
-    while (i > 0 && j > 0) {
-        if (A[i - 1] == B[j - 1]) {
-            lcs += A[i - 1];  // 공통 문자를 추가
+    
+    int lcsLength = dp[n][m];
+    cout << lcsLength << "\n";
+    
+    if(lcsLength == 0)
+        return 0;
+    
+    string lcs;
+    int i = n, j = m;
+    while(i > 0 && j > 0){
+        if(s1[i-1] == s2[j-1]){
+            lcs.push_back(s1[i-1]);
             i--;
             j--;
         } else {
-            if (dp[i - 1][j] > dp[i][j - 1]) {
+            if(dp[i-1][j] >= dp[i][j-1])
                 i--;
-            } else {
+            else
                 j--;
-            }
         }
     }
-
-    // 뒤집으면 정방향 LCS가 됨
+    
     reverse(lcs.begin(), lcs.end());
-
-    // LCS 길이가 0이 아니라면 출력
-    if (lcs_length > 0) {
-        cout << lcs << "\n";
-    }
-
+    cout << lcs;
+    
     return 0;
 }
