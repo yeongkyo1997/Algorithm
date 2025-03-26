db79c6ebe689ee7fca1f68f7003c92ca008f56ad
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-03-27 08:43:19 +0900

    [Gold IV] Title: 플로이드, Time: 184 ms, Memory: 114904 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,23 +1,27 @@
-N = int(input())
-M = int(input())
-inf = 1e10
-board = [[inf] * (N + 1) for _ in range(N + 1)]
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
-        if i == j:
-            board[i][j] = 0
-for _ in range(M):
+import sys
+from collections import defaultdict
+
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+m = int(input())
+
+dist = [[float("inf")] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    dist[i][i] = 0
+
+for _ in range(m):
     a, b, c = map(int, input().split())
-    board[a][b] = min(board[a][b], c)
+    dist[a][b] = min(dist[a][b], c)
 
-for k in range(1, N + 1):
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
-            board[i][j] = min(board[i][j], board[i][k] + board[k][j])
 
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
-        if board[i][j] == inf:
-            board[i][j] = 0
-        print(board[i][j], end=' ')
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            dist[i][j] = min(dist[i][k] + dist[k][j], dist[i][j])
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(dist[i][j] if dist[i][j] != float("inf") else 0, end=" ")
     print()
