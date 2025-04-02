21ac5c49dbf09d2dc8251a2ea0d178675507b45d
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-04-03 08:08:16 +0900

    [Bronze II] Title: 분해합, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,8 +1,31 @@
-N = int(input())
+import sys
 
-for i in range(1, N + 1):
-    if i + sum(map(int, str(i))) == N:
-        print(i)
-        break
-else:
-    print(0)
+
+def solve():
+
+    n_str = sys.stdin.readline().strip()
+    n = int(n_str)
+
+    start_m = max(1, n - 9 * len(n_str))
+
+    found = False
+    for m in range(start_m, n):
+
+        sum_digits = 0
+        temp_m = m
+        while temp_m > 0:
+            sum_digits += temp_m % 10
+            temp_m //= 10
+
+        decomposition_sum = m + sum_digits
+
+        if decomposition_sum == n:
+            print(m)
+            found = True
+            break
+
+    if not found:
+        print(0)
+
+
+solve()
