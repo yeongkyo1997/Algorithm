34be9d642e4f662d11f501d553ff3b04a7d30b6a
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-04-08 08:12:25 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1020 ms, Memory: 154844 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,45 +1,24 @@
-import collections
-import heapq
-import math
 import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
-n, m, k, x = map(int, input().split())
-graph = collections.defaultdict(list)
-dist = collections.defaultdict(lambda: math.inf)
-visited = [False] * (n + 1)
-
-for _ in range(m):
+from collections import deque
+input = sys.stdin.readline
+N, M, K, X = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
     a, b = map(int, input().split())
-    graph[a].append((b, 1))
-    
-
-
-def dijkstra():
-    dist[x] = 0
-    heap = []
-    heapq.heappush(heap, (0, x))
-
-    while heap:
-        cur = heapq.heappop(heap)
-
-        for ele in graph[cur[1]]:
-            if dist[ele[0]] > dist[cur[1]] + ele[1]:
-                dist[ele[0]] = dist[cur[1]] + ele[1]
-                heapq.heappush(heap, (dist[ele[0]], ele[0]))
-
-
-dijkstra()
-
-answer = []
-for i in range(1, n + 1):
-    if dist[i] == k:
-        answer.append(i)
-
-if answer:
-    answer.sort()
-    for i in answer:
+    graph[a].append(b)
+dist = [-1]*(N+1)
+dist[X] = 0
+q = deque([X])
+while q:
+    cur = q.popleft()
+    for nxt in graph[cur]:
+        if dist[nxt] == -1:
+            dist[nxt] = dist[cur] + 1
+            q.append(nxt)
+found = False
+for i in range(1, N+1):
+    if dist[i] == K:
         print(i)
-else:
+        found = True
+if not found:
     print(-1)
