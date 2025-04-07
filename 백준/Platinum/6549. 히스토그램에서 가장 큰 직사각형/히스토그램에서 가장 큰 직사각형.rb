d4af5431412a289f3b99d62910abc04b02ffb90e
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-04-08 08:55:32 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 292 ms, Memory: 139708 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.rb
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.rb
@@ -1,5 +1,5 @@
 loop{l=gets.split.map(&:to_i);n=l.shift;break if n==0
 h=l+[0];s=[];m=0
-h.each_with_index{|hh,i|
-while s[0]&&h[s[-1]]>=hh;ht=h[s.pop];wd=s[0]?i-s[-1]-1:i;m=[m,ht*wd].max end;s<<i}
+h.each_with_index{|z,i|
+while s[0]&&h[s[-1]]>=z;t=h[s.pop];w=s[0]?i-s[-1]-1:i;m=[m,t*w].max end;s<<i}
 puts m}
