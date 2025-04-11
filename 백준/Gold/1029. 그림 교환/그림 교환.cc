6adbdd7acf97c1ebf166b3471e929965ea24ca65
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-04-11 17:17:49 +0900

    [Gold I] Title: 그림 교환, Time: 28 ms, Memory: 21224 KB -BaekjoonHub

--- 백준/Gold/1029. 그림 교환/그림 교환.cc
+++ 백준/Gold/1029. 그림 교환/그림 교환.cc
@@ -1,61 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
+#include <cstring> 
 
 using namespace std;
 
-int D[1 << 15][17];
-int N;
-char price[17][17];
+int n;
+int cost[15][15];
+int dp[15][10][1 << 15];
+int solve(int current_artist, int current_price, int visited_mask) {
+    if (dp[current_artist][current_price][visited_mask] != -1) {
+        return dp[current_artist][current_price][visited_mask];
+    }
 
-int vec2i(vector<int> V) {
-	int a = 0;
-	for (auto val : V) {
-		a = ((a << 1) | val);
-	}
-	return a;
+    int max_owners = __builtin_popcount(visited_mask);
+
+    for (int next_artist = 0; next_artist < n; ++next_artist) {
+        if (!(visited_mask & (1 << next_artist))) {
+            int selling_price = cost[current_artist][next_artist];
+            if (selling_price >= current_price) {
+                max_owners = max(max_owners,
+                                 solve(next_artist, selling_price, visited_mask | (1 << next_artist)));
+            }
+        }
+    }
+
+    return dp[current_artist][current_price][visited_mask] = max_owners;
 }
-int bit(int i) {
-	return 1 << (N - 1 - i);
-}
-int main(void) {
-	cin >> N;
 
-	for (int i = 0; i < N; i++)
-		cin >> price[i];
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			price[i][j] -= '0';
+    cin >> n;
 
-	for (int i = 0; i < (1 << N); i++)
-		for (int j = 0; j < N; j++)
-			D[i][j] = 0x7ffff;
-	D[bit(0)][0] = 0;
+    for (int i = 0; i < n; ++i) {
+        string row;
+        cin >> row;
+        for (int j = 0; j < n; ++j) {
+            cost[i][j] = row[j] - '0'; 
+        }
+    }
 
-	int ans = 1;
+    memset(dp, -1, sizeof(dp));
 
-	for (int buyer = 2; buyer <= N; buyer++) {
-		vector<int> tmp(N);
-		for (int i = N - buyer; i < N; i++)
-			tmp[i] = 1;
-		do {
-			int bitmask1 = vec2i(tmp);
-			for (int i = 0; i < N; i++) {
-				if (tmp[i] == 1) {
+    int result = solve(0, 0, 1);
 
-					int bitmask2 = bitmask1 - bit(i);
-					for (int lst = 0; lst < N; lst++) {
-						if (lst == i)
-							continue;
-						if (D[bitmask2][lst] <= price[lst][i] && price[lst][i] < D[bitmask1][i]) {
-							D[bitmask1][i] = price[lst][i];
-							ans = buyer;
-						}
-					}
-				}
-			}
-		} while (next_permutation(tmp.begin(), tmp.end()));
-	}
-	cout << ans;
+    cout << result << endl;
+
+    return 0;
 }
