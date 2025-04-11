63ff684787526b41dbb9a8f7f43f516d23beac78
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-04-11 16:22:30 +0900

    [Bronze V] Title: Арифметическая магия, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int n;
+    std::cin >> n; 
+
+    std::cout << 1 << std::endl;
+
+    return 0;
+}
