9cd3e53a1192fcf46bef90e91f6ad8545217a40d
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-04-27 18:17:28 +0900

    [Ruby V] Title: 수열과 쿼리 25, Time: 408 ms, Memory: 18432 KB -BaekjoonHub

--- 백준/Ruby/17473. 수열과 쿼리 25/수열과 쿼리 25.cc
+++ 백준/Ruby/17473. 수열과 쿼리 25/수열과 쿼리 25.cc
@@ -1,212 +1,170 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-const int K = 20;
+const int MAXN = 200000;
+const int FULL_MASK = (1 << 20) - 1;
 
-const int ALL_ONES = (1 << K) - 1;
+// 트리의 각 노드 정보
+struct Node {
+    int mx;        // 구간 내 최대값
+    int mn_and;    // 구간 내 모든 값의 비트 AND
+    int mx_or;     // 구간 내 모든 값의 비트 OR
+    int lz_and;    // 지연 AND 마스크
+    int lz_or;     // 지연 OR 마스크
+} seg[4*MAXN+5];
 
-struct Node
-{
-    int val_or = 0;
-    int val_and = ALL_ONES;
-    int max_val = 0;
-    int lazy_or = 0;
-    int lazy_and = ALL_ONES;
-};
+int N, M;
+int A[MAXN+1];
 
-vector<Node> tree;
-vector<int> a;
-int n;
-
-void pull(int v)
-{
-
-    if (2 * v + 1 >= tree.size())
-        return;
-    tree[v].val_or = tree[2 * v].val_or | tree[2 * v + 1].val_or;
-    tree[v].val_and = tree[2 * v].val_and & tree[2 * v + 1].val_and;
-    tree[v].max_val = max(tree[2 * v].max_val, tree[2 * v + 1].max_val);
-}
-
-void apply_transformation(int v, int op_and, int op_or)
-{
-
-    if (op_and == ALL_ONES && op_or == 0)
-        return;
-
-    tree[v].max_val = (tree[v].max_val & op_and) | op_or;
-    tree[v].val_or = (tree[v].val_or & op_and) | op_or;
-    tree[v].val_and = (tree[v].val_and & op_and) | op_or;
-
-    tree[v].lazy_or = (tree[v].lazy_or & op_and) | op_or;
-    tree[v].lazy_and = (tree[v].lazy_and & op_and) | op_or;
-}
-
-void push(int v)
-{
-
-    if (tree[v].lazy_and == ALL_ONES && tree[v].lazy_or == 0)
-        return;
-
-    if (2 * v + 1 >= tree.size())
-        return;
-
-    apply_transformation(2 * v, tree[v].lazy_and, tree[v].lazy_or);
-    apply_transformation(2 * v + 1, tree[v].lazy_and, tree[v].lazy_or);
-
-    tree[v].lazy_and = ALL_ONES;
-    tree[v].lazy_or = 0;
-}
-
-void build(int v, int tl, int tr)
-{
-
-    tree[v].lazy_or = 0;
-    tree[v].lazy_and = ALL_ONES;
-    if (tl == tr)
-    {
-
-        if (tl < n)
-        {
-            tree[v].val_or = a[tl];
-            tree[v].val_and = a[tl];
-            tree[v].max_val = a[tl];
-        }
-        else
-        {
-
-            tree[v].val_or = 0;
-            tree[v].val_and = ALL_ONES;
-            tree[v].max_val = 0;
-        }
-    }
-    else
-    {
-        int tm = tl + (tr - tl) / 2;
-        build(2 * v, tl, tm);
-        build(2 * v + 1, tm + 1, tr);
-        pull(v);
+// 노드 초기화
+void init_node(int idx, int l, int r) {
+    seg[idx].lz_and = FULL_MASK;  // AND 마스크 초기값은 전부 1
+    seg[idx].lz_or  = 0;          // OR 마스크 초기값은 전부 0
+    if (l == r) {
+        int v = A[l];
+        seg[idx].mx     = v;
+        seg[idx].mn_and = v;
+        seg[idx].mx_or  = v;
+    } else {
+        int m = (l + r) >> 1;
+        init_node(idx<<1, l, m);
+        init_node(idx<<1|1, m+1, r);
+        // 자식에서 정보 끌어오기
+        seg[idx].mx     = max(seg[idx<<1].mx, seg[idx<<1|1].mx);
+        seg[idx].mn_and = seg[idx<<1].mn_and & seg[idx<<1|1].mn_and;
+        seg[idx].mx_or  = seg[idx<<1].mx_or  | seg[idx<<1|1].mx_or;
     }
 }
 
-void update_and(int v, int tl, int tr, int l, int r, int x)
-{
-
-    if (r < tl || tr < l || l > r)
-    {
-        return;
-    }
-
-    if (l <= tl && tr <= r && ((tree[v].val_or & ~x) == (tree[v].val_and & ~x)))
-    {
-
-        apply_transformation(v, x, 0);
-        return;
-    }
-
-    push(v);
-
-    int tm = tl + (tr - tl) / 2;
-    update_and(2 * v, tl, tm, l, r, x);
-    update_and(2 * v + 1, tm + 1, tr, l, r, x);
-
-    pull(v);
+// 노드에 AND 태그 적용 함수
+inline void apply_and(int idx, int mask) {
+    // 기존 값에 비트 AND
+    seg[idx].mx     &= mask;
+    seg[idx].mn_and &= mask;
+    seg[idx].mx_or  &= mask;
+    // 지연 태그 합성: f(x) = ( (x & lz_and) | lz_or ) & mask
+    seg[idx].lz_and &= mask;
+    seg[idx].lz_or  &= mask;
 }
 
-void update_or(int v, int tl, int tr, int l, int r, int x)
-{
-
-    if (r < tl || tr < l || l > r)
-    {
-        return;
-    }
-
-    if (l <= tl && tr <= r && ((tree[v].val_or & x) == (tree[v].val_and & x)))
-    {
-
-        apply_transformation(v, ALL_ONES, x);
-        return;
-    }
-
-    push(v);
-
-    int tm = tl + (tr - tl) / 2;
-    update_or(2 * v, tl, tm, l, r, x);
-    update_or(2 * v + 1, tm + 1, tr, l, r, x);
-
-    pull(v);
+// 노드에 OR 태그 적용 함수
+inline void apply_or(int idx, int mask) {
+    seg[idx].mx     |= mask;
+    seg[idx].mn_and |= mask;
+    seg[idx].mx_or  |= mask;
+    // 지연 태그 합성: f(x) = ( (x & lz_and) | lz_or ) | mask
+    seg[idx].lz_or |= mask;
 }
 
-int query_max(int v, int tl, int tr, int l, int r)
-{
-
-    if (r < tl || tr < l || l > r)
-    {
-        return 0;
+// 자식에게 지연 태그 전파
+void push(int idx) {
+    if (seg[idx].lz_and != FULL_MASK || seg[idx].lz_or != 0) {
+        apply_and(idx<<1, seg[idx].lz_and);
+        apply_or (idx<<1, seg[idx].lz_or);
+        apply_and(idx<<1|1, seg[idx].lz_and);
+        apply_or (idx<<1|1, seg[idx].lz_or);
+        // 현재 노드 지연 태그 초기화
+        seg[idx].lz_and = FULL_MASK;
+        seg[idx].lz_or  = 0;
     }
-
-    if (l <= tl && tr <= r)
-    {
-        return tree[v].max_val;
-    }
-
-    push(v);
-
-    int tm = tl + (tr - tl) / 2;
-    int max_left = query_max(2 * v, tl, tm, l, r);
-    int max_right = query_max(2 * v + 1, tm + 1, tr, l, r);
-
-    return max(max_left, max_right);
 }
 
-int main()
-{
+// 자식에서 정보 갱신
+inline void pull(int idx) {
+    seg[idx].mx     = max(seg[idx<<1].mx, seg[idx<<1|1].mx);
+    seg[idx].mn_and = seg[idx<<1].mn_and & seg[idx<<1|1].mn_and;
+    seg[idx].mx_or  = seg[idx<<1].mx_or  | seg[idx<<1|1].mx_or;
+}
 
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> n;
-    a.resize(n);
-
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> a[i];
-    }
-
-    tree.resize(4 * n);
-
-    build(1, 0, n - 1);
-
-    int m;
-    cin >> m;
-    for (int k = 0; k < m; ++k)
-    {
-        int type, l, r;
-        cin >> type >> l >> r;
-        l--;
-        r--;
-
-        if (type == 1)
-        {
-            int x;
-            cin >> x;
-            update_and(1, 0, n - 1, l, r, x);
-        }
-        else if (type == 2)
-        {
-            int x;
-            cin >> x;
-            update_or(1, 0, n - 1, l, r, x);
-        }
-        else
-        {
-            cout << query_max(1, 0, n - 1, l, r) << "\n";
+// 구간 [L,R] 에 AND 연산
+void upd_and(int idx, int l, int r, int L, int R, int mask) {
+    if (r < L || R < l) return;
+    if (L <= l && r <= R) {
+        // 이 구간에서 0으로 클리어할 비트가 모두 균일하면(모두 0이거나 모두 1이어서)
+        int bits_to_clear = seg[idx].mx_or & (~mask);
+        if ((seg[idx].mn_and & (~mask)) == bits_to_clear) {
+            // 안전하게 태그로 처리
+            apply_and(idx, mask);
+            return;
         }
     }
+    if (l == r) {
+        // 리프 노드면 직접 적용
+        seg[idx].mx     &= mask;
+        seg[idx].mn_and &= mask;
+        seg[idx].mx_or  &= mask;
+        return;
+    }
+    push(idx);
+    int m = (l + r) >> 1;
+    upd_and(idx<<1, l, m, L, R, mask);
+    upd_and(idx<<1|1, m+1, r, L, R, mask);
+    pull(idx);
+}
 
+// 구간 [L,R] 에 OR 연산
+void upd_or(int idx, int l, int r, int L, int R, int mask) {
+    if (r < L || R < l) return;
+    if (L <= l && r <= R) {
+        // 이 구간에서 1로 세팅할 비트가 모두 균일하면(모두 0이거나 모두 1)
+        int bits_to_set = (~seg[idx].mn_and) & mask;
+        if ((~seg[idx].mx_or & mask) == bits_to_set) {
+            apply_or(idx, mask);
+            return;
+        }
+    }
+    if (l == r) {
+        seg[idx].mx     |= mask;
+        seg[idx].mn_and |= mask;
+        seg[idx].mx_or  |= mask;
+        return;
+    }
+    push(idx);
+    int m = (l + r) >> 1;
+    upd_or(idx<<1, l, m, L, R, mask);
+    upd_or(idx<<1|1, m+1, r, L, R, mask);
+    pull(idx);
+}
+
+// 구간 [L,R] 최대값 조회
+int query_max(int idx, int l, int r, int L, int R) {
+    if (r < L || R < l) return 0;
+    if (L <= l && r <= R) {
+        return seg[idx].mx;
+    }
+    push(idx);
+    int m = (l + r) >> 1;
+    return max(
+        query_max(idx<<1, l, m, L, R),
+        query_max(idx<<1|1, m+1, r, L, R)
+    );
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 입력
+    cin >> N;
+    for (int i = 1; i <= N; ++i) {
+        cin >> A[i];
+    }
+    init_node(1, 1, N);
+
+    cin >> M;
+    while (M--) {
+        int t, L, R, X;
+        cin >> t >> L >> R;
+        if (t == 1) {
+            cin >> X;
+            upd_and(1, 1, N, L, R, X);
+        } else if (t == 2) {
+            cin >> X;
+            upd_or(1, 1, N, L, R, X);
+        } else {
+            cout << query_max(1, 1, N, L, R) << "\n";
+        }
+    }
     return 0;
 }
