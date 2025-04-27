8f4fc6bd7c71b5660d7680ba0c4e8b18bb040ec7
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-04-27 18:12:00 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.cc
@@ -1,48 +1,90 @@
-#include <cstdio>
-
-int n, m, x, y, k;
-int a[20][20];
-int dice[6], temp[6];
-const int direct[4][6] = {
-    {2, 1, 5, 0, 4, 3},
-    {3, 1, 0, 5, 4, 2},
-    {4, 0, 2, 3, 5, 1},
-    {1, 5, 2, 3, 0, 4}
-};
-const int dx[] = {0, 0, -1, 1}, dy[] = {1, -1, 0, 0};
-
-void solve() {
-    while (k--) {
-        int d;
-        scanf("%d", &d); d--;
-        x += dx[d], y += dy[d];
-        if (x < 0 || x >= n || y < 0 || y >= m) {
-            x -= dx[d], y -= dy[d];
-            continue;
-        }
-        for (int i=0; i<6; i++) {
-            temp[i] = dice[i];
-        }
-        for (int i=0; i<6; i++) {
-            dice[i] = temp[direct[d][i]];
-        }
-        if (a[x][y]) {
-            dice[5] = a[x][y];
-            a[x][y] = 0;
-        } else {
-            a[x][y] = dice[5];
-        }
-        printf("%d\n", dice[0]);
-    }
-}
+#include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
-    scanf("%d %d %d %d %d", &n, &m, &x, &y, &k);
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
-            scanf("%d", &a[i][j]);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M, x, y, K;
+    cin >> N >> M >> x >> y >> K;
+
+    // 지도 정보 저장
+    vector<vector<int>> board(N, vector<int>(M));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> board[i][j];
         }
     }
-    solve();
+
+    // 명령 입력
+    vector<int> cmds(K);
+    for (int i = 0; i < K; i++) {
+        cin >> cmds[i];
+    }
+
+    // 주사위 상태: 1:위, 2:북, 3:동, 4:서, 5:남, 6:아래
+    int dice[7] = {0,0,0,0,0,0,0};
+
+    // 동(1), 서(2), 북(3), 남(4) 이동 벡터
+    int dx[5] = {0, 0, 0, -1, 1};
+    int dy[5] = {0, 1, -1, 0, 0};
+
+    // 명령 처리
+    for (int cmd : cmds) {
+        int nx = x + dx[cmd];
+        int ny = y + dy[cmd];
+
+        // 범위 벗어나면 무시
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+
+        // 주사위 굴리기
+        int temp;
+        if (cmd == 1) {
+            // 동쪽으로 굴리기
+            temp = dice[1];
+            dice[1] = dice[4];
+            dice[4] = dice[6];
+            dice[6] = dice[3];
+            dice[3] = temp;
+        } else if (cmd == 2) {
+            // 서쪽으로 굴리기
+            temp = dice[1];
+            dice[1] = dice[3];
+            dice[3] = dice[6];
+            dice[6] = dice[4];
+            dice[4] = temp;
+        } else if (cmd == 3) {
+            // 북쪽으로 굴리기
+            temp = dice[1];
+            dice[1] = dice[5];
+            dice[5] = dice[6];
+            dice[6] = dice[2];
+            dice[2] = temp;
+        } else if (cmd == 4) {
+            // 남쪽으로 굴리기
+            temp = dice[1];
+            dice[1] = dice[2];
+            dice[2] = dice[6];
+            dice[6] = dice[5];
+            dice[5] = temp;
+        }
+
+        // 이동한 칸과 주사위 바닥면 상호 복사
+        if (board[nx][ny] == 0) {
+            board[nx][ny] = dice[6];
+        } else {
+            dice[6] = board[nx][ny];
+            board[nx][ny] = 0;
+        }
+
+        // 현재 윗면 출력
+        cout << dice[1] << '\n';
+
+        // 좌표 업데이트
+        x = nx;
+        y = ny;
+    }
+
     return 0;
 }
