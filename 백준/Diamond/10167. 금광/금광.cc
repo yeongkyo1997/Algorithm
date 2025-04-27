8561aae28ec8877e4c5328f2fdf7452ff6e8eca6
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-04-27 17:56:50 +0900

    [Diamond V] Title: 금광, Time: 252 ms, Memory: 2592 KB, Score: 100 point -BaekjoonHub

--- 백준/Diamond/10167. 금광/금광.cc
+++ 백준/Diamond/10167. 금광/금광.cc
@@ -1,90 +1,99 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>        // 추가된 부분
 using namespace std;
+using ll = long long;
 
-class Node {
-public:
-	long long tsum, lsum, rsum, msum;
-	
-	Node() : tsum{ 0 }, lsum{ 0 }, rsum{ 0 }, msum{ 0 } {};
-	Node sum(int val) {
-		this->tsum += val;
-		this->lsum = this->rsum = this->msum = this->tsum;
-		return *this;
-	}
-	Node merge(const Node& left, const Node& right) {
-		this->tsum = left.tsum + right.tsum;
-		this->lsum = max(left.lsum, left.tsum + right.lsum);
-		this->rsum = max(right.rsum, left.rsum + right.tsum);
-		this->msum = max({ left.rsum + right.lsum, left.msum, right.msum, this->lsum, this->rsum });
-		return *this;
-	}
+// 세그먼트 트리 각 노드 정보
+struct Node {
+    ll sum;   // 구간 전체 합
+    ll pref;  // 구간 접두사 최대 합
+    ll suff;  // 구간 접미사 최대 합
+    ll best;  // 구간 부분 배열(아무 위치든) 최대 합
 };
 
-class Dot {
-public:
-	int x, y, w;
-
-	Dot() {};
-	Dot(int x, int y, int w) : x{ x }, y{ y }, w{ w } {};
-};
-
-long long res;
-int N, xs_len, ys_len;
-
-vector < Node > tree;
-vector < int > xs, ys;
-vector < Dot > dot_list;
-vector < vector<Dot> > zip;
-
-Node update(int inx, int val, int node, int x, int y) {
-	if (y < inx || inx < x)
-		return tree[node];
-	if (x == y)
-		return tree[node].sum(val);
-
-	int mid = (x + y) >> 1;
-	return tree[node].merge(update(inx, val, node * 2, x, mid), update(inx, val, node * 2 + 1, mid + 1, y));
+// 두 자식 노드를 합쳐 부모 노드 정보를 생성
+Node combine(const Node &a, const Node &b) {
+    Node c;
+    c.sum  = a.sum + b.sum;
+    c.pref = max(a.pref, a.sum + b.pref);
+    c.suff = max(b.suff, b.sum + a.suff);
+    c.best = max( max(a.best, b.best), a.suff + b.pref );
+    return c;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	cin >> N;
-	dot_list.resize(N);
-	for (Dot& dot : dot_list) {
-		cin >> dot.x >> dot.y >> dot.w;
-		xs.push_back(dot.x);
-		ys.push_back(dot.y);
-	}
-	sort(xs.begin(), xs.end());
-	sort(ys.begin(), ys.end());
+    int N;
+    cin >> N;
+    vector<ll> xs, ys;
+    xs.reserve(N);
+    ys.reserve(N);
+    vector<tuple<ll,ll,ll>> pts(N);  // tuple 사용
 
-	xs.erase(unique(xs.begin(), xs.end()), xs.end());
-	xs_len = xs.size();
-	ys.erase(unique(ys.begin(), ys.end()), ys.end());
-	ys_len = ys.size();
+    // 입력 읽기 및 좌표 리스트에 저장
+    for (int i = 0; i < N; i++) {
+        ll x, y, w;
+        cin >> x >> y >> w;
+        pts[i] = make_tuple(x, y, w);
+        xs.push_back(x);
+        ys.push_back(y);
+    }
 
-	zip.resize(xs_len);
-	for (Dot& dot : dot_list) {
-		int x = lower_bound(xs.begin(), xs.end(), dot.x) - xs.begin();
-		int y = lower_bound(ys.begin(), ys.end(), dot.y) - ys.begin();
+    // x, y 좌표 압축
+    sort(xs.begin(), xs.end());
+    xs.erase(unique(xs.begin(), xs.end()), xs.end());
+    sort(ys.begin(), ys.end());
+    ys.erase(unique(ys.begin(), ys.end()), ys.end());
+    int X = xs.size(), Y = ys.size();
 
-		zip[x].push_back(Dot(x, y, dot.w));
-	}
+    // 같은 y좌표를 가진 점들을 모아둠
+    vector<vector<pair<int,ll>>> byY(Y);
+    for (auto &t : pts) {
+        ll x, y, w;
+        tie(x, y, w) = t;  // tie 사용 가능
+        int xi = lower_bound(xs.begin(), xs.end(), x) - xs.begin();
+        int yi = lower_bound(ys.begin(), ys.end(), y) - ys.begin();
+        byY[yi].push_back({xi, w});
+    }
 
-	for (int i = 0; i < xs_len; i++) {
-		tree.resize(ys_len * 4);
+    // 세그먼트 트리 크기 결정
+    int size = 1;
+    while (size < X) size <<= 1;
+    vector<Node> tree(2 * size);
 
-		for (int j = i; j < xs_len; j++) {
-			for (Dot& k : zip[j])
-				update(k.y, k.w, 1, 0, ys_len - 1);
-			res = max(tree[1].msum, res);
-		}
+    ll answer = 0;
+    // y범위 [i..j] 로 스위핑
+    for (int i = 0; i < Y; i++) {
+        // 트리 초기화
+        for (int k = 1; k < 2*size; k++)
+            tree[k] = {0,0,0,0};
 
-		tree.clear();
-	}
+        for (int j = i; j < Y; j++) {
+            // y = ys[j] 에 해당하는 모든 점 반영
+            for (auto &p : byY[j]) {
+                int pos = p.first;
+                ll w  = p.second;
+                int node = pos + size;
+                tree[node].sum  += w;
+                ll s = tree[node].sum;
+                tree[node].pref = s;
+                tree[node].suff = s;
+                tree[node].best = s;
+                node >>= 1;
+                while (node > 0) {
+                    tree[node] = combine(tree[2*node], tree[2*node+1]);
+                    node >>= 1;
+                }
+            }
+            // 현재 y-구간[i..j]에서 최대 이익 갱신
+            answer = max(answer, tree[1].best);
+        }
+    }
 
-	cout << res;
-
-	return 0;
+    cout << answer << "\n";
+    return 0;
 }
