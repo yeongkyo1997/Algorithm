f714997af8aaf2c4b3c5664872bb2f88223121bf
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-06-22 10:28:53 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,49 +1,61 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-const int dx[] = { -1,0,1,0 };
-const int dy[] = { 0,-1,0,1 };
+int M, N, K;
+int board[101][101];
+bool vis[101][101];
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
 
-int n, m, k, cnt, a[111][111];
-vector<int> v;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-int dfs(int x, int y) {
-	int ret = 1;
-	a[x][y] = 1;
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i], ny = y + dy[i];
+    cin >> M >> N >> K;
+    // 직사각형 채우기
+    for(int i = 0; i < K; i++){
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        for(int y = y1; y < y2; y++){
+            for(int x = x1; x < x2; x++){
+                board[y][x] = 1;
+            }
+        }
+    }
 
-		if (nx < 0 || ny < 0 || nx >= n || ny >= m)
-			continue;
+    vector<int> areas;
 
-		if (!a[nx][ny]) ret += dfs(nx, ny);
-	}
-	return ret;
-}
+    // BFS로 빈 칸 영역 찾기
+    for(int y = 0; y < M; y++){
+        for(int x = 0; x < N; x++){
+            if(board[y][x] == 0 && !vis[y][x]){
+                int cnt = 0;
+                queue<pair<int,int>> q;
+                vis[y][x] = true;
+                q.push({y, x});
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+                while(!q.empty()){
+                    auto cur = q.front(); q.pop();
+                    int cy = cur.first;
+                    int cx = cur.second;
+                    cnt++;
+                    for(int dir = 0; dir < 4; dir++){
+                        int ny = cy + dy[dir];
+                        int nx = cx + dx[dir];
+                        if(ny<0 || ny>=M || nx<0 || nx>=N) continue;
+                        if(vis[ny][nx] || board[ny][nx]==1) continue;
+                        vis[ny][nx] = true;
+                        q.push({ny, nx});
+                    }
+                }
+                areas.push_back(cnt);
+            }
+        }
+    }
 
-	cin >> n >> m >> k;
-	while (k--) {
-		int e, b, c, d;
-		cin >> e >> b >> c >> d;
-		for (int i = b; i < d; i++)
-			for (int j = e; j < c; j++)
-				a[i][j] = 1;
-	}
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			if (!a[i][j])
-				v.push_back(dfs(i, j));
-
-	sort(v.begin(), v.end());
-	cout << v.size() << '\n';
-	for (auto now : v)
-		cout << now << ' ';
+    sort(areas.begin(), areas.end());
+    cout << areas.size() << "\n";
+    for(int a : areas) cout << a << " ";
+    cout << "\n";
+    return 0;
 }
