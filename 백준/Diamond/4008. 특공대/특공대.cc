7e84881b4c3518eb9dc23c872d92e474cf9cabce
IN YEONG KYO <yeongkyo1997@gmail.com>
2025-09-04 09:30:59 +0900

    [Diamond V] Title: 특공대, Time: 88 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Diamond/4008. 특공대/특공대.cc
+++ 백준/Diamond/4008. 특공대/특공대.cc
@@ -1,97 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
+using int64 = long long;
 
-struct Line
-{
-    ll m;
-    ll c;
+struct Line {
+    int64 m; // slope
+    int64 b; // intercept
+    // evaluate at x
+    inline int64 eval(int64 x) const {
+        return m * x + b;
+    }
 };
 
-bool is_bad(const Line &l1, const Line &l2, const Line &l3)
-{
-
-    __int128 lhs = (__int128)(l2.c - l1.c) * (l1.m - l3.m);
-    __int128 rhs = (__int128)(l3.c - l1.c) * (l1.m - l2.m);
-    return lhs >= rhs;
+// l2가 l1과 l3 사이에서 쓸모없는지 판단 (기울기 증가 가정: m1 < m2 < m3)
+// x12 >= x23 이면 l2는 불필요.
+// (b1 - b2)/(m2 - m1) >= (b2 - b3)/(m3 - m2)
+// (b1 - b2)*(m3 - m2) >= (b2 - b3)*(m2 - m1)
+static inline bool bad(const Line& l1, const Line& l2, const Line& l3) {
+    __int128 left  = (__int128)(l1.b - l2.b) * (l3.m - l2.m);
+    __int128 right = (__int128)(l2.b - l3.b) * (l2.m - l1.m);
+    return left >= right;
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
+    cin.tie(nullptr);
 
     int n;
-    cin >> n;
-
-    ll a, b, c;
+    if (!(cin >> n)) return 0;
+    int64 a, b, c;
     cin >> a >> b >> c;
 
-    vector<ll> x(n);
-    for (auto &val : x)
-        cin >> val;
+    deque<Line> hull;
+    auto add_line = [&](int64 m, int64 k) {
+        Line L{m, k};
+        while (hull.size() >= 2 && bad(hull[hull.size()-2], hull[hull.size()-1], L))
+            hull.pop_back();
+        hull.push_back(L);
+    };
+    auto query = [&](int64 x) -> int64 {
+        while (hull.size() >= 2 && hull[0].eval(x) <= hull[1].eval(x))
+            hull.pop_front();
+        return hull.front().eval(x);
+    };
 
-    vector<ll> prefix_sum(n + 1, 0);
-    for (int i = 1; i <= n; i++)
-        prefix_sum[i] = prefix_sum[i - 1] + x[i - 1];
+    // 초기 상태: j=0 라인 추가 (S[0]=0, dp[0]=0)
+    // m0 = -2*a*S[0] = 0, b0 = dp[0] + a*S[0]^2 - b*S[0] = 0
+    add_line(0, 0);
 
-    vector<ll> dp(n + 1, 0);
-    dp[0] = 0;
+    int64 S = 0;      // prefix sum
+    int64 dp = 0;     // dp[i]
+    for (int i = 1; i <= n; ++i) {
+        int64 x;
+        cin >> x;
+        S += x;
 
-    deque<Line> dq;
+        // best = max_j (m_j * S + b_j)
+        int64 best = query(S);
 
-    Line initial;
-    initial.m = 0;
-    initial.c = 0;
-    dq.push_back(initial);
+        // dp[i] = a*S^2 + b*S + c + best
+        int64 dp_i = a * S * S + b * S + c + best;
 
-    for (int i = 1; i <= n; i++)
-    {
-        ll x_i = prefix_sum[i];
+        // 새 라인 추가: m_i = -2*a*S, b_i = dp[i] + a*S^2 - b*S
+        int64 m_i = -2 * a * S;
+        int64 k_i = dp_i + a * S * S - b * S;
+        add_line(m_i, k_i);
 
-        while (dq.size() >= 2)
-        {
-            Line first = dq[0];
-            Line second = dq[1];
-
-            ll val1 = first.m * x_i + first.c;
-            ll val2 = second.m * x_i + second.c;
-            if (val2 >= val1)
-            {
-                dq.pop_front();
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        Line best = dq[0];
-        ll contribution = best.m * x_i + best.c;
-        dp[i] = a * x_i * x_i + b * x_i + c + contribution;
-
-        Line new_line;
-        new_line.m = -2 * a * prefix_sum[i];
-        new_line.c = dp[i] + a * prefix_sum[i] * prefix_sum[i] - b * prefix_sum[i];
-
-        while (dq.size() >= 2)
-        {
-            Line l1 = dq[dq.size() - 2];
-            Line l2 = dq[dq.size() - 1];
-            Line l3 = new_line;
-            if (is_bad(l1, l2, l3))
-            {
-                dq.pop_back();
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        dq.push_back(new_line);
+        dp = dp_i;
     }
 
-    cout << dp[n];
+    cout << dp << '\n';
+    return 0;
 }
